603eb1d78f1fcbbafcc555ac638950537f6deea9
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2018-10-20 14:26:18 +0200

    Update Contents.m

--- mss_examples/Contents.m
+++ mss_examples/Contents.m
@@ -3,29 +3,29 @@
 % Ref.: T. I. Fossen (2011). Handbook of Marine Craft Hydrodynamics and
 %       Motion Control. John Wiley & Sons Ltd.
 %
-% Examples
-%    ExLQFinHor   - LQ finite time-horizon tracking of mass-damper-spring system
-%    ExLQR        - Computes the LQR gains for a mass-damper system
-%    ExLQtrack    - Computes the LQ optimal tracking gains for a mass-damper system
-%    ExMDS        - Plots the step response of a 2nd-order mass-damper system 
-%    ExKT         - Computation of Nomoto gain and time const. using nonlinear least-squares (NLS)
-%    ExLinspec    - Linear approx. to PM, JONSWAP and Torsethaugen spectra using NLS
-%    ExMSI        - Plots the ISO 2631-1 and O'Hanlon and McCauley Motion Sickness Incidence curves
-%    ExNomoto     - Generates the Nomoto Bode plots for a cargo ship and tanker
-%    ExKF         - Example implementation of a KF using different measurement/sample frequencies
-%    ExEKF        - Example implementation of an EKF using different measurement/sample frequencies
-%    ExObsCtr     - Observability and Controllability of Ships
-%    ExPassiveObs - Plots the loop transfer function of the passive observer
-%    ExPathGen    - Path generation using cubic polynominals
-%    ExPullout    - Produces a pullout maneuver for the mariner class vessel and a container ship
-%    ExQuadProg   - Quadratic programming for way-point trajectory generation
-%    ExQuest      - 6 DOF position/attitude vector from camera measurements using the QUEST algorithm
-%    ExRefMod     - 2nd-order reference model with nonlinear damping and velocity saturation
-%    ExSpline     - Cubic Hermite and spline interpolation of waypoints 
-%    EXRRD1       - Roll and sway-yaw transfer functions for the Son and Nomoto container ship
-%    ExRRD2       - Rudder-Roll Damping (RRD) system for the Son and Nomoto container ship
-%    ExRRD3       - Inverse response in roll due to right-half-plane zero (non-minimum phase)
-%    ExTurnCircle - Generates the turning circle for the mariner class vessel and a container ship
-%    ExWageningen - Computes thrust and torque for the Wageningen B-series propellers
-%    ExWindForce  - Plotting the wind forces and moment using Isherwoods (1972) formulas
-%    ExZigZag     - Produces a zigzag maneuver for the mariner class vessel and a container ship
+% Examples 
+% ExKF         - Discrete-time KF using different measurement/sampling-time frequencies
+% ExEKF        - Discrte-time EKF using different measurement/sampling-time frequencies
+% ExKT         - Computation of Nomoto gain and time const. using nonlinear least-squares (NLS)
+% ExLinspec    - Linear approx. to PM, JONSWAP and Torsethaugen spectra using NLS
+% ExLQFinHor   - LQ finite time-horizon tracking of mass-damper-spring system
+% ExLQR        - Computes the LQR gains for a mass-damper system
+% ExLQtrack    - Computes the LQ optimal tracking gains for a mass-damper system
+% ExMDS        - Plots the step response of a 2nd-order mass-damper system 
+% ExMSI        - Plots the ISO 2631-1 and O'Hanlon and McCauley Motion Sickness Incidence curves
+% ExNomoto     - Generates the Nomoto Bode plots for a cargo ship and tanker
+% ExObsCtr     - Observability and Controllability of Ships
+% ExPassiveObs - Plots the loop transfer function of the passive observer
+% ExPathGen    - Path generation using cubic polynominals
+% ExPullout    - Produces a pullout maneuver for the mariner class vessel and a container ship
+% ExQuadProg   - Quadratic programming for way-point trajectory generation
+% ExQuest      - 6 DOF position/attitude vector from camera measurements using the QUEST algorithm
+% ExRefMod     - 2nd-order reference model with nonlinear damping and velocity saturation
+% ExSpline     - Cubic Hermite and spline interpolation of waypoints 
+% EXRRD1       - Roll and sway-yaw transfer functions for the Son and Nomoto container ship
+% ExRRD2       - Rudder-Roll Damping (RRD) system for the Son and Nomoto container ship
+% ExRRD3       - Inverse response in roll due to right-half-plane zero (non-minimum phase)
+% ExTurnCircle - Generates the turning circle for the mariner class vessel and a container ship
+% ExWageningen - Computes thrust and torque for the Wageningen B-series propellers
+% ExWindForce  - Plotting the wind forces and moment using Isherwoods (1972) formulas
+% ExZigZag     - Produces a zigzag maneuver for the mariner class vessel and a container ship
