fed6e4596d198cc86ce0049998c978eb72b92f6c
Thor I. Fossen <thor.fossen@ntnu.no>
2019-04-25 20:53:36 +0200

    Update Gmtrx.m

--- GNC/Gmtrx.m
+++ GNC/Gmtrx.m
@@ -1,4 +1,4 @@
-function G = Gmtrx(nabla,A_wp,GMT,GML,r_p)
+function G = Gmtrx(nabla,A_wp,GMT,GML,LCF,r_p)
 % G = GMTRX(nabla,A_wp,GMT,GML,r_gp) computes the 6x6 system spring stiffness matrix G
 % about an arbitrarily point P for a floating vessel (small roll and pitch angles).
 % For submerged vessels, see gvect.m
@@ -6,7 +6,10 @@ function G = Gmtrx(nabla,A_wp,GMT,GML,r_p)
 % Inputs:  nabla: deplasement
 %          Awp: water plane area
 %          GMT, GML: transverse/longitudinal metacentric heights
-%          r_p = [x_p y_p z_p]': location of P with respect to CO
+%          LCF = x coordinate from CO to CF, positive forwards
+%                negative for conventional ships
+%          r_p = [x_p y_p z_p]': location of P with respect to CO,  
+%                 use  r_p = [0, 0, 0]' for CO midships
 %
 % Author:     Thor I. Fossen
 % Date:       14th June 2001
@@ -14,12 +17,18 @@ function G = Gmtrx(nabla,A_wp,GMT,GML,r_p)
 %                              one zero in G was removed
 %             20th Oct 2008,   improved documentation, use r_p for
 %                              arbitrarily point
+%             25 Apr 2019,     added LCF as input parameter
 
 rho = 1025;  % density of water
 g   = 9.81;	 % acceleration of gravity
 
+% Location of CF
+r_f = [LCF, 0, 0]';
+
+% Values in CF
 Zz     = -rho*g*A_wp;
 Kphi   = -rho*g*nabla*GMT;
 Mtheta = -rho*g*nabla*GML;
-G_CO   = diag([0 0 -Zz -Kphi -Mtheta 0]);  % assumes that CO = CF
+G_CF   = diag([0 0 -Zz -Kphi -Mtheta 0]);  
+G_CO = Hmtrx(r_f)' * G_CF * Hmtrx(r_f);
 G = Hmtrx(r_p)' * G_CO * Hmtrx(r_p);
