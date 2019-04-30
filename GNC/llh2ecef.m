55ee76b5cb93de3650e41ec9dd2beb9a133f4668
Thor I. Fossen <thor.fossen@ntnu.no>
2019-04-30 10:00:49 +0200

    Update llh2ecef.m

--- GNC/llh2ecef.m
+++ GNC/llh2ecef.m
@@ -5,9 +5,11 @@ function [x,y,z] = llh2ecef(l,mu,h)
 % Author:    Thor I. Fossen
 % Date:      14th June 2001
 % Revisions: 27th January 2003, inputs l and mu are defined in rad
+%            30 Apr 2019, added decimals to r_e and r_p
 
 r_e = 6378137; % WGS-84 data
-r_p = 6356752;
+r_p = 6356752.3142;
+
 e = 0.08181979099211;
 N = r_e^2/sqrt( (r_e*cos(mu))^2 + (r_p*sin(mu))^2 );
 x = (N+h)*cos(mu)*cos(l);
