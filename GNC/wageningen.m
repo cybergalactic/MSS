f1f0b744d198d5f8b4d18d4688157a63775bcd4c
Thor I. Fossen <thor.fossen@ntnu.no>
2019-06-18 13:44:25 +0200

    Update wageningen.m

--- GNC/wageningen.m
+++ GNC/wageningen.m
@@ -1,11 +1,11 @@
-function [K_T, K_Q] = Wageningen(Ja,PD,AEAO,z)
-% [K_T, K_Q] = Wageningen(Ja,PD,AEAO,z) computes the thrust and torque
+function [KT, KQ] = wageningen(Ja,PD,AEAO,z)
+% [KT, KQ] = wageningen(Ja,PD,AEAO,z) computes the thrust and torque
 % coefficients of the Wageningen B-series propellers for:
 %
 % Ja = Va/(n*D)  advance number
-% PD            pitch/diameter ratio (typically 0.5-2.5)
-% AEAO          blade area ratio (ratio of expanded blade area to propeller disk area)
-% z             number of propeller blades%
+% PD             pitch/diameter ratio (typically 0.5-2.5)
+% AEAO           blade area ratio (ratio of expanded blade area to propeller disk area)
+% z              number of propeller blades
 %
 % see ExWageningen.m for use of [K_T, K_Q] = Wageningen(Ja,PD,AEAO,z)
 %
@@ -17,17 +17,17 @@ function [K_T, K_Q] = Wageningen(Ja,PD,AEAO,z)
 % blades, the blade area ratio, the pitch_diameter ratio and the advance
 % coefficient.
 %
-% Reference: Barnitsas, M.M., Ray, D. and Kinley, P.
-% Kt, Kq and Efficiency Curves for the Wageningen B-Series Propellers
-% https://deepblue.lib.umich.edu/handle/2027.42/3557
+% Reference: Barnitsas, M.M., Ray, D. and Kinley, P. (1981).
+% KT, KQ and Efficiency Curves for the Wageningen B-Series Propellers
+% http://deepblue.lib.umich.edu/handle/2027.42/3557
 %
 % Author:    Thor I. Fossen
 % Date:      7 October 2018
-% Revisions:
+% Revisions: 18 June 2019 - minor updates
 
 load('WageningData.mat');
     
-K_T = sum(WagCThrust_stuv.*((Ja).^WagThrust_s).*(PD.^WagThrust_t).*...
+KT = sum(WagCThrust_stuv.*((Ja).^WagThrust_s).*(PD.^WagThrust_t).*...
     (AEAO.^WagThrust_u).*(z.^WagThrust_v));
-K_Q = sum(WagCTorque_stuv.*((Ja).^WagTorque_s).*(PD.^WagTorque_t).*...
+KQ = sum(WagCTorque_stuv.*((Ja).^WagTorque_s).*(PD.^WagTorque_t).*...
     (AEAO.^WagTorque_u).*(z.^WagTorque_v));
