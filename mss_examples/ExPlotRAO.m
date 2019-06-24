f8fbdc921aaa3dc0bea3600627ceafbe70d73055
Thor I. Fossen <thor.fossen@ntnu.no>
2019-06-24 10:13:10 +0200

    Update ExPlotRAO.m

--- mss_examples/ExPlotRAO.m
+++ mss_examples/ExPlotRAO.m
@@ -1,4 +1,4 @@
-% ExPlotRAOs  Script for plotting motion and force RAOs
+% ExPlotRAO  Script for plotting motion and force RAOs
 %
 % Author:    Thor I. Fossen
 % Date:      23 June 2019
@@ -6,40 +6,50 @@
 
 load supply;
 
+pre  = {'Surge','Sway','Heave','Roll','Pitch','Yaw'};
+post = {' (m)',' (m)',' (m)',' (deg)',' (deg)',' (deg)'};
+txtMa = strcat(pre,' motion RAO - amplitude',post); 
+txtMp = strcat(pre,' motion RAO - phase (deg)'); 
+post = {' (N)',' (N)',' (N)',' (Nm)',' (Nm)',' (Nm)'};
+txtFa = strcat(pre,' force RAO - amplitude',post); 
+txtFp = strcat(pre,' force RAO - phase (deg)'); 
+
 for DOF = 1:6
     figure(DOF);
     
-    % Plot motion RAO
+    % Plot motion RAOs
     w     = vessel.motionRAO.w;
     amp   = vessel.motionRAO.amp;
     phase = vessel.motionRAO.phase;
-    subplot(411); plotRAOamp(w,amp,DOF);  
-    subplot(412); plotRAOphs(w,phase,DOF);
+    subplot(411); plotRAOamp(w,amp,DOF);   title(txtMa(DOF));
+    subplot(412); plotRAOphs(w,phase,DOF); title(txtMp(DOF));    
     
-    % Plot force RAO
+    % Plot force RAOs
     w     = vessel.forceRAO.w;
     amp   = vessel.forceRAO.amp;
-    phase = vessel.forceRAO.phase;
-    subplot(413); plotRAOamp(w,amp,DOF);
-    subplot(414); plotRAOphs(w,phase,DOF);
+    phase = vessel.forceRAO.phase;    
+    subplot(413); plotRAOamp(w,amp,DOF);   title(txtFa(DOF));    
+    subplot(414); plotRAOphs(w,phase,DOF); title(txtFp(DOF));     
 end
 
 
 function plotRAOamp(w,amp,DOF)
     velno = 1;   
     arg = amp{DOF}(:,:,velno);
-    plot(w,arg(:,1),'-*k',w, arg(:,4),'-ko',w, arg(:,7),'-k<',w, arg(:,10),'-kx')
-    xlabel('wave encounter frequency (rad/s)')
+    S = 1; if (DOF > 3), S = 180/pi; end  % scale DOF 4,5,6 to degrees
+    plot(w,S*arg(:,1),'-*k',w, S*arg(:,4),'-ko',w, S*arg(:,7),'-k<',w, S*arg(:,10),'-kx')
+    xlabel('frequency (rad/s)')
     legend('0 deg','30 deg','60 deg','90 deg')
     grid
 end
 
 function plotRAOphs(w,phase,DOF)
     velno = 1;    
+    txt = {'Surge','Sway','Heave','Roll','Pitch','Yaw'};
     phs = (180/pi)*unwrap(phase{DOF}(:,:,velno));  
     plot(w,phs(:,1),'-*k',w, phs(:,4),'-ko',w, phs(:,7),'-k<',w, phs(:,10),'-kx')
     legend('0 deg','30 deg','60 deg','90 deg')
-    xlabel('wave encounter frequency (rad/s)')
+    xlabel('frequency (rad/s)')
     grid
 end
 
