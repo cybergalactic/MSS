7ff41bc840a6b1886a08bfe36f5972c80869bfec
Thor I. Fossen <thor.fossen@ntnu.no>
2020-07-30 16:52:36 +0200

    Update ssa.m

--- GNC/ssa.m
+++ GNC/ssa.m
@@ -3,18 +3,16 @@ function angle = ssa(angle,unit)
 % angles. Examples:
 %
 % angle = ssa(angle) maps an angle in rad to the interval [-pi pi) 
-% angle = ssa(angle,'deg') maps an angle in deg to the interval [-180, 180)
+% angle = ssa(angle,'deg') maps an angle in deg to the interval [-180 180)
 %
 % For feedback control systems and state estimators used to control the 
 % attitude of vehicles, the difference of two angles should always be
-% mapped to [-pi pi) or [-180, 180] to avoid step inputs/discontinuties.           
+% mapped to [-pi pi) or [-180 180] to avoid step inputs/discontinuties.           
 %
-% Notice that in many languages, the modulus operator mod(x,y) returns a
-% value with the same sign as x:
-%
-% - C, C++, C# and JavaScript: Use a custom mod function e.g.: 
-%   mod(x, y) = x - floor(x/y) * y;
-% - Unity game engine: Use Mathf.DeltaAngle
+% Note that in many languages (C, C++, C#, JavaScript), the modulus  
+% operator mod(x,y) returns a value with the same sign as x. 
+% For these use a custom mod function: mod(x,y) = x - floor(x/y) * y
+% For the Unity game engine use: Mathf.DeltaAngle
 %
 % Author:     Thor I. Fossen
 % Date:       2018-09-21
