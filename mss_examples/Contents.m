d20d4eb676474314ea557eedeb6f6a61ec9f0996
Thor I. Fossen <thor.fossen@ntnu.no>
2020-10-08 15:43:40 +0200

    Update Contents.m

--- mss_examples/Contents.m
+++ mss_examples/Contents.m
@@ -1,12 +1,15 @@
 % MSS Example Files 
 %
-% Ref.: T. I. Fossen (2011). Handbook of Marine Craft Hydrodynamics and
-%       Motion Control. John Wiley & Sons Ltd.
+% Ref.: T. I. Fossen (2021). Handbook of Marine Craft Hydrodynamics and
+%       Motion Control. John Wiley & Sons Ltd., 2nd edition
 %
-% Examples 
+% Examples: 
 % ExEKF        - Discrte-time EKF using different measurement/sampling-time frequencies
 % ExKF         - Discrete-time KF using different measurement/sampling-time frequencies
 % ExFeedback   - Numerical integration of 1st-order system with feedback and feedforward control laws
+% ExINS_AHRS   - INS error-state Kalman filter with AHRS
+% ExINS_MEKF   - INS error-state MEKF using quaternions
+% ExFFT        - FFT used to compute the encounter frequency
 % ExKT         - Computation of Nomoto gain and time const. using nonlinear least-squares (NLS)
 % ExLinspec    - Linear approx. to PM, JONSWAP and Torsethaugen spectra using NLS
 % ExLQFinHor   - LQ finite time-horizon tracking of mass-damper-spring system
@@ -16,6 +19,7 @@
 % ExMSI        - Plots the ISO 2631-1 and O'Hanlon and McCauley Motion Sickness Incidence curves
 % ExNomoto     - Generates the Nomoto Bode plots for a cargo ship and tanker
 % ExObsCtr     - Observability and Controllability of Ships
+% ExOtter      - 
 % ExPassiveObs - Plots the loop transfer function of the passive observer
 % ExPathGen    - Path generation using cubic polynominals
 % ExPlotRAOs   - Script for plotting motion and force RAOs
@@ -24,10 +28,12 @@
 % ExQuest      - 6 DOF position/attitude vector from camera measurements using the QUEST algorithm
 % ExRefMod     - 2nd-order reference model with nonlinear damping and velocity saturation
 % ExResonance  - Plots the amplitude as a function of resonant frequencies for ships
-% ExSpline     - Cubic Hermite and spline interpolation of waypoints 
 % EXRRD1       - Roll and sway-yaw transfer functions for the Son and Nomoto container ship
 % ExRRD2       - Rudder-Roll Damping (RRD) system for the Son and Nomoto container ship
 % ExRRD3       - Inverse response in roll due to right-half-plane zero (non-minimum phase)
+% ExSMC        - Integral sliding mode control applied to AUV control
+% ExSpline     - Cubic Hermite and spline interpolation of waypoints 
+% ExSTA        - Supertwisting adaptive sliding model control applied to AUV control
 % ExTurnCircle - Generates the turning circle for the mariner class vessel and a container ship
 % ExWageningen - Computes thrust and torque for the Wageningen B-series propellers
 % ExWindForce  - Plotting the wind forces and moment using Isherwoods (1972) formulas
