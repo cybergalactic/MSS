8a28a366d5d028046bd267422b795a21dd04d125
Thor I. Fossen <thor.fossen@ntnu.no>
2021-01-26 15:47:02 +0100

    Update EditAB.m

--- FDI/EditAB.m
+++ FDI/EditAB.m
@@ -1,18 +1,20 @@
-function [Amod,Bmod,Wmod] = EditAB(A,B,W)
+function [Amod,Bmod,Wmod]=EditAB(A,B,W)
 % Function to prepare the data for identification: 
 % Select frequency range and eliminate wildpoints.
 %
-% Use: [Amod,Bmod,Wmod] = EditAB(A,B,W)
+% Use: [Amod,Bmod,Wmod]=EditAB(A,B,W)
 %
 % A - vector of frequency dependant added mass A(w)
 % B - vector of frequency dependant potential damping B(w)
 % W - Vector of frequencies at which A(w) and B(w) are computed
 %
-% The function first prompts the user to select the range of frequencies 
-% for identification. This is done by clicking with the mouse on a 
+%Description:
+%
+% The function first prompts the user to select the range of freuencies to
+% for idnetification. This is done by clicking with the mouse on a
 % plot of the added mass and damping. The low-frequency data point should be clicked first, 
 % then the high-frequency point, and finally press return. The selected data is
-% then replotted.
+% then re-plotted.
 %
 % After selecting the freuquency range, the function allows the elimination
 % of data wildpoints. This is done by clicking the points that are to be
@@ -20,6 +22,7 @@ function [Amod,Bmod,Wmod] = EditAB(A,B,W)
 % enter. This process can be re-initialised within the function in case 
 % a point in deleted accidentaly.
 %
+<<<<<<< HEAD
 % You can test this for elements A(6,6) and B(6,6) using the WAMIT tanker data:
 %
 % > load tanker;
@@ -28,9 +31,14 @@ function [Amod,Bmod,Wmod] = EditAB(A,B,W)
 % Author:    Tristan Perez
 % Date:      2009-09-01 
 % Revisions: 2020-12-23, minor bug fixes 
+=======
+% Created by Tristan Perez (trisan.perez@ntnu.no)
+% Date 2009/9/1, Trondheim, Norway.
+% Revisions:
+>>>>>>> parent of d7c3583... Update EditAB.m
 
 %Select Frequency range from plot
-while 1
+while 1,
 
     figure(101)
     subplot(211),plot(W,A,'o-','LineWidth',2)
@@ -41,22 +49,22 @@ while 1
     grid on
     ylabel('B(w)')
     xlabel('Frequency [rad/s]')
-    disp('---------------------------------------------------------------------')
-    disp('Click on the plot twice to define low and high frequency, RETURN when finsihed')
+    display('---------------------------------------------------------------------')
+    display('Click on the plot twice to define low and high frequency, RETURN when finsihed')
     FR=ginput;
     %
     Wmin = FR(1,1);
     Wmax = FR(2,1);
     Dmin=abs(Wmin-W(1));
     Dmax=abs(Wmax-W(1));
-    for k=1:length(W)
+    for k=1:length(W),
         Dm = abs(Wmin-W(k));
         DM = abs(Wmax-W(k));
-        if Dm<=Dmin
+        if Dm<=Dmin,
             Dmin=Dm;
             Kmin=k;
         end
-        if DM<=Dmax
+        if DM<=Dmax;
             Dmax=DM;
             Kmax=k;
         end
@@ -81,17 +89,17 @@ while 1
     xlabel('Frequency [rad/s]')
     
     %% Eliminate wild points
-    disp('---------------------------------------------------------------------')
+    display('---------------------------------------------------------------------')
     Usropt=input('Do you need to eliminate wild points? (y = yes or RETURN to continue):','s');
-    if Usropt == 'y'
+    if Usropt == 'y',
         disp('Click on the wild points and finish with ENTER')
         FR=ginput;
         %
         Wwp = FR(:,1);
-        for k=1:length(Wwp)
-            for l=1:length(Wm)-1
-                if Wm(l) < Wwp(k) && Wwp(k) < Wm(l+1)
-                    if abs(Wm(l)-Wwp(k)) < abs(Wwp(k)-Wm(l+1))
+        for k=1:length(Wwp),
+            for l=1:length(Wm)-1,
+                if Wm(l) < Wwp(k) & Wwp(k) < Wm(l+1),
+                    if abs(Wm(l)-Wwp(k)) < abs(Wwp(k)-Wm(l+1)),
                         Wm=[Wm(1:l-1); Wm(l+1:end)];
                         Bm=[Bm(1:l-1); Bm(l+1:end)];
                         Am=[Am(1:l-1); Am(l+1:end)];
@@ -106,7 +114,6 @@ while 1
     else
         break
     end
-    
     %Plot added mass and damping to be used for sysid
     figure(101)
     subplot(211),plot(Wm,Am,'o-','LineWidth',2)
@@ -117,7 +124,7 @@ while 1
     grid on
     ylabel('B(w)')
     xlabel('Frequency [rad/s]')
-    disp('---------------------------------------------------------------------')
+    display('---------------------------------------------------------------------')
     Usropt=input('Start again with range selection an wild point elimination? (y = yes or RETURN to continue):','s');
     if isempty(Usropt)
         break
