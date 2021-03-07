cb022e736a4bcdac1be93222ee4e205e4f4eb356
Thor I. Fossen <thor.fossen@ntnu.no>
2021-03-07 20:08:27 +0100

    Update viscous.m

--- HYDRO/utils/viscous.m
+++ HYDRO/utils/viscous.m
@@ -1,10 +1,9 @@
 function Bv = viscous(vessel)
-% VISCOUS_SKIN (MSS Hydro)
 %
-% [Bv] = viscous(vessel,surge_estimate) computes a constant viscous skin
-% friction and roll damping matirx Bv. 
+% [Bv] = viscous(vessel,surge_estimate) computes the viscous damping
+%     matrix Bv. 
 % 
-%     DOF (i = 1,2,6):  Bv_ii = beta_i*max(B_ii(w))  
+%     DOF (i = 1,2,6):  Bv_ii = beta_i * max(B_ii(w))  
 %
 %     DOF 4          :  Bv_44 = b*B44(w4) where b is the roll 
 %                               amplification factor at w4 satisfying:
@@ -23,33 +22,41 @@ function Bv = viscous(vessel)
 %         
 % Author:    Thor I. Fossen
 % Date:      2005-05-26
-% Revisions: 
-%
-% _________________________________________________________________________
-%
-% MSS HYDRO is a Matlab toolbox for guidance, navigation and control.
-% The toolbox is part of the Marine Systems Simulator (MSS).
+% Revisions: 2021-03-07 Major improvements, new formulas for viscous damping
 
 [dof1,dof2,nfreqsB,nspeeds] = size(vessel.B);  % potential damping dimensions
 w      = vessel.freqs;
-nfreqs = length(w);                     % number of freqs for Bv
+nfreqs = length(w);                            % number of freqs for Bv
 
-%--------------------------------------------------------------------------
 disp(' ')
 disp('-----------------------------------------')
 disp('VISCOUS DAMPING')
 disp('-----------------------------------------')
-disp('Skin friction in surge, sway and yaw:')
-disp('   B_v(w) = k*max(B_p)  (deault k = 0.2)')   
+disp('Viscous damping formula in surge, sway and yaw: Bv(w) = k * max(B(w))')   
+visc_input1 = input('Damping factors at w = 0 for DOFs 1,2,6 (default: k = [0.2 0.2 0.2]) : ');
+
+% test for default values
+if isempty(visc_input1)
+    beta_visc = [0.2 0.2 0.2];
+else
+    beta_visc = visc_input1;    
+end
+
 disp(' ');
-disp('Viscous roll damping B44(w4) = B44p(w4) + B44v at resonance frequency w4.')
-disp('   The relative damping ratio zeta4 is specified such that:')
-disp('   B44(w4) = 2*zeta4*w4*(Ix+A44(w4))') 
+disp('Roll damping: B44_total(w4) = B44(w4) + B44v(w4) at w4.')
+disp('The relative damping ratio zeta4 is specified such that:')
+disp('B44_total(w4) = 2 * zeta4 * w4 * (Ix + A44(w4)).') 
+
 winf = length(w);  % inf frequency
 
 % period DOF 4
-[T,zeta] = DPperiods(vessel,1);
-w4 = 2*pi/T(4);
+MRB = vessel.MRB;
+for k = 1:nfreqs
+    A(:,:,k) = reshape(vessel.A(:,:,k,1),6,6,1);
+end
+G   = reshape(vessel.C(:,:,nfreqs,1),6,6);
+w_0 = sqrt(G(4,4)/(MRB(4,4)+A(4,4))); 
+w4 = natfrequency(vessel,4,w_0,1);
  
 A44 = interp1(vessel.freqs,reshape(vessel.A(4,4,:,1),1,length(vessel.freqs)),w4);
 B44 = interp1(vessel.freqs,reshape(vessel.B(4,4,:,1),1,length(vessel.freqs)),w4);
@@ -79,27 +86,20 @@ end
 
 % increase roll damping
 disp(' ')
-disp(sprintf('Relative damping ratio in roll at resonance w4 = %3.2f rads/s is zeta4 = %5.4f',w4,zeta(4)));   
-disp(sprintf('This is increased to %3.2f by adding viscous damping B44v = %0.3g',zeta_new,B44v_def));
+fprintf('Relative damping ratio in roll at w4 = %3.2f rads/s is zeta4 = %5.4f\n',w4,zeta(4));   
+fprintf('This can be increased to %3.2f by adding viscous damping B44v = %0.3g\n',zeta_new,B44v_def);
 disp(' ')
-disp(sprintf('Potential damping:        B44p(w4) = %0.3g',B44));
-visc_input1 = input(sprintf('Additional viscous ROLL damping (default: B44v(w4) = %0.3g)      : ',B44v_def));
-visc_input2 = input('Viscous damping at w = 0 for DOF 1,2,6  (default: k = [0.2 0.2 0.2]) : ');
+fprintf('Potential damping: B44(w4) = %0.3g\n',B44);
+visc_input2 = input(sprintf('Additional viscous ROLL damping (default: B44v(w4) = %0.3g)       : ',B44v_def));
 
 % test for default values
-if isempty(visc_input1)
+if isempty(visc_input2)
     B44v = B44v_def;  % default
 else
-    B44v = visc_input1;
+    B44v = visc_input2;
 end
 
-if isempty(visc_input2)
-    beta_visc = [0.2 0.2 0.2];
-else
-    beta_visc = visc_input2;    
-end
-
-% skin friction factors in surge, sway and yaw
+% viscous damping factors in surge, sway and yaw
 B11_max = beta_visc(1)*max(reshape(vessel.B(1,1,:,1),1,nfreqsB));
 B22_max = beta_visc(2)*max(reshape(vessel.B(2,2,:,1),1,nfreqsB));
 B66_max = beta_visc(3)*max(reshape(vessel.B(6,6,:,1),1,nfreqsB));
@@ -107,9 +107,15 @@ B66_max = beta_visc(3)*max(reshape(vessel.B(6,6,:,1),1,nfreqsB));
 % build diagonal viscous damping matrix for all speeds
 Bv =  diag([B11_max B22_max 0 B44v 0 B66_max]);
 
-% compute time constants/periods with viscous damping Bv
-vessel_new = vessel;
+% compute time constants/periods with and without viscous damping Bv
+vessel_new = vessel; 
 vessel_new.Bv = Bv;
+disp(' ');
+disp('VISCOUS DAMPING:')
+DPperiods(vessel_new,1);
+
+vessel_new.Bv = 0*Bv;
+disp('DAMPING FROM HYDRODYNAMIC CODE:')
 DPperiods(vessel_new,1);
 
 disp(' ');
