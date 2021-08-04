758f34779d0d609bfffd799fef149a19fc1f6de4
Thor I. Fossen <thor.fossen@ntnu.no>
2021-08-04 21:01:13 +0200

    Update SIMrig.m

--- VESSELS/SIMrig.m
+++ VESSELS/SIMrig.m
@@ -1,40 +1,34 @@
-% SIMrig    User editable script for simulation of a semisubmersible with
-% two pontoons and four legs. The data is generated using rig.m, which
-% calls data_rig.m. 
+% SIMrig   User editable script for simulation of a semisubmersible with
+% two pontoons and four columns. The data is generated using the script
+% rig.m, which calls data_rig.m. 
 %
-%   m =  27 162 500 (mass)
-%   H_p = 13.0 (pontoon height)
-%   B_p = 12.0 (pontoon width)
-%   L_p = 84.6 (pontoon length)
-%   r_CG = [-0.5 0 -19.5]', center of gravity with respect to the CO
+%   m =  27 162 500 kg (mass)
+%   H_p = 13.0 m (pontoon height)
+%   B_p = 12.0 m (pontoon width)
+%   L_p = 84.6 m (pontoon length)
+%   r_CG = [-0.5 0 -19.5]', CG with respect to the CO
 %
 % Author:      Thor I. Fossen
 % Date:        2019-03-12
-% Revisions:   2020-10-22  retuning of PID controller
+% Revisions:   2020-10-22 retuning of PID controller
+%              2020-08.04 major update
 
-clear all
+clearvars
 
 f = 20;              % sampling frequency
 h = 1/f;             % sampling time
 N = round(1000*f);   % number of samples
 
-w_n = 0.02;          % Bandwidth
-Kp = w_n^2;          % Proportional gain 
-Kd = 0;              % Derivative  gain 
-Ki = 0.1 * Kp * w_n; % Integral  gain 
-
-% Regular wave data
-beta_wave = 60;  % wave direction 
-A_wave = 2000000; % wave gain 
-X_wave = A_wave * cos(beta_wave * pi/180);
-Y_wave = A_wave * sin(beta_wave * pi/180);
+% environmnetal data
+A_wave = 5000000 * [0.5 0.1 0 100 10 10]';      % wave gain 
+nu_c = [-0.3 -0.2 0 0 0 0]';                    % ocean current velocities
 
 % initial values
 eta = zeros(6,1);   % generalized position
-nu  = zeros(6,1);   % generalized velcoity
-z_3 = zeros(3,1);   % integral state
+nu  = zeros(6,1);   % generalized velocity
+z3 = zeros(3,1);    % integral state
 
-% Semi-sub model
+% Semisub model matrices
 MRB = 1.0e+10 * [
     0.0027         0         0         0   -0.0530         0
     0    0.0027         0    0.0530         0   -0.0014
@@ -68,14 +62,19 @@ G = 1.0e+10 * [
     0         0         0         0         0         0 ];
 
 M = MRB + MA;    
-Minv = inv(M);
+Minv = inv(M);  
 
-M_33 = [M(1,1) M(1,2) M(1,6)          % 3-DOF inertia mattrix
-        M(2,1) M(2,2) M(2,6)
-        M(6,1) M(6,2) M(6,6) ];
+% Nonlinear PID controller  
+M3 = diag( [M(1,1), M(2,2), M(6,6) ]);  % 3-DOF matrices 
+D3 = diag( [D(1,1), D(2,2), D(6,6) ]);  
+G3 = diag( [G(1,1), G(2,2), G(6,6) ]); 
 
-    
-% *************** MAIN SIMULATION LOOP ************************
+w_n = 0.1;                          % Bandwidth
+Kp = M3 * w_n^2  * eye(3) - G3;     % Proportional gain 
+Kd = 2 * w_n * M3 - D3;             % Derivative  gain 
+Ki = (w_n / 10) * Kp;               % Integral  gain     
+
+%% *************** MAIN SIMULATION LOOP ************************
 for i=1:N+1
     t = (i-1)*h;                   % simulation time in seconds
 
@@ -84,26 +83,24 @@ for i=1:N+1
     J = eulerang(phi,theta,psi); 
     R = Rzyx(phi,theta,psi); 
     
-    % 3-DOF nonlinear DP control system
-    eta_3 = [eta(1) eta(2) eta(6)]' ;
-    nu_3  = [nu(1) nu(2) nu(6)]';   
-    tau_3 = -M_33 * (Kp * R' * eta_3 - Kd * nu_3 + Ki * R' * z_3); 
-    tau = [tau_3(1) tau_3(2) 0 0 0 tau_3(3)]';
+    % 3-DOF nonlinear DP control system (no wave filtering)
+    eta3 = [eta(1) eta(2) ssa( eta(6) )]' ;
+    nu3  = [nu(1) nu(2) nu(6)]';   
+    tau3 = -R' * (Kp * eta3 + Ki * z3) - Kd * nu3; 
+    tau = [tau3(1) tau3(2) 0 0 0 tau3(3)]';
     
-    % rig model
-    t_wave = zeros(6,1);
-    t_wave(1) = X_wave * sin(0.2 * t);
-    t_wave(2) = Y_wave * sin(0.2 * t);   
+    % equations of motion
+    t_wave = A_wave * sin(0.2 * t);
     etadot = J * nu;
-    nudot = Minv * (tau - D * nu + t_wave);
+    nudot = Minv * (tau - D * (nu-nu_c) - G * eta + t_wave);
     
     % store data for presentation
     xout(i,:) = [t, eta', nu'];
     
     % numerical integration
-    eta = euler2(etadot, eta, h);             % Euler integration
+    eta = euler2(etadot, eta, h);             % Euler's method
     nu  = euler2(nudot, nu, h);     
-    z_3 = euler2(eta_3, z_3, h);
+    z3  = euler2(eta3, z3, h);
 
 end
 % *************** END SIMULATION LOOP ************************
@@ -123,14 +120,21 @@ p     = xout(:,11) * 180/pi;
 q     = xout(:,12) * 180/pi;
 r     = xout(:,13) * 180/pi; 
 
-% plots
+%% plots
 figure(1)
-plot(y,x)
-grid,axis('equal'),xlabel('East'),ylabel('North'),title('Semi-sub position (m)')
+subplot(211)
+plot(y,x,'linewidth',2)
+grid,axis('equal'),xlabel('East'),ylabel('North'),title('Semisub xy-plot (m)')
+subplot(212)
+plot(t,x,t,y,'linewidth',2),title('Semisub positions (m)'),
+legend('x position','y position'),grid
 
 figure(2)
-subplot(311),plot(t,phi),xlabel('time (s)'),title('roll angle \phi (deg)'),grid
-subplot(312),plot(t,theta),xlabel('time (s)'),title('pitch angle \theta (deg)'),grid
-subplot(313),plot(t,psi),xlabel('time (s)'),title('yaw angle \psi (deg)'),grid
+subplot(311),plot(t,phi,'linewidth',2),xlabel('time (s)')
+title('roll angle \phi (deg)'),grid
+subplot(312),plot(t,theta,'linewidth',2),xlabel('time (s)')
+title('pitch angle \theta (deg)'),grid
+subplot(313),plot(t,psi,'linewidth',2),xlabel('time (s)')
+title('yaw angle \psi (deg)'),grid
 
 
