41cf22c2375dfbc7e36b545bfe66f5d2f60263d6
Thor I. Fossen <thor.fossen@ntnu.no>
2021-08-09 19:49:51 +0200

    Update flat2llh.m

--- GNC/flat2llh.m
+++ GNC/flat2llh.m
@@ -3,7 +3,7 @@ function [l,mu,h] = flat2llh(x,y,z,l0,mu0,h_ref)
 % latitude mu (rad) and height h (m) for the NED coordinates (x,y,z) using
 % a flat Earth coordinate system defined by the WGS-84 ellipsoid.  
 % The flat Earth coordinate origin is located  at (l0, mu0) with reference 
-% height h_ref in meters above the surface of the Earth. Both h and h_ref 
+% height h_ref in meters above the surface of the elipsoid. Both h and h_ref 
 % are positive upwards, while z is postive downwards (NED).
 %
 % Author:    Thor I. Fossen
