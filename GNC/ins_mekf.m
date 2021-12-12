704d1d97acc72f1e4df1a620e948e84275c4a0c5
Thor I. Fossen <thor.fossen@ntnu.no>
2021-12-12 20:55:00 +0100

    Update ins_mekf.m

--- GNC/ins_mekf.m
+++ GNC/ins_mekf.m
@@ -42,6 +42,7 @@ function [x_ins, P_prd] = ins_mekf(...
 % Author:    Thor I. Fossen
 % Date:      26 Mar 2020
 % Revisions: 29 Nov 2020, bugfix - removed magnetometer projections
+% Revisions: 12 Dec 2021 - replaced Euler's method with exact discretization
 
 % Bias time constants (user specified)
 T_acc = 1000; 
@@ -151,10 +152,11 @@ end
 P_prd = Ad * P_hat * Ad' + Ed * Qd * Ed';
 
 % INS propagation: x_ins[k+1]
-p_ins = p_ins + h * v_ins;
-v_ins = v_ins + h * (R * f_ins + g_n);
-q_ins = q_ins + h * T * w_ins;
-q_ins = q_ins / sqrt(q_ins' * q_ins); % normalization
+a_ins = R * f_ins + g_n;                            % linear acceleration
+p_ins = p_ins + h * v_ins + h^2/2 * a_ins;          % exact discretization
+v_ins = v_ins + h * a_ins;                          % exact discretization
+q_ins = q_ins + h * T * w_ins;                      % Euler's method
+q_ins = q_ins / sqrt(q_ins' * q_ins);               % normalization
 
 x_ins = [p_ins; v_ins; b_acc_ins; q_ins; b_ars_ins];
 
