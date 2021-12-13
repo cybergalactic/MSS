981dee684cee43d63edeaaddb909ef7d1f1aabc1
Thor I. Fossen <thor.fossen@ntnu.no>
2021-12-13 09:26:33 +0100

    Update Tquat.m

--- GNC/Tquat.m
+++ GNC/Tquat.m
@@ -1,14 +1,37 @@
-function Tq = Tquat(q)
-% Tq = TQUAT(q) computes the quaternion transformation matrix for attitude
+function T = Tquat(u)
+% Tq = Tquat(q) computes the quaternion transformation matrix Tq of 
+%   dimension for 4 x 3 for attitude such that q_dot = Tq * w 
+% Tw = Tquat(w) computes the quaternion transformation matrix Tw of
+%   dimension 4 x 4 for attitude such that q_dot = Tw * q
 %
-% Author:   Thor I. Fossen
-% Date:     10th September 2010
-% Revisions: 
-
-eta  = q(1); eps1 = q(2); eps2 = q(3); eps3 = q(4); 
+% The standard inout is the unit quaternion q = [ eta eps1 eps2 eps3 ]' of 
+% dimenison 4. If the angular rates w ≈ [ p q r ]' of dimension 3 are used 
+% as input, the alternative matrix Tw is returned.
+%
+% Author:    Thor I. Fossen
+% Date:      10 Sep 2010 - core algorithm   q_dot = Tquat(q) w
+% Revisions: 13 Dec 2021 - added option for q_dot = Tquat(w) q
  
-Tq = 0.5*[...
-   -eps1 -eps2 -eps3        
-    eta  -eps3  eps2
-    eps3  eta  -eps1
-   -eps2  eps1  eta   ];
+if (length(u) == 4)                % T = Tq and q = u
+    
+    eta  = u(1); eps1 = u(2); eps2 = u(3); eps3 = u(4);
+    
+    T = 0.5 * [...
+        -eps1 -eps2 -eps3        
+         eta  -eps3  eps2
+         eps3  eta  -eps1
+        -eps2  eps1  eta   ];
+    
+elseif (length(u) == 3)            % T = Tw and w = u
+    
+    w = [u(1) u(2) u(3)]';
+    
+    T = 0.5 * [...
+        0  -w'
+        w  -Smtrx(w) ];
+    
+else
+    
+    error('input must be of dim. 4 (unit quaternion) or dim. 3 (angular rates)') 
+    
+end
