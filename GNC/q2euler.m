df5f4096f734da93a5f630aa042b65216503d4dc
Thor I. Fossen <thor.fossen@ntnu.no>
2021-12-15 13:00:24 +0100

    Update q2euler.m

--- GNC/q2euler.m
+++ GNC/q2euler.m
@@ -1,14 +1,14 @@
 function [phi,theta,psi] = q2euler(q)
-% [phi,theta,psi] = Q2EULER(q) computes the Euler angles from the unit 
-% quaternions q = [eta eps1 eps2 eps3]
+% [phi,theta,psi] = q2euler(q) computes the Euler angles from the unit 
+% quaternion q = [eta eps1 eps2 eps3]
 %
-% Author:   Thor I. Fossen
+% Author:    Thor I. Fossen
 % Date:      2001-06-14  
-% Revisions: 2007-09-03  Test for singular solution theta = +-90 deg has been improved
+% Revisions: 2021-12-15 Added test to avoid abs(asin) > 1, i.e. norm(q) > 1
 
+q = q / norm(q);      % normalize q, handle round-off errors 
 R = Rquat(q);
-if abs(R(3,1))>1.0, error('solution is singular for theta = +- 90 degrees'); end
- 
+
 phi   = atan2(R(3,2),R(3,3));
 theta = -asin(R(3,1));
 psi   = atan2(R(2,1),R(1,1));
