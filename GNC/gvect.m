e91ae18a277c937e479e4ec1eb748e0ccf661972
Thor I. Fossen <thor.fossen@ntnu.no>
2021-12-16 09:36:26 +0100

    Update gvect.m

--- GNC/gvect.m
+++ GNC/gvect.m
@@ -1,18 +1,20 @@
 function g = gvect(W,B,theta,phi,r_bg,r_bb)
 % g = gvect(W,B,theta,phi,r_bg,r_bb) computes the 6x1 vector of restoring 
-% forces about an arbitrarily point CO for a submerged body. For floating 
-% vessels, use Gmtrx.m
+% forces about an arbitrarily point CO for a submerged body. Use 
+% g = gRvect(W,B,R,r_bg,r_bb) if the rotation matrix R is the desired input. 
+% For floating vessels, use Gmtrx.m.
 % 
-% Inputs:  W, B: weight and buoyancy
-%          phi,theta: roll and pitch angles
-%          r_bg = [x_g y_g z_g]: location of the CG with respect to the CO
-%          r_bb = [x_b y_b z_b]: location of the CB with respect to th CO
+% Inputs: 
+%  W, B: weight and buoyancy (kg)
+%  phi,theta: roll and pitch angles (rad)
+%  r_bg = [x_g y_g z_g]: location of the CG with respect to the CO (m)
+%  r_bb = [x_b y_b z_b]: location of the CB with respect to th CO (m)
 %
 % Author:    Thor I. Fossen
-% Date:      14th June 2001
-% Revisions: 20 oct 2008 improved documentation
-%            22 sep 2013 corrected sign error on last row (Mohammad Khani)
-%            24 Apr 2021, minor updates
+% Date:      14 Jun 2001
+% Revisions: 20 Oct 2008 new description
+%            22 Sep 2013 corrected sign error on last row (Mohammad Khani)
+%            16 Dec 2021 minor updates of the documentation
 
 sth  = sin(theta); cth  = cos(theta);
 sphi = sin(phi);   cphi = cos(phi);
@@ -23,4 +25,6 @@ g = [...
   -(W-B) * cth * cphi
   -(r_bg(2)*W-r_bb(2)*B) * cth * cphi + (r_bg(3)*W-r_bb(3)*B) * cth * sphi
    (r_bg(3)*W-r_bb(3)*B) * sth        + (r_bg(1)*W-r_bb(1)*B) * cth * cphi
-  -(r_bg(1)*W-r_bb(1)*B) *cth * sphi  - (r_bg(2)*W-r_bb(2)*B) * sth       ];
+  -(r_bg(1)*W-r_bb(1)*B) * cth * sphi - (r_bg(2)*W-r_bb(2)*B) * sth       ];
+
+end
