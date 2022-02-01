e54e027c76570d806b53c8ae657658adcc711006
Thor I. Fossen <thor.fossen@ntnu.no>
2022-02-01 11:04:34 +0100

    Update remus100.m

--- VESSELS/remus100.m
+++ VESSELS/remus100.m
@@ -35,7 +35,7 @@ function [xdot,U] = remus100(x,ui,Vc,betaVc)
 %            10 Oct 2021, Increased the parasetic drag (alpha = 0) to 0.2
 %            21 Oct 2021, implay61.m is called using the relative velocity
 %            30 Dec 2021, Added the time derivative of the current velocity
-%            30 Jan 2022, Updated lift and drag forces
+%            01 Feb 2022, Updated lift and drag forces
 
 % Check of input and state dimensions
 if (length(x) ~= 12),error('x-vector must have dimension 12!'); end
@@ -65,7 +65,7 @@ nu_c = [u_c v_c 0 0 0 0]';                  % ocean current velocities
 Dnu_c = [nu(6)*v_c -nu(6)*u_c 0 0 0 0]';    % time derivative of nu_c
 
 % Amplitude saturation of control signals
-max_ui = [30*pi/180 50*pi/180  1500/60]';   % deg, deg, rps
+max_ui = [30*pi/180 70*pi/180  1500/60]';   % deg, deg, rps
 
 % Relative velocities, speed and angle of attack
 nu_r = nu - nu_c;                                 % relative velocity
@@ -91,12 +91,12 @@ D_prop = 0.055;          % propeller diameter
 KT = 0.4739;             % [KT, KQ] = wageningen(0,1,0.8,3)
 KQ = 0.0730;
 
-% Tail rudder
+% Tail rudder (single)
 CL_delta_r = 0.5;        % rudder lift coefficient
 A_r = 0.10 * 0.05;       % rudder area (m2)
 x_r = -a;                % rudder x-position (m)
 
-% Stern plane
+% Stern plane (double)
 CL_delta_s = 0.7;        % stern-plane lift coefficient
 A_s = 2 * 0.10 * 0.05;   % stern-plane area (m2)
 x_s = -a;                % stern-plane z-position (m)
@@ -108,12 +108,12 @@ zeta4 = 0.3;             % relative damping ratio in roll
 zeta5 = 0.8;             % relative damping ratio in pitch
 T6 = 5;                  % time constant in yaw (s)
 
-% mass and added mass
+% Rigid-body mass and hydrodynamic added mass
 [MRB,CRB] = spheroid(a,b,nu(4:6),r_bg);
 [MA,CA] = imlay61(a, b, nu_r, r44);
 
-% nonlinear quadratic velocity terms in pitch and yaw (Munk moments) 
-% are cancelled since only linear damping is used
+% Nonlinear quadratic velocity terms in pitch and yaw (Munk moments) 
+% are set to zero since only linear damping is used
 CA(5,1) = 0;   
 CA(5,4) = 0;
 CA(6,1) = 0;
@@ -123,7 +123,7 @@ M = MRB + MA;
 C = CRB + CA;
 m = MRB(1,1); W = m*g_mu; B = W;
 
-% dissipative forces and moments
+% Dissipative forces and moments
 D = Dmtrx([T1 T2 T6],[zeta4 zeta5],MRB,MA,[W r_bg' r_bb']);
 D(1,1) = D(1,1) * exp(-3*U_r);   % vanish at high speed where crossflow
 D(2,2) = D(2,2) * exp(-3*U_r);   % drag and lift/drag dominates
@@ -132,7 +132,7 @@ D(6,6) = D(6,6) * exp(-3*U_r);
 tau_liftdrag = forceLiftDrag(D_auv,S,CD_0,alpha,U_r);
 tau_crossflow = crossFlowDrag(L_auv,D_auv,D_auv,nu_r);
 
-% restoring forces and moments
+% Restoring forces and moments
 g = gvect(W,B,eta(5),eta(4),r_bg,r_bb);
 
 % kinematics
@@ -143,21 +143,25 @@ if (abs(delta_r) > max_ui(1)), delta_r = sign(delta_r) * max_ui(1); end
 if (abs(delta_s) > max_ui(2)), delta_s = sign(delta_s) * max_ui(2); end
 if (abs(n)       > max_ui(3)), n = sign(n) * max_ui(3); end
 
-% control forces and moments
+% Control forces and moments
 X_prop = rho * D_prop^4 * KT * abs(n) * n;  % propeller thrust 
 K_prop = rho * D_prop^5 * KQ * abs(n) * n;  % propeller-induced roll moment
 
-% rudder and stern-plane drag
-X_r = -0.5 * rho * U_r^2 * A_r * CL_delta_r * delta_r^2; 
-X_s = -0.5 * rho * U_r^2  * A_s * CL_delta_s * delta_s^2;
+% Horizontal- and vertical-plane relative speed
+U_rh = sqrt( nu_r(1)^2 + nu_r(2)^2 );  
+U_rv = sqrt( nu_r(1)^2 + nu_r(3)^2 );  
 
-% rudder sway force 
-Y_r = -0.5 * rho * U_r^2 * A_r * CL_delta_r * delta_r;
+% Rudder and stern-plane drag
+X_r = -0.5 * rho * U_rh^2 * A_r * CL_delta_r * delta_r^2; 
+X_s = -0.5 * rho * U_rv^2 * A_s * CL_delta_s * delta_s^2;
 
-% stern-plane heave force
-Z_s = -0.5 * rho * U_r^2 * A_s * CL_delta_s * delta_s;
- 
-% generalized force vector
+% Rudder sway force 
+Y_r = -0.5 * rho * U_rh^2 * A_r * CL_delta_r * delta_r;
+
+% Rtern-plane heave force
+Z_s = -0.5 * rho * U_rv^2 * A_s * CL_delta_s * delta_s;
+
+% Generalized force vector
 tau = zeros(6,1);                                
 tau(1) = X_prop + X_r + X_s;
 tau(2) = Y_r;
@@ -166,7 +170,7 @@ tau(4) = K_prop;
 tau(5) = x_s * Z_s;
 tau(6) = x_r * Y_r;
 
-% state-space model
+% State-space model
 xdot = [...
   Dnu_c + M  \ (tau + tau_liftdrag + tau_crossflow - C * nu_r - D * nu_r  - g)
   J * nu ];
