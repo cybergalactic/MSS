009b8b51ce2c56e546904219eeff12e5603af078
Thor I. Fossen <thor.fossen@ntnu.no>
2022-05-07 10:43:43 +0200

    Update quatern.m

--- GNC/quatern.m
+++ GNC/quatern.m
@@ -1,19 +1,17 @@
 function [J,J1,J2] = quatern(q)
-% [J,J1,J2] = QUATERN(q) computes the quaternion transformation matrices
+% [J,Rq,Tq] = quatern(q) computes the quaternion transformation matrices
 %
-% Author:   Thor I. Fossen
-% Date:     14th June 2001
-% Revisions: 6 October 2001, T I. Fossen - eta as first element in q 
+%  J = [ Rq     0
+%           0  Tq ]
+%
+% where J1 = Rq and J2 = Tq, see Rquat.m and Tquat.m.
+%
+% Author:    Thor I. Fossen
+% Date:      14 Jun 2001
+% Revisions: 07 May 2022  - added calls to Rquat and Tquat
 
-eta  = q(1); eps1 = q(2); eps2 = q(3); eps3 = q(4); 
- 
 J1 = Rquat(q);
- 
-J2 = 0.5*[...
-   -eps1 -eps2 -eps3        
-    eta  -eps3  eps2
-    eps3  eta  -eps1
-   -eps2  eps1  eta   ];
+J2 = Tquat(q);
  
 J = [ J1  zeros(3,3);
       zeros(4,3) J2 ];
