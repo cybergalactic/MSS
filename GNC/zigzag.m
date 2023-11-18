a42ad8431dcb1d5421d66b36a325bd505cd6b42d
Thor I. Fossen <thor.fossen@ntnu.no>
2023-11-18 14:17:56 +0100

    Update zigzag.m

--- GNC/zigzag.m
+++ GNC/zigzag.m
@@ -1,16 +1,18 @@
-function [t,u,v,r,x,y,psi,U] = zigzag(ship,x,ui,t_final,t_rudderexecute,h,maneuver)
-% ZIGZAG      [t,u,v,r,x,y,psi,U] = zigzag(ship,x,ui,t_final,t_rudderexecute,h,maneuver)
-%             performs the zig-zag maneuver, see ExZigZag.m
+function [t,u,v,r,x,y,psi,U] = zigzag(ship,x,ui,t_final,t_rudderexecute, ...
+    h,maneuver)
+% [t,u,v,r,x,y,psi,U] = zigzag(ship,x,ui,t_final,t_rudderexecute,h,maneuver)
+% performs the zigzag maneuver, see ExZigZag.m
 %
 % Inputs :
-% 'ship'          = ship model. Compatible with the models under .../gnc/VesselModels/
-% x               = initial state vector for ship model
-% ui              = [delta,:] where delta=0 and the other values are non-zero if any
-% t_final         = final simulation time
-% t_rudderexecute = time control input is activated
-% h               = sampling time
-% maneuver        = [rudder angle, heading angle]. Default 20-20 deg that is: maneuver = [20, 20] 
-%                    rudder is changed to maneuver(1) when heading angle is larger than maneuver(2)
+% 'ship':          ship model. Compatible with the models under .../VESSELS
+% x:               initial state vector for ship model
+% ui:              [delta,:] where delta=0 and the other values are nonzero if any
+% t_final:         final simulation time
+% t_rudderexecute: time for which control input is activated
+% h:               sampling time
+% maneuve          [rudder angle, heading angle]. Default 20-20 deg, i.e. 
+%                  maneuver = [20, 20] rudder is changed to maneuver(1) when 
+%                  heading angle is larger than maneuver(2)
 %
 % Outputs :
 % t               = time vector
@@ -18,9 +20,10 @@ function [t,u,v,r,x,y,psi,U] = zigzag(ship,x,ui,t_final,t_rudderexecute,h,maneuv
 %
 % Author:    Thor I. Fossen
 % Date:      22th July 2001
-% Revisions: 15th July 2002, switching logic has been modified to handle arbitrarily maneuvers
+% Revisions: 15th July 2002, switching logic has been modified to handle 
+%                            arbitrarily maneuvers
 
-if nargin>7 | nargin<6, error('number of inputs must be 6 or 7'); end
+if nargin>7 || nargin<6, error('number of inputs must be 6 or 7'); end
 if t_final<t_rudderexecute, error('t_final must be larger than t_rudderexecute'); end
 if nargin==6, maneuver = [20,20]; end
 
@@ -31,46 +34,50 @@ disp('Simulating...')
 
 u_ship=ui;
 
-for i=1:N+1,
+for i=1:N+1
     time = (i-1)*h;
     
     psi = x(6)*180/pi;
     r   = x(3);
     
-    if round(time)==t_rudderexecute, 
-        u_ship(1)=maneuver(1)*pi/180; 
+    if round(time) == t_rudderexecute 
+        u_ship(1) = deg2rad( maneuver(1) ); 
     end
     
-    if round(time) > t_rudderexecute, 
-        if (psi>=maneuver(2) & r>0),
-            u_ship(1) = -maneuver(1)*pi/180; 
-        elseif (psi<=-maneuver(2) & r<0),
-            u_ship(1) = maneuver(1)*pi/180;            
+    if round(time) > t_rudderexecute
+        if ( psi >= maneuver(2) && r > 0 )
+            u_ship(1) = -deg2rad( maneuver(1) ); 
+        elseif (psi <= -maneuver(2) && r < 0 )
+            u_ship(1) = deg2rad( maneuver(1) );            
         end   
     end
  
-    [xdot,U] = feval(ship,x,u_ship);       % ship model
+    [xdot,U] = feval(ship,x,u_ship);          % ship model
     
     xout(i,:) = [time,x(1:6)',U,u_ship(1)];  
     
-    x = euler2(xdot,x,h);                     % Euler integration
+    x = euler2(xdot,x,h);                     % Euler's method
 end
 
 % time-series
-t     = xout(:,1);
-u     = xout(:,2); 
-v     = xout(:,3);         
-r     = xout(:,4)*180/pi; 
-x     = xout(:,5);
-y     = xout(:,6);
-psi   = xout(:,7)*180/pi;
-U     = xout(:,8);
-delta_c = xout(:,9)*180/pi;
+t       = xout(:,1);
+u       = xout(:,2); 
+v       = xout(:,3);         
+r       = rad2deg( xout(:,4) ); 
+x       = xout(:,5);
+y       = xout(:,6);
+psi     = rad2deg( xout(:,7) );
+U       = xout(:,8);
+delta_c = rad2deg( xout(:,9) );
 
 % plots
 figure(1)
 plot(x,y,'linewidth',2),grid,axis('equal'),xlabel('x-position'),ylabel('y-position')
 title('Zig-zag test')
+set(findall(gcf,'type','line'),'linewidth',2)
+set(findall(gcf,'type','text'),'FontSize',14)
+set(findall(gcf,'type','legend'),'FontSize',14)
+
 figure(2)
 subplot(211),plot(t,psi,'linewidth',2)
 hold on
@@ -79,3 +86,6 @@ hold off
 xlabel('time (s)'),title('yaw angle \psi (deg)'),grid
 legend('\psi','\delta_c')
 subplot(212),plot(t,U),xlabel('time (s)'),title('speed U (m/s)'),grid
+set(findall(gcf,'type','line'),'linewidth',2)
+set(findall(gcf,'type','text'),'FontSize',14)
+set(findall(gcf,'type','legend'),'FontSize',14)
