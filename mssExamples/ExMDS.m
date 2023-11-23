d1dd383900b17ba41d6749f96a4e667d9930a22a
Thor I. Fossen <thor.fossen@ntnu.no>
2023-11-23 21:13:52 +0100

    Update ExMDS.m

--- mssExamples/ExMDS.m
+++ mssExamples/ExMDS.m
@@ -1,4 +1,5 @@
-% ExMD       Plots the step response of a 2nd-order mass-damper system
+% ExMD       Plots the step response of a 2nd-order mass-damper-spring system
+%
 % Author:    Thor I. Fossen
 % Date:      16th June 2001
 % Revisions: 
@@ -7,17 +8,21 @@ wn = 1;
 
 subplot(211)
 t = 0:0.01:20;
-z = 0.5; sys = tf([wn*wn],[1 2*z*wn wn*wn]); step(sys,t)
+z = 0.5; sys = tf(wn*wn,[1 2*z*wn wn*wn]); step(sys,t)
 hold on
-z = 1.0; sys = tf([wn*wn],[1 2*z*wn wn*wn]); step(sys,t)
-z = 2.0; sys = tf([wn*wn],[1 2*z*wn wn*wn]); step(sys,t)
+z = 1.0; sys = tf(wn*wn,[1 2*z*wn wn*wn]); step(sys,t)
+z = 2.0; sys = tf(wn*wn,[1 2*z*wn wn*wn]); step(sys,t)
 hold off
 legend('\zeta = 0.5','\zeta = 1.0','\zeta = 2.0')
 
 subplot(212)
 t = 0:0.01:50;
-z = 0.1; sys = tf([wn*wn],[1 2*z*wn wn*wn]); step(sys,t)
+z = 0.1; sys = tf(wn*wn,[1 2*z*wn wn*wn]); step(sys,t)
 hold on
-sys = tf([wn*wn],[1 0 wn*wn]); step(sys,t)
+sys = tf(wn*wn,[1 0 wn*wn]); step(sys,t)
 hold off
 legend('\zeta = 0.1','\zeta = 0.0')
+
+set(findall(gcf,'type','line'),'linewidth',2)
+set(findall(gcf,'type','text'),'FontSize',14)
+set(findall(gcf,'type','legend'),'FontSize',14)
