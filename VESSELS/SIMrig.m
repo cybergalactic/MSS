c291c6ac80029ba5c0367c123ccb2de48dc96776
Thor I. Fossen <thor.fossen@ntnu.no>
2024-03-27 17:14:23 +0100

    Update SIMrig.m

--- VESSELS/SIMrig.m
+++ VESSELS/SIMrig.m
@@ -1,34 +1,42 @@
 % SIMrig   User editable script for simulation of a semisubmersible with
-% two pontoons and four columns. The data is generated using the script
-% rig.m, which calls data_rig.m. 
+% two pontoons and four columns. The model matrices MRB, MA, D and G are
+% generated using the script 'rig.m', which calls the data file 'data_rig.m'. 
 %
-%   m =  27 162 500 kg (mass)
+% Main characteristics:
+%   m = 27 162 500 kg (mass)
 %   H_p = 13.0 m (pontoon height)
 %   B_p = 12.0 m (pontoon width)
 %   L_p = 84.6 m (pontoon length)
 %   r_CG = [-0.5 0 -19.5]', CG with respect to the CO
 %
-% Author:      Thor I. Fossen
-% Date:        2019-03-12
-% Revisions:   2020-10-22 retuning of PID controller
-%              2020-08.04 major update
+% Author:     Thor I. Fossen
+% Date:       2019-03-12
+% Revisions:  2020-10-22 Retuning the PID controller
+%             2020-08-04 Major update
+%             2024-03-27 Using PIDnonlinearMIMO.m as new DP control law
 
-clearvars
+clearvars;
+clear PIDnonlinearMIMO;         % clear persistent integrator state
 
-f = 20;              % sampling frequency
-h = 1/f;             % sampling time
-N = round(1000*f);   % number of samples
+f = 20;                         % sampling frequency
+h = 1/f;                        % sampling time
+N = round(200 * f);             % number of samples
 
-% environmnetal data
-A_wave = 5000000 * [0.5 0.1 0 100 10 10]';      % wave gain 
-nu_c = [-0.3 -0.2 0 0 0 0]';                    % ocean current velocities
+% Environmnetal data
+nu_c = [-1.0 -0.2 0 0 0 0]';    % ocean current velocities
 
-% initial values
-eta = zeros(6,1);   % generalized position
-nu  = zeros(6,1);   % generalized velocity
-z3 = zeros(3,1);    % integral state
+% Nonlinear PID controller  
+psi_d = deg2rad(10);            % Heading angle step
+eta_ref = [0 0 psi_d]';         % DP system setpoints
+wn = 0.2;                       % closed-loop natural frequency (rad/s)
+zeta = 1.0;                     % closed-loop relative damping factor  
+T_f = 5;                        % low-pass filter time constant (s)
 
-% Semisub model matrices
+% Initial values
+eta = [0 0 0 deg2rad(10) deg2rad(5) 0]';   % generalized position vector
+nu  = zeros(6,1);                          % generalized velocity vector
+
+% Semisub model matrices obtained by running 'rig.m'
 MRB = 1.0e+10 * [
     0.0027         0         0         0   -0.0530         0
     0    0.0027         0    0.0530         0   -0.0014
@@ -64,77 +72,83 @@ G = 1.0e+10 * [
 M = MRB + MA;    
 Minv = inv(M);  
 
-% Nonlinear PID controller  
-M3 = diag( [M(1,1), M(2,2), M(6,6) ]);  % 3-DOF matrices 
-D3 = diag( [D(1,1), D(2,2), D(6,6) ]);  
-G3 = diag( [G(1,1), G(2,2), G(6,6) ]); 
+%% MAIN LOOP 
+simdata = zeros(N+1,13);            % table for simulation data
 
-w_n = 0.1;                          % Bandwidth
-Kp = M3 * w_n^2  * eye(3) - G3;     % Proportional gain 
-Kd = 2 * w_n * M3 - D3;             % Derivative  gain 
-Ki = (w_n / 10) * Kp;               % Integral  gain     
-
-%% *************** MAIN SIMULATION LOOP ************************
 for i=1:N+1
-    t = (i-1)*h;                   % simulation time in seconds
+    
+    t = (i-1)*h;                    % simulation time in seconds
 
-    % kinematics
-    phi = eta(4); theta = eta(5); psi = eta(6);
-    J = eulerang(phi,theta,psi); 
-    R = Rzyx(phi,theta,psi); 
-    
-    % 3-DOF nonlinear DP control system (no wave filtering)
-    eta3 = [eta(1) eta(2) ssa( eta(6) )]' ;
-    nu3  = [nu(1) nu(2) nu(6)]';   
-    tau3 = -R' * (Kp * eta3 + Ki * z3) - Kd * nu3; 
-    tau = [tau3(1) tau3(2) 0 0 0 tau3(3)]';
-    
-    % equations of motion
-    t_wave = A_wave * sin(0.2 * t);
-    etadot = J * nu;
-    nudot = Minv * (tau - D * (nu-nu_c) - G * eta + t_wave);
-    
-    % store data for presentation
-    xout(i,:) = [t, eta', nu'];
-    
-    % numerical integration
-    eta = euler2(etadot, eta, h);             % Euler's method
-    nu  = euler2(nudot, nu, h);     
-    z3  = euler2(eta3, z3, h);
+    % Measurements
+    eta(1) = eta(1) + 0.01 * randn;
+    eta(2) = eta(2) + 0.01 * randn;
+    eta(6) = eta(6) + 0.001 * randn;
+
+    % DP control law
+    if t > 100
+        eta_ref = [0 0 -psi_d]';
+    end
+
+    tau_thr = PIDnonlinearMIMO(eta,nu,eta_ref,M,wn,zeta,T_f,h);
+
+    % Equations of motion
+    nudot = Minv * ( tau_thr - D * (nu-nu_c) - G * eta );
+
+    % Kinematics
+    J = eulerang(eta(4),eta(5),eta(6));
+
+    % Store data for presentation
+    simdata(i,:) = [t, eta', nu'];
+
+    % Euler's integration methods (k+1), (Fossen 2021, Eq. B27-B28)
+    nu = nu + h * nudot;          % Forward Euler
+    eta = eta + h * J * nu;       % Backward Euler
 
 end
-% *************** END SIMULATION LOOP ************************
 
-% Time-series
-t     = xout(:,1);
-x     = xout(:,2); 
-y     = xout(:,3);          
-z     = xout(:,4);   
-phi   = xout(:,5) * 180/pi; 
-theta = xout(:,6) * 180/pi; 
-psi   = xout(:,7) * 180/pi;
-u     = xout(:,8);
-v     = xout(:,9);
-w     = xout(:,10);
-p     = xout(:,11) * 180/pi;
-q     = xout(:,12) * 180/pi;
-r     = xout(:,13) * 180/pi; 
+%% PLOTS
+t     = simdata(:,1);
+x     = simdata(:,2); 
+y     = simdata(:,3);          
+z     = simdata(:,4);   
+phi   = rad2deg(simdata(:,5)); 
+theta = rad2deg(simdata(:,6)); 
+psi   = rad2deg(simdata(:,7));
+u     = simdata(:,8);
+v     = simdata(:,9);
+w     = simdata(:,10);
+p     = rad2deg(simdata(:,11));
+q     = rad2deg(simdata(:,12));
+r     = rad2deg(simdata(:,13)); 
 
-%% plots
 figure(1)
 subplot(211)
 plot(y,x,'linewidth',2)
 grid,axis('equal'),xlabel('East'),ylabel('North'),title('Semisub xy-plot (m)')
 subplot(212)
 plot(t,x,t,y,'linewidth',2),title('Semisub positions (m)'),
-legend('x position','y position'),grid
+legend('x position','y position','Location','best'),grid
+set(findall(gcf,'type','line'),'linewidth',2)
+set(findall(gcf,'type','text'),'FontSize',14)
+set(findall(gcf,'type','legend','Location','best'),'FontSize',14)
 
 figure(2)
-subplot(311),plot(t,phi,'linewidth',2),xlabel('time (s)')
+subplot(311)
+plot(t,phi,'linewidth',2),xlabel('time (s)')
 title('roll angle \phi (deg)'),grid
-subplot(312),plot(t,theta,'linewidth',2),xlabel('time (s)')
+subplot(312)
+plot(t,theta,'linewidth',2),xlabel('time (s)')
 title('pitch angle \theta (deg)'),grid
-subplot(313),plot(t,psi,'linewidth',2),xlabel('time (s)')
+subplot(313)
+plot(t,psi,'linewidth',2)
+hold on
+plot( [0, t(end)],-rad2deg([psi_d, psi_d]),'c')
+plot( [0, t(end)], rad2deg([psi_d, psi_d]),'c')
+hold off
+xlabel('time (s)')
 title('yaw angle \psi (deg)'),grid
+set(findall(gcf,'type','line'),'linewidth',2)
+set(findall(gcf,'type','text'),'FontSize',14)
+set(findall(gcf,'type','legend','Location','best'),'FontSize',14)
 
 
