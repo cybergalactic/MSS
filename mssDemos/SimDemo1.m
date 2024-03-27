025f13baec494c0fb4c12fb769fafb811078ae26
Thor I. Fossen <thor.fossen@ntnu.no>
2024-03-27 21:30:45 +0100

    Update SimDemo1.m

--- mssDemos/SimDemo1.m
+++ mssDemos/SimDemo1.m
@@ -1,11 +1,12 @@
 echo on
 % SIMDEMO1    User editable script for simulation of the 
-%              mariner class vessel under feedback control
+%             Mariner class vessel under feedback control
 %
-% Calls:      mariner.m and euler2.m
+% Calls:      mariner.m
+%             euler2.m
 %
 % Author:     Thor I. Fossen
-% Date:       19th July 2001
+% Date:       19 Ju 2001
 % Revisions: 
 
 echo off 
@@ -20,11 +21,11 @@ Td = 10;     % controller derivative time
 % initial states:  x = [ u v r x y psi delta ]' 
 x = zeros(7,1);   
 
-% --- MAIN LOOP ---
+%% MAIN LOOP
 N = round(t_f/h);               % number of samples
 xout = zeros(N+1,length(x)+2);  % memory allocation
 
-for i=1:N+1,
+for i=1:N+1
     time = (i-1)*h;                   % simulation time in seconds
 
     r   = x(3);
@@ -44,7 +45,7 @@ for i=1:N+1,
     x = euler2(xdot,x,h);             % Euler integration
 end
 
-% time-series
+%% PLOTS
 t     = xout(:,1);
 u     = xout(:,2); 
 v     = xout(:,3);          
@@ -55,12 +56,17 @@ psi   = xout(:,7)*180/pi;
 delta = xout(:,8)*180/pi;
 U     = xout(:,9);
 
-% plots
 figure(1)
 plot(y,x),grid,axis('equal'),xlabel('East'),ylabel('North'),title('Ship position')
+set(findall(gcf,'type','line'),'linewidth',2)
+set(findall(gcf,'type','text'),'FontSize',14)
+set(findall(gcf,'type','legend'),'FontSize',14)
 
 figure(2)
 subplot(221),plot(t,r),xlabel('time (s)'),title('yaw rate r (deg/s)'),grid
 subplot(222),plot(t,U),xlabel('time (s)'),title('speed U (m/s)'),grid
 subplot(223),plot(t,psi),xlabel('time (s)'),title('yaw angle \psi (deg)'),grid
 subplot(224),plot(t,delta),xlabel('time (s)'),title('rudder angle \delta (deg)'),grid
+set(findall(gcf,'type','line'),'linewidth',2)
+set(findall(gcf,'type','text'),'FontSize',14)
+set(findall(gcf,'type','legend'),'FontSize',14)
