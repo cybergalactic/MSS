b201f76a8df25da8ac2e80a1beefe02b5bfe64fe
Thor I. Fossen <thor.fossen@ntnu.no>
2024-04-19 09:49:53 +0200

    Update Contents.m

--- Contents.m
+++ Contents.m
@@ -1,12 +1,21 @@
-% The Marine Systems Simulator (MSS) is a Matlab and Simulink library for 
-% marine systems. It includes models for ships, underwater vehicles, 
-% uncrewed surface vehicles, and floating structures. The library also 
-% contains guidance, navigation, and control (GNC) blocks for real-time 
-% simulation. 
+% The m-files of the MSS (Marine Systems Simulator) toolbox are fully 
+% compatible with MATLAB (www.mathworks.com) and the free software 
+% GNU Octave (www.octave.org), facilitating broad accessibility and 
+% application in marine systems simulation. It includes models for ships, 
+% underwater vehicles, uncrewed surface vehicles, and floating structures. 
+% The library also contains guidance, navigation, and control (GNC) blocks 
+% for real-time simulation. 
 % 
-% mssHelp     - Online Quick Reference Guide 
-% mssExamples - Book examples       /MSS/mssExamples/ 
-% mssDemos    - GNC m-file demos    /MSS/mssDemos/
+% To begin using the MSS toolbox, ensure it is installed and properly set 
+% up in your MATLAB/Octave environment. Copy the directory MSS/... to your 
+% computer and "add the path with subfolders" to Matlab/Octave. 
+% 
+%   >> mssHelp - Online Quick Reference Guide 
+%
+% Useful demonstrations and examples:
+%
+%   Book examples       /MSS/mssExamples/ 
+%   GNC m-file demos    /MSS/mssDemos/
 %
 % The algorithms are described in the lecture notes available at 
 % https://wiley.fossen.biz, and in the book by T. I. Fossen (2021), 
@@ -17,4 +26,4 @@
 % and ShipX vessel and RAO data, located under /MSS/SIMULINK/. The Simulink 
 % library is opened by typing
 %
-% >> mssSimulink
+%   >> mssSimulink (not availaible in GNU Octave)
