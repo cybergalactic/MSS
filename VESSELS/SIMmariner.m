c2bcba1a8652f88ddbe35dcd6783dbd523591260
Thor I. Fossen <thor.fossen@ntnu.no>
2024-04-19 21:38:32 +0200

    Update SIMmariner.m

--- VESSELS/SIMmariner.m
+++ VESSELS/SIMmariner.m
@@ -1,5 +1,7 @@
-% SIMmariner  
-% User editable script for simulation of the mariner class vessel under 
+function SIMmariner()
+% SIMmariner is compatibel with MATLAB and GNU Octave (www.octave.org)
+%
+% User-editable script for simulation of the mariner class vessel under 
 % PID heading control.
 %
 % Calls:      mariner.m
@@ -7,7 +9,9 @@
 %
 % Author:     Thor I. Fossen
 % Date:       2018-07-21
-% Revisions:  2024-03-27 Added animation of the ship North-East positions
+% Revisions:  
+%   2024-03-27 : Added animation of the ship North-East positions.
+%   2024-04-19 : Added compability to GNU Octave.
 
 close all;
 clear animateShip  % clear the persistent animation variables
@@ -52,6 +56,11 @@ for i=1:N+1
 end
 
 %% PLOTS
+screenSize = get(0, 'ScreenSize'); % Returns [left bottom width height]
+screenW = screenSize(3);
+screenH = screenSize(4);
+
+% Simdata(i,:) = [t, x', U]
 t     = simdata(:,1);
 u     = simdata(:,2); 
 v     = simdata(:,3);          
@@ -62,31 +71,43 @@ psi   = rad2deg(simdata(:,7));
 delta = rad2deg(simdata(:,8));
 U     = simdata(:,9);
 
-figNo = 1;
-shipSize = 1.0;
-animateShip(x,y,shipSize,'b-',figNo); % animation of the North-East positions
+% Plot and animation of the North-East positions
+figure(1)
+if isoctave() % Octave NE-plot
+    plot(y,x,'b')
+    xlabel('East'); ylabel('North');title('North-East plot (m)')
+    grid,axis('equal')
+    set(findall(gcf,'type','line'),'linewidth',2)
+    set(findall(gcf,'type','text'),'FontSize',14)
+else % Matlab animation
+    shipSize = 1.0;
+    set(gcf, 'Position', [1, 1, screenW/2, screenH]);
+    animateShip(x,y,shipSize,'b-',1);
+end
 
 figure(2)
 subplot(221)
 plot(t,r)
-xlabel('time (s)')
+xlabel('Time (s)')
 title('yaw rate r (deg/s)')
 grid
 subplot(222)
 plot(t,U)
-xlabel('time (s)')
+xlabel('Time (s)')
 title('speed U (m/s)')
 grid
 subplot(223)
 plot(t,psi,[0,t(end)],rad2deg([psi_ref psi_ref]))
-xlabel('time (s)')
+xlabel('Time (s)')
 title('yaw angle \psi (deg)')
 grid
 subplot(224)
 plot(t,delta)
-xlabel('time (s)')
+xlabel('Time (s)')
 title('rudder angle \delta (deg)')
 grid
 set(findall(gcf,'type','line'),'linewidth',2)
 set(findall(gcf,'type','text'),'FontSize',14)
-set(findall(gcf,'type','legend'),'FontSize',14)
+
+end
+
