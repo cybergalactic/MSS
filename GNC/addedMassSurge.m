954f65e276756c2ba7b4a7a76b21ded8649dec86
Thor I. Fossen <thor.fossen@ntnu.no>
2024-04-22 22:21:06 +0200

    Update addedMassSurge.m

--- GNC/addedMassSurge.m
+++ GNC/addedMassSurge.m
@@ -1,37 +1,37 @@
-function [A11,ratio] = addedMassSurge(m,L,rho)
-% A11 = addedMassSurge(m,L,rho) approximates the added mass in surge by
-% the formula of Söding (1982):
+function [A11, ratio] = addedMassSurge(m, L, rho)
+% addedMassSurge is compatible with MATLAB and GNU Octave (www.octave.org). 
+% The function A11 = addedMassSurge(m,L,rho) approximates the added mass 
+% in surge bythe formula of Söding (1982):
 %
 %   A11 = -Xudot = 2.7 * rho * nabla^(5/3) / L^2
 %
-% Output:  
-%   A11: added mass in surge due to a linear acceleration in surge (kg)
-%   ratio: optional output for computation of the mass ratio A11/m,
-%          typical values are ratios < 0.20.
+% Inputs:
+%   m   : mass of the ship or vehicle (kg)
+%   L   : length of the ship or vehicle (m)
+%   rho : density of water (kg/m3), default value is 1025 kg/m3
 %
-% Inputs:   
-%   m: ship/vehicle mass (kg)
-%   L: ship/vehicle length (m)
-%   rho: density of water, default value 1025 (kg/m3)
+% Outputs:
+%   A11   : added mass in surge (kg)
+%   ratio : optional output, ratio of added mass to actual mass (A11/m)
 %
-% Examples:
-%   A11 = addedMassSurge(m,L)         - use default rho = 1025 (kg/m3)
-%   A11 = addedMassSurge(m,L,rho)
-%   [A11,ratio] = addedMassSurge(m,L,rho)
+% Example usage:
+%   A11 = addedMassSurge(m, L)         - use default rho = 1025 (kg/m3)
+%   A11 = addedMassSurge(m, L, rho)
+%   [A11,ratio] = addedMassSurge(m, L, rho)
 %
 % Reference: H. Söding (1982). Prediction of Ship Steering Capabilities. 
 %   Schiffstechnik, 3-29.
 %  
 % Author:    Thor I. Fossen
-% Date:      17 Dec 2021
+% Date:      2021-12-17
 % Revisions: 
 
-if (nargin == 2)
-    rho = 1025;                         % default density of water (kg/m3)
+if nargin == 2      % Check if the density of water is not specified
+    rho = 1025;     % Default density of water (kg/m^3)
 end
 
-nabla = m / rho;                        % volume displacement
-A11 = 2.7 * rho * nabla^(5/3) / L^2;    % added mass A11
-ratio = A11 / m;
+nabla = m / rho;                       % Volume displacement (m^3)
+A11 = 2.7 * rho * nabla^(5/3) / L^2;   % Added mass in surge (kg)
+ratio = A11 / m;                       % Ratio of added mass to actual mass
 
 end
