9e14b49d1bd950f7192106d562eb74f8e9f36a06
Thor I. Fossen <thor.fossen@ntnu.no>
2024-04-27 20:59:48 +0200

    Update ins_mekf.m

--- INS/functions/ins_mekf.m
+++ INS/functions/ins_mekf.m
@@ -1,54 +1,70 @@
 function [x_ins, P_prd] = ins_mekf(...
    x_ins, P_prd, mu, h, Qd, Rd, f_imu, w_imu, m_imu, m_ref, y_pos, y_vel)
-% Error-state (indirect) feedback EKF for INS aided by position
-% measurements y_pos. The velocity aiding signal y_vel is optionally. 
-% The EKF is implemented as a feedback filter using reset. 
-% Attitude is parametrized using unit quaternions and Gibbs vector is used
-% to represent the error states in the MEKF.  
+% ins_mekf_psi is compatible with MATLAB and GNU Octave (www.octave.org).
+% The function implements an error-state (indirect) feedback Kalman filter 
+% (ESKF) specifically for Inertial Navigation Systems (INS) that are 
+% aided by magnetometer and positional data. Attitude is parametrized using 
+% the 4-parameter unit quaternion representation and the Gibbs vector in 
+% the Multiplicative Error State Kalman Filter (MEKF) formulation, thus 
+% avoiding gimbal lock. 
 %
-% See ExINS_MEKF for how to implement the Kalman filter loop:
+% Usage scenarios are detailed in examples SIMaidedINSquat and ExINS_MEKF, 
+% demonstrating the implementation of the Kalman filter loop using the 
+% corrector-predictor representation:
 %
-% if (new slow measurement)
+%   - With new slow measurements:
+%      [x_ins,P_prd] = ins_mekf(...
+%         x_ins, P_prd, mu, h, Qd, Rd, f_imu, w_imu, m_imu, m_ref, y_pos)
+%      [x_ins,P_prd] = ins_mekf(...
+%         x_ins, P_prd, mu, h, Qd, Rd, f_imu, w_imu, m_imu, m_ref, y_pos, y_vel)
 %
-%   ins_mekf(...
-%   x_ins, P_prd, mu, h, Qd, Rd, f_imu, w_imu, m_imu, m_ref, y_pos, y_vel)
+%   - Without new measurements (no aiding):
+%      [x_ins,P_prd] = ins_mekf_psi(...
+%         x_ins, P_prd, mu, h, Qd, Rd, f_imu, w_imu, m_imu, m_ref)
 %
-% else (no measurement)
+% This function models the INS errors in a 15-dimensional state space, 
+% including position, velocity, biases, and attitude errors:
 %
-%   ins__mekf(x_ins, P_prd, mu, h, Qd, Rd, f_imu, w_imu, m_imu, m_ref)
-%
-% end
-%
-% Error-state-space model (15 states):
 %   delta_x[k+1] = f(delta_x[k], u[k], w[k])
 %     delta_y[k] = h(delta_x[k], u[k]) + varepsilon[k]
 %
-% Inputs: 
-%    x_ins[k], INS state vector [p_ins, v_ins, b_acc_ins, q_ins, b_ars_ins] 
-%    P_prd[k],  15 x 15 covariance matrix
-%    mu, lattitude [rad]
-%    h sampling time [s]
-%    Qd, Rd, Kalman filter process and measurement covariance matrices
-%    f_imu[k], w_imu[k], m_imu[k] fast IMU measurements (spec. force, ARS, mag.)
-%    m_ref, magentometer NED reference vector
-%           (m_ref = R' m_imu can be computed for phi = theta = psi = 0)
-%    y_pos[k], slow position measurement (x, y, z) used for aiding
-%    y_vel[k], slow (optionally) velocity measurement used for aiding
+% Inputs:
+%   x_ins[k] : INS state vector at step k, includes position, velocity, 
+%              accelerometer biases, attitude (Euler angles), and gyro biases.
+%   P_prd[k] : 15x15 covariance matrix of the prediction step.
+%   mu       : Latitude in radians, used to calculate Earth's gravity vector.
+%   h        : Sampling time in seconds.
+%   Qd, Rd   : Process and measurement noise covariance matrices for the 
+%              Kalman filter.
+%   f_imu[k] : Specific force measurements from the IMU.
+%   w_imu[k] : Angular rate measurements from the IMU.
+%   m_imu[k] : Magnetic field measurements from the IMU.
+%   m_ref    : Magentometer NED reference vector
+%             (m_ref = R' m_imu can be computed for phi = theta = psi = 0)
+%   y_psi[k] : Fast compass measurement (yaw angle).
+%   y_pos[k] : Slow position measurements aids the filter.
+%   y_vel[k] : (Optionally) Slow velocity measurements aids the filter.
 %
-% Outputs: 
-%    x_ins[k+1] state estimate, equal to the INS state vector (16 states)
-%    P_prd[k+1] Kalman fiter covariance matrix for error states (15 x 15)
+% Outputs:
+%   x_ins[k+1] - Updated INS state vector after propagation.
+%   P_prd[k+1] - Updated prediction covariance matrix after propagation.
 %
-% Author:    Thor I. Fossen
-% Date:      26 Mar 2020
-% Revisions: 29 Nov 2020, bugfix - removed magnetometer projections
-% Revisions: 13 Dec 2021 - replaced Euler's method with exact discretization
+% References:
+%   T. I. Fossen (2021). "Handbook of Marine Craft Hydrodynamics and Motion 
+%   Control," 2nd edition, John Wiley & Sons, Ltd., Chichester, UK.
+%
+% Author: Thor I. Fossen
+% Date: 2020-04-26
+% Revisions: 
+%   2020-11-29: Bugfix - removed magnetometer projection algorithms.
+%   2021-12-13: Improved numerical stability by replacing Euler's method 
+%               with exact discretization in the INS state propagation.
 
 % Bias time constants (user specified)
 T_acc = 1000; 
 T_ars = 500; 
 
-%% KF states and matrices
+%% ESKF states and matrices
 p_ins = x_ins(1:3);          % INS states
 v_ins = x_ins(4:6);
 b_acc_ins = x_ins(7:9);
@@ -56,14 +72,14 @@ q_ins = x_ins(10:13);
 b_ars_ins = x_ins(14:16);
 
 % Gravity vector
-g = gravity(mu);            % WGS-84 gravity model
+g = gravity(mu);             % WGS-84 gravity model
 g_n = [0 0 g]';    
 
 % Constants 
 O3 = zeros(3,3);
 I3 = eye(3);
 
-% Rotation matrix
+% Unit quaternion rotation matrix
 R = Rquat(q_ins);
 
 % Bias compensated IMU measurements
@@ -79,7 +95,7 @@ v1 = v1 / sqrt( v1' * v1 );
 v20 = m_ref / sqrt( m_ref' * m_ref );    % NED
 v2  = m_imu / sqrt( m_ref' * m_ref );    % BODY
  
-% Discrte-time KF matrices
+% Discrte-time ESKF matrices
 A = [ O3 I3  O3            O3              O3
       O3 O3 -R            -R*Smtrx(f_ins)  O3
       O3 O3 -(1/T_acc)*I3  O3              O3
@@ -106,11 +122,11 @@ Ed = h *[  O3 O3    O3 O3
            O3 O3    O3 I3  ];
 
 %% Kalman filter algorithm       
-if (nargin == 10)             % no aiding
+if (nargin == 10)             % No aiding
     
     P_hat = P_prd;
     
-else                         % INS aiding 
+else                          % INS aiding 
     
     % KF gain: K[k]
     K = P_prd * Cd' * inv(Cd * P_prd * Cd' + Rd); 
@@ -137,13 +153,13 @@ else                         % INS aiding
 	delta_q_hat = 1/sqrt(4 + delta_a' * delta_a) * [2 delta_a']';
     
     % INS reset: x_ins[k]
-	p_ins = p_ins + delta_x_hat(1:3);	         % position
-	v_ins = v_ins + delta_x_hat(4:6);			 % velocity
-	b_acc_ins = b_acc_ins + delta_x_hat(7:9);    % acc bias
-	b_ars_ins = b_ars_ins + delta_x_hat(13:15);  % ars bias
+	p_ins = p_ins + delta_x_hat(1:3);	         % Reset position
+	v_ins = v_ins + delta_x_hat(4:6);			 % Reset velocity
+	b_acc_ins = b_acc_ins + delta_x_hat(7:9);    % Reset ACC bias
+	b_ars_ins = b_ars_ins + delta_x_hat(13:15);  % Reset ARS bias
      
     q_ins = quatprod(q_ins, delta_q_hat);   % Schur product    
-    q_ins = q_ins / sqrt(q_ins' * q_ins);   % normalization       
+    q_ins = q_ins / sqrt(q_ins' * q_ins);   % Normalization       
     
 end
 
@@ -151,12 +167,12 @@ end
 P_prd = Ad * P_hat * Ad' + Ed * Qd * Ed';
 
 % INS propagation: x_ins[k+1]
-a_ins = R * f_ins + g_n;                     % linear acceleration
-p_ins = p_ins + h * v_ins + h^2/2 * a_ins;   % exact discretization
-v_ins = v_ins + h * a_ins;                   % exact discretization
-q_ins = expm( Tquat(w_ins) * h ) * q_ins;    % exact discretization
+a_ins = R * f_ins + g_n;                     % Linear acceleration
+p_ins = p_ins + h * v_ins + h^2/2 * a_ins;   % Exact discretization
+v_ins = v_ins + h * a_ins;                   % Exact discretization
+q_ins = expm( Tquat(w_ins) * h ) * q_ins;    % Exact discretization
 % q_ins = q_ins + h * Tquat(q_ins) * w_ins;  % Euler's method (alternative)
-q_ins = q_ins / sqrt(q_ins' * q_ins);        % normalization
+q_ins = q_ins / sqrt(q_ins' * q_ins);        % Normalization
 
 x_ins = [p_ins; v_ins; b_acc_ins; q_ins; b_ars_ins];
 
