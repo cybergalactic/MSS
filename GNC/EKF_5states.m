b4970b6815e0d625bc4ec4e565e23d17e8874165
Thor I. Fossen <thor.fossen@ntnu.no>
2024-05-14 08:10:53 +0200

    Update EKF_5states.m

--- GNC/EKF_5states.m
+++ GNC/EKF_5states.m
@@ -1,31 +1,45 @@
-function x_hat = EKF_5states(GNSS1,GNSS2,h_samp,Z,frame,Qd,Rd)  
-% EKF_5states estimates SOG, COG, and course rate from GNSS positions
-% (xn[k], yn[k]) expressed in NED or latitude-longitude (mu[k], l[k]) using 
-% a 5-states discrete-time extended Kalamn filter (EKF). The output is the 
-% predicted state vector x_hat[k+1] as defined below.
+function x_hat = EKF_5states(position1, position2,...
+    h_samp, Z, frame, Qd, Rd, alpha_1, alpha_2) 
+% EKF_5states is compatible with MATLAB and GNU Octave (www.octave.org).
+% This function estimates the Speed Over Ground (SOG), Course Over Ground 
+% (COG), and course rate from GNSS positions measurements (xn[k], yn[k]) 
+% expressed in NED or latitude-longitude (mu[k], l[k]) using a 5-states 
+% discrete-time extended Kalamn filter (EKF). The output is the predicted 
+% state vector x_hat[k+1] as defined below.
 %
-% Outputs:
-%  [x,y,U,chi omega_chi]  = EKF_5states(x,y,h,Z,'NED',Qd,Rd) 
-%  [mu,l,U,chi,omega_chi] = EKF_5states(mu,l,h,Z,'LL',Qd,Rd)
+% Examples:
+%   x_hat = EKF_5states(x, y, h, Z, 'NED', Qd, Rd) 
+%   x_hat = EKF_5states(x, y, h, Z, 'NED', Qd, Rd, alpha_1, alpha_2) 
+% where x_hat = [x, y, U, chi, omega_chi] 
+%
+%   x_hat = EKF_5states(mu, l, h, Z, 'LL', Qd, Rd)
+%   x_hat = EKF_5states(mu, l, h, Z, 'LL', Qd, Rd, alpha_1, alpha_2)
+% where x_hat = [mu, l, U, chi, omega_chi]
 %
 % Inputs:
-%  GNSS1,GNSS2: North-East positions (m) or Latitude-Longitude (rad)
-%  h_samp:      EKF sampling time (s), h_samp = 1 / measurement frequency
-%  Z:           h_samp * GNSS measurement frequency (Hz) (must be integer)
-%  frame:       'NED' (North-East) or 'LL' (Latitude-Longitude)
-%  Qd:          EKF 2x2 process cov. matrix for speed and course rate
-%  Rd:          EKF 2x2 GNSS measurement cov. matrix
+%  position1, position2: North-East positions (m) or Latitude-Longitude (rad)
+%  h_samp:    EKF sampling time (s), h_samp = 1 / measurement frequency
+%  Z:         h_samp * position measurement frequency (Hz) (must be integer)
+%  frame:     'NED' (North-East) or 'LL' (Latitude-Longitude)
+%  Qd:        EKF 2x2 process covariance matrix for speed and course rate
+%  Rd:        EKF 2x2 position measurement covariance matrix
+%  alpha_1:   (Optionally), Singer constant, speed
+%  alpha_2:   (Optionally), Singer constant, course rate
+%
+% See ExOtter.m for a case study using EKF_5states.m to estimate the COG,
+% SOG, and course rate.
+%
+% Simulink Models:
+%   demoMarinerPathFollowingCourseControl.slx : Simulink model demonstrating  
+%       path-following course control
 %
 % Reference: S. Fossen and T. I. Fossen (2021). Five-state Extended Kalman 
 % Filter for Estimation of Speed Over Ground (SOG), Course Over Ground (COG) 
 % and Course Rate of Unmanned Surface Vehicles (USVs): Experimental Results. 
 % Sensors 21(23). 
 %
-% See ExOtter.m for a case study using EKF_5states.m to estimate the COG,
-% SOG and course rate.
-%
 % Author:   Thor I. Fossen
-% Date:     25 July 2021
+% Date:     2021-07-25 
 % Revisions:
 
 persistent x_prd;
@@ -36,7 +50,7 @@ I5 = eye(5);
 
 if isempty(x_prd)
    disp('Init EKF states') 
-   x_prd = [ GNSS1 GNSS2 0 0 0 ]';
+   x_prd = [ position1 position2 0 0 0 ]';
    P_prd = I5;          
    count = 1;
 end
@@ -46,16 +60,18 @@ a = 6378137;           % Semi-major axis (equatorial radius)
 f = 1/298.257223563;   % Flattening 
 e = sqrt(2*f - f^2);   % Earth eccentricity
 
-alpha_1 = 0.01;        % Singer constant, speed
-alpha_2 = 0.1;         % Singer constant, course rate
+if nargin == 7  % Deafult values
+    alpha_1 = 0.01;        % Singer constant, speed
+    alpha_2 = 0.1;         % Singer constant, course rate
+end
 
 Cd  = [1 0 0 0 0            
        0 1 0 0 0 ];                                    
 Ed = h_samp * [ 0 0; 0 0; 1 0; 0 0; 0 1 ]; 
 
-if (count == 1)         % update if new measurement
-    y  = [ GNSS1        % y1 = x^n    or   y1 = latitude
-           GNSS2 ];     % y2 = y^n    or   y2 = longitude
+if (count == 1)        % update if new measurement
+    y  = [ position1        % y1 = x^n    or   y1 = latitude
+           position2 ];     % y2 = y^n    or   y2 = longitude
               
     K = P_prd * Cd' / ( Cd * P_prd * Cd' + Rd );   % KF gain 
     IKC = I5 - K * Cd;
