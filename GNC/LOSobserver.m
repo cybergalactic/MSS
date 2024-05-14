4b6bbd9c6f5bf9663779645bef32357683aef2d0
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2024-05-14 12:54:55 +0200

    Update LOSobserver.m

--- GNC/LOSobserver.m
+++ GNC/LOSobserver.m
@@ -1,57 +1,58 @@
-function [LOSangle_hat, LOSrate_hat] = ...
-    LOSobserver(LOSangle_hat, LOSrate_hat, LOSangle, h, K_f, T_f)
+function [LOSangle, LOSrate] = ...
+    LOSobserver(LOSangle, LOSrate, LOScommand, h, K_f, alpha)
 % The LOSobserver function estimates the desired Line-Of-Sight (LOS) angle 
-% and rate from a discerte-time LOS guidance law command, LOSangle[k], which 
-% can be computed using the functions LOSchi.m, LOSpsi.m, ILOSpsi.m, 
+% and rate from a discerte-time LOS guidance law command, LOScommand[k], 
+% which can be computed using the functions LOSchi.m, LOSpsi.m, ILOSpsi.m, 
 % ALOSpsi.m, etc. The observer propagates the estimate of the LOS angle, 
 % LOSangle_hat[k], according to
 %
-%  LOSangle_hat[k+1] = LOSangle_hat[k] + h * ( LOSrate_hat[k] + ...
-%    K_f * ssa( LOSangle[k] - LOSangle_hat[k]) )
+%  LOSangle[k+1] = LOSangle[k] + h * ( LOSrate[k] + ...
+%    K_f * ssa( LOScommand[k] - LOSangle[k]) )
 %
-% where the LOS yaw rate estimate, LOSrate_hat[k], is computed using 
-% numerical differentiation, LOSrate = T_f*s / (T_f*s + 1) * LOSangle. 
-% Exact discretization gives (Fossen 2021, Eqs. B.46-B.47) 
+% where the LOS yaw rate estimate, LOSrate[k], is computed using 
+% numerical differentiation, LOSrate = T_f * s / (T_f * s + 1) * LOSangle
+% where T_f is the differentiator time constant. Exact discretization gives 
+% (Fossen 2021, Eqs. B.46-B.47) 
 %
-%  LOSrate_hat[k] = LOSangle_hat[k][k] - xi[k]
-%  xi[k+1] = exp(-h/T_f) * xi[k] + (1 - exp(-h/T_f)) * LOSangle_hat[k]
+%  LOSrate[k] = LOSangle[k][k] - xi[k]
+%  xi[k+1] = exp(-h/T_f) * xi[k] + (1 - exp(-h/T_f)) * LOSangle[k]
 %
 % Inputs:
-%   LOSangle_hat: Estimate of the LOS angle at time k
-%   LOSrate_hat:  Estimate of the LOS angular rate t time k
-%   LOSangle:     Measured LOS angle t time k
-%   h:            Sampling time (s)
-%   K_f:          Observer gain, LOSangle (typically 0.1-0.5)
-%   T_f:          (OPTIONALLY) differentiator time constant, 
-%                     LOSrate = T_f*s / (T_f*s + 1) * LOSangle
-%                 If omitted, the LOSrate loop is chosen 5 x faster than 
-%                 the LOSangle loop corresponding to: 1/T_f = 5 * K_f 
-%
+%   LOSangle:    Estimate of the desired LOS angle at time k
+%   LOSrate:     Estimate of the desired LOS angular rate t time k
+%   LOScommand:  Commanded LOS angle t time k, computed using ALOSpsi.m,
+%                ILOSpsi.m, LOSchi.m, etc.
+%   h:           Sampling time (s)
+%   K_f:         Observer gain, LOSangle (typically 0.1-0.5)
+%   alpha:       (OPTIONALLY) bandwidth ratio between the differentiator 
+%                and the observer, typically 5 to 10. If omitted, the 
+%                LOSrate loop is chosen 5 x faster than the LOSangle loop.
 % Outputs:
-%   LOSangle_hat: Updated estimate of the LOS angle at time k+1
-%   LOSrate_hat:  Updated estimate of the LOS rate at time k+1
+%   LOSangle:    Updated estimate of the desired LOS angle at time k+1
+%   LOSrate:     Updated estimate of the desired LOS rate at time k+1
 %
 % Calls: ssa.m
 %  
 % Author:    Thor I. Fossen
 % Date:      2024-04-01
 
-if nargin == 5       % yaw rate loop is 5 times faster than yaw angle loop 
-    T_f = (1/5) * (1/K_f);  % 1/T_f = 5 * K_f  
+if nargin == 5       
+    % Yaw rate loop is 5 times faster than yaw angle loop
+    alpha = 5;
 end
+T_f = (1 / alpha) * 1 / K_f;    % Differentiator time constant, 
 
 % Internal differentiator state: xi[k]
-xi = LOSangle_hat - LOSrate_hat;
+xi = LOSangle - LOSrate;
 
-% Observer for the LOS angle: LOSangle_hat[k+1]
-LOSangle_hat = LOSangle_hat + h * ( LOSrate_hat + ...
-    K_f * ssa( LOSangle - LOSangle_hat) );
+% Observer for the LOS angle: LOSangle[k+1]
+LOSangle = LOSangle + h * (LOSrate + K_f * ssa( LOScommand - LOSangle) );
 
 % Propagation of the differentiator state: xi[k+1]
 PHI = exp(-h/T_f);
-xi = PHI * xi + (1 - PHI) * LOSangle_hat;
+xi = PHI * xi + (1 - PHI) * LOSangle;
 
-% LOS rate estimate: LOSrate_hat[k+1]
-LOSrate_hat = LOSangle_hat - xi;
+% LOS rate estimate: LOSrate[k+1]
+LOSrate = LOSangle - xi;
 
 end
