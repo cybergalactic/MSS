84819e0ab212160697f143e915d7af31c562628b
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-02 12:55:30 +0200

    Update npsauv.m

--- VESSELS/models/npsauv.m
+++ VESSELS/models/npsauv.m
@@ -1,10 +1,11 @@
-function [xdot,U] = npsauv(x,ui)
+function [xdot, U, B_delta] = npsauv(x, ui)
 % Compatibel with MATLAB and the free software GNU Octave (www.octave.org).
-% [xdot,U] = npsauv(x,ui) returns the speed U in m/s (optionally) and the 
-% time derivative of the state vector: x = [u v w p q r x y z phi theta psi]' 
-% for an Autnomous Underwater Vehicle (AUV) at the Naval Postgraduate
-% School, Monterrey. The length of the AUV is L = 5.3 m, while the state 
-% vector is defined by:
+% [xdot, U, B_delta] = npsauv(x, ui) returns the time derivative of the 
+% state vector: x = [u v w p q r xpos ypos zpos phi theta psi]', speed U 
+% in m/s (optionally) and, input matrix B_delta (optionally) for an 
+% Autnomous Underwater Vehicle (AUV) at the Naval Postgraduate School, 
+% Monterrey. The length of the AUV is 5.3 m and the mass is 5443 kg, while 
+% the state vector is defined by:
 %
 %   u:      Surge velocity              (m/s)
 %   v:      Sway velocity               (m/s)
@@ -21,81 +22,84 @@ function [xdot,U] = npsauv(x,ui)
 %
 % Input vector:
 %
-%   ui = [ delta_r delta_s delta_b delta_bp delta_bs n ]'  
+%   ui = [ delta_r delta_s delta_bp delta_bs n ]'  
 %
 %   delta_r:    Rudder angle                    (rad)
-%   delta_s:    Port and starboard stern planes (rad)
-%   delta_b:    Top and bottom bow planes       (rad)
+%   delta_s:    Stern plane                     (rad)
 %   delta_bp:   Port bow plane                  (rad)
 %   delta_bs:   Starboard bow plane             (rad)
 %   n:          Propeller shaft speed           (rpm)  
 %
+% M-file Simulators:
+%   SIMnpsauv.m : Script demonstrating 3-D ALOS path-following control.
+%
+% Simulink Simulators:
+%   demoNPSAUV.slx : Simulink model demonstrating PID heading control.
+%
 % Reference: 
 %   A. J. Healey and Lienard, D. (1993). Multivariable Sliding Mode Control 
-%   for Autonomous Diving and Steering of Unmanned Underwater Vehicles,
-%   IEEE Journal of Ocean Engineering 18(3):327-339.
+%     for Autonomous Diving and Steering of Unmanned Underwater Vehicles,
+%     IEEE Journal of Ocean Engineering 18(3):327-339.
 %
-% Author:    Trygve Lauvdal
-% Date:      1994-05-15
+% Author:    Thor I. Fossen
+% Date:      2024-06-03
 % Revisions: 
-%    2001-07-21 : Minor change of notation, including speed U in call
-%    2003-03-24 : Corrected the integrals for Cy, Cz, Cn and Cm by introducing 
-%                 a new variable temp. Also corrected an error in the 
-%                 restoring force Z, that is (W-B) * cos(theta) * cos(phi).
-%   2014-11-13 : Mwn * w * n was corrected to Mwn * w * u 
-%   2019-08-01 : Corrected bugs in the cross-flow drag formulas
-%   2024-04-20 : Added compability to GNU Octave.
 
 % Check of input and state dimensions
 if (length(x) ~= 12), error('x-vector must have dimension 12!'); end
-if (length(ui) ~= 6), error('u-vector must have dimension 6!'); end
+if (length(ui) ~= 5), error('u-vector must have dimension 5!'); end
 
 % Dimensional states
 u   = x(1);  v     = x(2);  w   = x(3);
 p   = x(4);  q     = x(5);  r   = x(6);
 phi = x(10); theta = x(11); psi = x(12);
 
-U = sqrt(u^2+v^2+w^2); % speed
+U = sqrt(u^2 + v^2 + w^2);  % speed
 
-% Rudder and propeller
-max_ui = zeros(6,1);
-max_ui(1)  = 20*pi/180;        % max value delta_r   (rad)
-max_ui(2)  = 20*pi/180;        % max value delta_s   (rad)
-max_ui(3)  = 20*pi/180;        % max value delta_b   (rad)
-max_ui(4)  = 20*pi/180;        % max value delta_bp  (rad)
-max_ui(5)  = 20*pi/180;        % max value delta_bs  (rad)
-max_ui(6)  = 1500;             % max value n         (rpm)
+% Rudder and propeller saturation limits
+max_ui = zeros(5,1);
+max_ui(1) = deg2rad(20);   % Max delta_r   (rad)
+max_ui(2) = deg2rad(20);   % Max delta_s   (rad)
+max_ui(3) = deg2rad(20);   % Max delta_bp  (rad)
+max_ui(4) = deg2rad(20);   % Max delta_bs  (rad)
+max_ui(5) = 1500;          % Max propeller speed (rpm)
 
-% Parameters, hydrodynamic derivatives and main dimensions
-c1 = cos(phi);
-c2 = cos(theta);
-c3 = cos(psi);
-s1 = sin(phi);
-s2 = sin(theta);
-s3 = sin(psi);
-t2 = tan(theta);
+% Saturation of control inputs
+ui = sat(ui, max_ui);      
 
-L   = 5.3;    g   = 9.8;
+delta_r  = ui(1);  
+delta_s  = ui(2);
+delta_bp = ui(3);
+delta_bs = ui(4);
+n        = ui(5) / 60 * 2*pi;
+
+% AUV parameters
+L   = 5.3;    g   = 9.81;
 xG  = 0;      yG  = 0;      zG = 0.061;
 xB  = 0;      yB  = 0;      zB = 0;
-rho = 1000;   m   = 5454.54/(rho/2*L^3);
 W   = 53400;  B   = 53400;
+rho = 1025;   mass = W/g;   
 Ix  = 2038;   Iy  = 13587;  Iz  = 13587;
 Ixy = -13.58; Iyz = -13.58; Ixz = -13.58;
 Cdy = 0.5;    Cdz = 0.6;
-Cy  = 0;      Cz  = 0;
-Cm  = 0;      Cn  = 0;
+r_bb = [xB yB zB]';
+r_bg = [xG yG zG]';
 
-r2 = rho*L^2/2;
-r3 = rho*L^3/2;
-r4 = rho*L^4/2;
-r5 = rho*L^5/2;
+% Prime-scaling variables (Fossen 2021, Appendix D.2)
+Tinv = diag( [1 1 1 L L L] );
+r2 = (1/2) * rho * L^2;
+r3 = (1/2) * rho * L^3;
+r4 = (1/2) * rho * L^4;
+r5 = (1/2) * rho * L^5;
+
+% Nondimensional mass and hydrodynamic derivaties
+m = mass / r3;
 
 Xpp   =  7.0e-3; Xqq    = -1.5e-2; Xrr   =  4.0e-3; Xpr   =  7.5e-4;
 Xudot = -7.6e-3; Xwq    = -2.0e-1; Xvp   = -3.0e-3; Xvr   =  2.0e-2;
 Xqds  =  2.5e-2; Xqdb2  = -1.3e-3; Xrdr  = -1.0e-3; Xvv   =  5.3e-2;
 Xww   =  1.7e-1; Xvdr   =  1.7e-3; Xwds  =  4.6e-2; Xwdb2 =  0.5e-2;
-Xdsds = -1.0e-2; Xdbdb2 = -4.0e-3; Xdrdr = -1.0e-2; Xqdsn =  2.0e-3;
+Xdsds = -1.0e-2; Xdrdr  = -1.0e-2; Xqdsn =  2.0e-3;
 Xwdsn =  3.5e-3; Xdsdsn = -1.6e-3;
 
 Ypdot =  1.2e-4; Yrdot  =  1.2e-3; Ypq   =  4.0e-3; Yqr   = -6.5e-3;
@@ -123,124 +127,130 @@ Nvdot =  1.2e-3; Np     = -8.4e-4; Nr    = -1.6e-2; Nvq   = -1.0e-2;
 Nwp   = -1.7e-2; Nwr    =  7.4e-3; Nv    = -7.4e-3; Nvw   = -2.7e-2;
 Ndr   = -1.3e-2; Nprop  =  0;
 
-% Rudder and shaft saturations
-for i=1:1:6
-   if abs(ui(i)) > max_ui(i)
-       ui(i)=sign(ui(i)) * max_ui(i);
-   end
-end
- 
-% Control input (rudder and propeller)
-delta_r  = ui(1);
-delta_s  = ui(2);
-delta_b  = ui(3);
-delta_bp = ui(4);
-delta_bs = ui(5);
-n        = ui(6)/60*2*pi;
+% Rigid-body and added mass matrices
+I_g = [Ix -Ixy -Ixz
+       -Ixy Iy -Iyz
+       -Ixz -Iyz Iz ];
 
+MRB = [  mass * eye(3)      -mass * Smtrx(r_bg)
+         mass * Smtrx(r_bg)  I_g                ];
+
+MA = -[ Xudot 0     0     0     0     0
+        0     Yvdot 0     Ypdot 0     Yrdot
+        0     0     Zwdot 0     Zqdot 0
+        0     Kvdot 0     Kpdot 0     Krdot
+        0     0     Mwdot 0     Mqdot 0
+        0     Nvdot 0     Npdot 0     Nrdot ];
+MA = r3 * Tinv * MA * Tinv;   
+
+M = MRB + MA;
+
+% Control forces and moments
 Cd0   = 0.00385;
-prop  = 0.012*n/u; 
-Xprop = Cd0*(abs(prop)*prop - 1);
-Ct    = 0.008*L^2*abs(prop)*prop/2;
-Ct1   = 0.008*L^2/2;
-epsi  = -1 + sign(n)/sign(u)*(sqrt(Ct+1)-1)/(sqrt(Ct1+1)-1);
+eps_prop = 1e-10;  % Avoid dividing by zero
+prop  = 0.012 * n / (u + eps_prop); 
+Xprop = Cd0 * ( abs(prop) * prop - 1 );
+Ct    = 0.008 * L^2 * abs(prop) * prop / 2;
+Ct1   = 0.008 * L^2 / 2;
+epsilon = -1 + sign(n)/sign(u) * ( sqrt(Ct+1)-1) / (sqrt(Ct1+1)-1 );
 
-tau1 = r3*(Xrdr*u*r*delta_r + (Xqds*delta_s + Xqdb2*delta_bp +...
-           Xqdb2*delta_bs)*u*q) +...
-       r2*(Xvdr*u*v*delta_r + (Xwds*delta_s + Xwdb2*delta_bs + ...
-           Xwdb2*delta_bp)*u*w + (Xdsds*delta_s^2 + ...
-           Xdbdb2*delta_b^2 + Xdrdr*delta_r^2)*u^2) +...
-       r3*Xqdsn*u*q*delta_s*epsi + r2*(Xwdsn*u*w*delta_s +...
-       Xdsdsn*u^2*delta_s^2)*epsi + r2*u^2*Xprop; 
-tau2 = r2*Ydr*u^2*delta_r;
-tau3 = r2*u^2*(Zds*delta_s+Zdb2*delta_bs+Zdb2*delta_bp) + ...
-       r3*Zqn*u*q*epsi+r2*(Zwn*u*w+Zdsn*u^2*delta_s)*epsi;
-tau4 = r4*Kpn*u*p*epsi+r3*u^3*Kprop + ...
-       r3*u^2*(Kdb2*delta_bp+Kdb2*delta_bs);
-tau5 = r4*Mqn*u*q*epsi+r3*(Mwn*w*u+Mdsn*u^2*delta_s)*epsi+...
-       r3*u^2*(Mds*delta_s+Mdb2*delta_bp+Mdb2*delta_bs);
-tau6 = r3*u^2*Nprop + r3*u^2*Ndr*delta_r;
+tau1 = r3 * (Xrdr*u*r*delta_r + (Xqds*delta_s + Xqdb2*delta_bp +...
+             Xqdb2*delta_bs)*u*q) +...
+       r2 * (Xvdr*u*v*delta_r + (Xwds*delta_s + Xwdb2*delta_bs + ...
+             Xwdb2*delta_bp)*u*w + (Xdsds*delta_s^2 + ...
+             + Xdrdr*delta_r^2)*u^2) +...
+       r3 * Xqdsn*u*q*delta_s*epsilon + r2*(Xwdsn*u*w*delta_s +...
+            Xdsdsn*u^2*delta_s^2)*epsilon + r2*u^2*Xprop;
+tau2 = r2 * Ydr*u^2*delta_r;
+tau3 = r2 * u^2*(Zds*delta_s+Zdb2*delta_bs+Zdb2*delta_bp) + ...
+       r3 * Zqn*u*q*epsilon+r2*(Zwn*u*w+Zdsn*u^2*delta_s)*epsilon;
+tau4 = r4 * Kpn*u*p*epsilon+r3*u^3*Kprop + ...
+       r3 * u^2*(Kdb2*delta_bp+Kdb2*delta_bs);
+tau5 = r4 * Mqn*u*q*epsilon+r3*(Mwn*w*u+Mdsn*u^2*delta_s)*epsilon+...
+       r3 * u^2*(Mds*delta_s+Mdb2*delta_bp+Mdb2*delta_bs);
+tau6 = r3 * u^2*Nprop + r3*u^2*Ndr*delta_r;
 
-% Drag forces and moments assuming block shaped body
-dxL = L/10;
-xL  = 0; 
-Ucf = sqrt((v+xL*r)^2+(w-xL*q)^2);
+tau_control = [tau1 tau2 tau3 tau4 tau5 tau6]';
 
-if ~(Ucf == 0)
-    for xL = -L/2:dxL:L/2
-        Ucf = sqrt((v+xL*r)^2+(w-xL*q)^2);
-        temp = (0.5*0.6*(v+xL*r)^2+0.6*(w-xL*q)^2)*(v+xL*r)/Ucf; 
-        Cy = Cy + dxL*temp;
-    end
+% 4-DOF optional input matrix B_delta for the control surfaces in sway, 
+% heave, pitch, and yaw, defined by
+%   tau_control = [ tau1
+%                   B_delta * u_delta + b0(ui) ]
+%   u_delta = [ delta_r delta_s delta_bp delta_bs ]' 
+%   b0 = [ tau1
+%          0
+%          r3 * Zqn * u * q * epsilon + r2 * Zwn * u * w * epsilon
+%          tau4
+%          r4 * Mqn * u * q * epsilon + r3 * Mwn * u * w * epsilon
+%          r3 * u^2 * Nprop ]
+B_delta = u^2 * [
+    r2 * Ydr, 0, 0, 0
+    0, r2 * Zds + r2 * Zdsn * epsilon, r2 * Zdb2, r2 * Zdb2
+    0, r3 * Mdsn * epsilon + r3 * Mds, r3 * Mdb2, r3 * Mdb2
+    r3 * Ndr, 0, 0, 0 ];
+
+% Cross-flow drag assuming a block-shaped body and strip theory
+dxL = L/10;             % Number of sections
+epsilon_drag = 1e-6;    % Avoid dividing by zero
+Hx = 0.53;              % Average height of submerged body
+Bx = 0.53;              % Average width of submerged body
+
+Cy = 0; Cz = 0; Cm = 0; Cn = 0;
+for xL = -L/2:dxL:L/2   % Cross-flow drag integrals
+
+    Ucf = sqrt((v + xL * r)^2 + (w - xL * q)^2) + epsilon_drag;
+    drag_term = Cdy * Hx * (v + xL * r)^2 + Cdz * Bx * (w - xL * q)^2;
     
-    for xL = -L/2:dxL:L/2
-        Ucf = sqrt((v+xL*r)^2+(w-xL*q)^2);
-        temp = (0.5*0.6*(v+xL*r)^2+0.6*(w-xL*q)^2)*(w-xL*q)/Ucf;
-        Cz = Cz + dxL*temp;
-    end
-    
-    for xL = -L/2:dxL:L/2
-        Ucf = sqrt((v+xL*r)^2+(w-xL*q)^2);
-        temp = (0.5*0.6*(v+xL*r)^2+0.6*(w-xL*q)^2)*(w+xL*q)/Ucf*xL;
-        Cm = Cm + dxL*temp;
-    end
-    
-    for xL = -L/2:dxL:L/2
-        Ucf = sqrt((v+xL*r)^2+(w-xL*q)^2);
-        temp = (0.5*0.6*(v+xL*r)^2+0.6*(w-xL*q)^2)*(v+xL*r)/Ucf*xL;
-        Cn = Cn + dxL*temp;
-    end
+    Cy = Cy + dxL * drag_term * (v + xL * r) / Ucf;
+    Cz = Cz + dxL * drag_term * (w - xL * q) / Ucf;
+    Cm = Cm + dxL * drag_term * (w + xL * q) / Ucf * xL;
+    Cn = Cn + dxL * drag_term * (v + xL * r) / Ucf * xL;
+
 end
+tau_crossflow = (rho/2) * [ 0 -Cy Cz 0 -Cm -Cn]';
+
+% Restoring forces and moments
+tau_hydrostatic = -gvect(W, B, theta, phi, r_bg, r_bb);
 
 % Hydrodynamic forces and moments
-X = r3*((m+Xvr)*v*r + (Xwq-m)*w*q + Xvp*v*p) + ... 
-    r4*((m*xG/L+Xqq)*q^2 + (m*xG/L+Xrr)*r^2 - m*yG/L*p*q + ...
-       (Xpr-m*zG/L)*p*r + Xpp*p^2 ) + ...
-    r2*(Xvv*v^2 + Xww*w^2 )- (W - B)*sin(theta) + tau1;
+X_h = r2 * ( Xvv*v^2 + Xww*w^2 ) + ...
+      r3 * ( (m+Xvr)*v*r + (Xwq-m)*w*q + Xvp*v*p ) + ... 
+      r4 * ( (m*xG/L+Xqq)*q^2 + (m*xG/L+Xrr)*r^2 - m*yG/L*p*q + ...
+             (Xpr-m*zG/L)*p*r + Xpp*p^2 );
 
-Y = r2*(Yv*u*v + Yvw*v*w) + ...
-    r3*(Yp*u*p + Yr*u*r +Yvq*v*q + Ywp*w*p + Ywr*w*r) +...
-    r4*(Ypq*p*q + Yqr*q*r) + (W-B)*cos(theta)*sin(phi) - ...
-    m*(rho/2*L^3)*(u*r -w*p + xG*p*q - yG*(p^2+r^2) + zG*q*r)+tau2-rho/2*Cy;
+Y_h = r2 * ( Yv*u*v + Yvw*v*w ) + ...
+      r3 * ( Yp*u*p + Yr*u*r +Yvq*v*q + Ywp*w*p + Ywr*w*r ) + ...
+      r4 * ( Ypq*p*q + Yqr*q*r) - ...
+      mass * ( u*r -w*p + xG*p*q - yG*(p^2+r^2) + zG*q*r );
 
-Z = r2*(Zw*w*u + Zvv*v^2) +...
-    r3*(Zq*u*q + Zvp*v*p + Zvr*v*r) +...
-    r4*(Zpp*p^2 + Zpr*p*r + Zrr*r^2) + ...
-    (W-B)*cos(theta)*cos(phi) - ...
-    m*(rho/2*L^3)*(v*p - u*q + xG*p*r + yG*q*r -zG*(p^2+q^2))+tau3+ rho/2*Cz;
+Z_h = r2 * ( Zw*w*u + Zvv*v^2 ) +...
+      r3 * ( Zq*u*q + Zvp*v*p + Zvr*v*r ) +...
+      r4 * ( Zpp*p^2 + Zpr*p*r + Zrr*r^2 ) + ...
+      mass * ( v*p - u*q + xG*p*r + yG*q*r -zG*(p^2+q^2) );
 
-K = r3*(Kv*u*v + Kvw*v*w) +...
-    r4*(Kp*u*p + Kr*u*r + Kvq*v*q + Kwp*w*p + Kwr*w*r) +...
-    r5*(Kpq*p*q + Kqr*q*r) +...
-    (Iy-Iz)*q*r - Ixy*p*r - (r^2-q^2)*Iyz + Ixz*p*q - ...
-    m*(rho/2*L^3)*(yG*(v*p-u*q) - zG*(u*r - w*p)) + ...
-    (yG*W-yB*B)*c1*c2 - (zG*W-zB*B)*c2*s1 + tau4;
+K_h = r3 * ( Kv*u*v + Kvw*v*w ) +...
+      r4 * ( Kp*u*p + Kr*u*r + Kvq*v*q + Kwp*w*p + Kwr*w*r ) +...
+      r5 * ( Kpq*p*q + Kqr*q*r ) +...
+      (Iy-Iz) * q*r - Ixy * p*r - (r^2-q^2) * Iyz + Ixz * p*q - ...
+      mass * ( yG*(v*p-u*q) - zG*(u*r - w*p) );
 
-M = r3*(Muw*u*w + Mvv*v^2) +...
-    r4*(Muq*u*q + Mvp*v*p + Mvr*v*r) +...
-    r5*(Mpp*p^2 + Mpr*p*r + Mrr*r^2) - ...
-    (xG*W-xB*B)*c1*c2 - (zG*W-zB*B)*s2 + ...
-    (Iz-Ix)*p*r + Ixy*q*r - Iyz*p*q - (p^2-r^2)*Ixz + ...
-    m*(rho/2*L^3)*(xG*(v*p-u*q) - zG*(w*q-v*r)) + tau5 - rho/2*Cm;
+M_h = r3 * ( Muw*u*w + Mvv*v^2 ) +...
+      r4 * ( Muq*u*q + Mvp*v*p + Mvr*v*r ) +...
+      r5 * ( Mpp*p^2 + Mpr*p*r + Mrr*r^2 ) - ...
+      (Iz-Ix) * p*r + Ixy * q*r - Iyz * p*q - (p^2-r^2) * Ixz + ...
+      mass * ( xG*(v*p-u*q) - zG*(w*q-v*r) );
+ 
+N_h = r3 * ( Nv*u*v + Nvw*v*w )+...
+      r4 * ( Np*u*p + Nr*u*r + Nvq*v*q + Nwp*w*p + Nwr*w*r )+...
+      r5 * ( Npq*p*q + Nqr*q*r ) +...
+      (Ix-Iy) * p*q + (p^2-q^2) * Ixy + Iyz * p*r - Ixz * q*r -...
+      mass * ( xG*(u*r-w*p) - yG*(w*q-v*r) );
 
-N = r3*(Nv*u*v + Nvw*v*w)+...
-    r4*(Np*u*p + Nr*u*r + Nvq*v*q + Nwp*w*p + Nwr*w*r)+...
-    r5*(Npq*p*q + Nqr*q*r) +...
-    (xG*W-xB*B)*s1*c2 + (yG*W-yB*B)*s2 + ...
-    (Ix-Iy)*p*q + (p^2-q^2)*Ixy + Iyz*p*r - Ixz*q*r -...
-    m*(rho/2*L^3)*(xG*(u*r-w*p) - yG*(w*q-v*r)) + tau6 - rho/2*Cn ;
+tau_hydrodynamic = [ X_h Y_h Z_h K_h M_h N_h ]';
 
-% Dimensional state derivatives (xdot = in(M)*f(x) is expanded to avoid inv(M))
-xdot = [1.662e-4*X+1.846e-10*Y+1.303e-7*Z+3.726e-9*K-1.132e-6*M+7.320e-10*N
-        1.846e-10*X+1.052e-4*Y+3.843e-10*Z+9.638e-6*K-3.340e-9*M+2.368e-6*N
-         1.303e-7*X+3.843e-10*Y+4.315e-5*Z+7.757e-9*K-2.357e-6*M+1.524e-9*N
-          3.726e-9*X+9.638e-6*Y+7.757e-9*Z+2.431e-4*K-6.742e-8*M-7.740e-7*N
-         -1.132e-6*X-3.340e-9*Y-2.357e-6*Z-6.742e-8*K+2.049e-5*M-1.324e-8*N
-         7.320e-10*X+2.368e-6*Y+1.524e-9*Z-7.740e-7*K-1.324e-8*M+4.838e-5*N    
-            c3*c2*u + (c3*s2*s1-s3*c1)*v + (s3*s1+c3*c1*s2)*w 
-            s3*c2*u + (c1*c3+s1*s2*s3)*v + (c1*s2*s3-c3*s1)*w 
-                     -s2*u + c2*s1*v + c1*c2*w 
-                        p + s1*t2*q + c1*t2*r 
-                            (c1*q - s1*r) 
-                           s1/c2*q + c1/c2*r                               ];
+% State derivatives 
+xdot = [ M \ ( tau_control + ...
+            tau_hydrodynamic + tau_crossflow + tau_hydrostatic );
+         eulerang(phi,theta,psi) * x(1:6) ];
+
 end
