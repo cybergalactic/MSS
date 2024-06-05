2da6cf8b1332d4827e821d6c40b4265f0cb700bc
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-05 09:45:24 +0200

    Update sat.m

--- GNC/sat.m
+++ GNC/sat.m
@@ -1,26 +1,33 @@
 function x = sat(x, x_max)
 % Compatible with MATLAB and the free software GNU Octave (www.octave.org)
-%
 % x = sat(x, x_max) saturates the input x at the specified maximum absolute 
-% value x_max.
+% value x_max. If the inputs are vectors, saturation is performed
+% elementwise.
 %
 % Inputs:
-%   x:     A scalar, vector, or matrix of numerical values.
-%   x_max: A positive scalar specifying the maximum allowable absolute value.
+%   x:     A vector of numerical values.
+%   x_max: A vector specifying the maximum allowable absolute values.
 %
 % Outputs:
-%   x - The saturated values of the original input x
+%   x:     A vector of saturated values of the original x elements
 %
 % Author: Thor I. Fossen
 % Date: 2024-04-20
+% Revisions:
+%   2024-06-05 : Extended to accept vectors as inputs
 
-% Check if x_max is positive
-if x_max < 0
-    error('x_max must be a non-negative number.');
-end
+for i = 1:length(x)
 
-if abs(x) > x_max
-    x = sign(x) .* x_max;
+    % Check if x_max(i) is positive
+    if x_max(i) <= 0
+        error(['x_max(' num2str(i) ' must be a positive.']);
+    end
+
+    % Saturation
+    if abs(x(i)) > x_max(i)
+        x(i) = sign(x(i)) * x_max(i);
+    end
+
 end
 
 end
