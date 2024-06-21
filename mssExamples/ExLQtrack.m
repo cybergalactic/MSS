06c6770f2c3553ab0d8a99d51d008ec134ddefeb
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2024-06-21 13:05:59 +0200

    Update ExLQtrack.m

--- mssExamples/ExLQtrack.m
+++ mssExamples/ExLQtrack.m
@@ -1,16 +1,19 @@
-% ExLQtrack  Computes the LQ optimal tracking gains for a mass-damper system
+% ExLQtrack is compatible with MATLAB and GNU Octave (www.octave.org).
+% The script computes the LQ optimal tracking gains for a mass-damper
+% system.
+
 % Author:    Thor I. Fossen
-% Date:      25 June 2001
+% Date:      2001-06-25
 % Revisions: 
 
 % Design matrices
-Q = diag([1]);      % user editable tracking error weights
-R = diag([1]);      % user editable input weights
-%
+Q = diag(1);      % User editable tracking error weights
+R = diag(1);      % User editable input weights
+
 % System matrices
-A = [0 1; -1 -2];   % user editable state matrix
-B = [0; 1];         % user editable input matrix
-C = [1 0];          % user editable output matrix
+A = [0 1; -1 -2];   % User editable state matrix
+B = [0; 1];         % User editable input matrix
+C = [1 0];          % User editable output matrix
 
 % Compute optimal gain matrices
 [G1,G2] = lqtracker(A,B,C,Q,R)
