74f6c1aaaeb66428cfccbf872e5655ef2d09ed86
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2024-06-21 13:05:53 +0200

    Update ExNomoto.m

--- mssExamples/ExNomoto.m
+++ mssExamples/ExNomoto.m
@@ -1,27 +1,31 @@
-% ExNomoto  Generates the Nomoto Bode plots of the two ships in Chapter 2
-%           See also: nomoto.m
+% exNomoto requires the Matlab control systems toolbox
+% The script generates the Nomoto Bode plots of two MSS ships.
+%      
 % Author:   Thor I. Fossen
-% Date:     19th June 2001
+% Date:     2001-06-19
 % Revisions: 
 
-% Mariner class cargo ship
+% Mariner-class cargo ship
 T1 = 118; T2 = 7.8; T3 = 18.5; K = 0.185;  
 
 T = T1+T2-T3;
 d1 = [T 1 0];           n1 = K; 
 d2 = [T1*T2 T1+T2 1 0]; n2 = K*[T3 1];
-w=logspace(-4,1,50);
+w =logspace(-4,1,50);
 wi = [0.0001 10];
 [mag1,phase1] = bode(n1,d1,w);
 [mag2,phase2] = bode(n2,d2,w);
 
-if K < 0,   % shift ship phase with 360 deg for course unstable ship
-phase1 = phase1-360;
-phase2 = phase2-360;
+if K < 0    % Shift ship phase with 360 deg for course unstable ship
+    phase1 = phase1 - 360;
+    phase2 = phase2 - 360;
 end
 
-clf,subplot(411),semilogx(w,20*log10(mag1),'b'),grid
-ylabel('Gain [dB]'),title('Mariner class vessel')
+clf
+subplot(411)
+semilogx(w,20*log10(mag1),'b')
+grid
+ylabel('Gain [dB]'),title('Mariner-class cargo vessel')
 hold on,semilogx(w,20*log10(mag2),'r'),hold off
 legend('1st-order model','2nd-order model')
 subplot(412),semilogx(w,phase1,'b'),grid
@@ -38,9 +42,9 @@ d2 = [T1*T2 T1+T2 1 0]; n2 = K*[T3 1];
 [mag1,phase1] = bode(n1,d1,w);
 [mag2,phase2] = bode(n2,d2,w);
 
-if K < 0,   % shift ship phase with 360 deg for course unstable ship
-phase1 = phase1-360;
-phase2 = phase2-360;
+if K < 0   % Shift ship phase with 360 deg for course unstable ship
+    phase1 = phase1 - 360;
+    phase2 = phase2 - 360;
 end
 
 subplot(413),semilogx(w,20*log10(mag1),'b'),grid
@@ -51,3 +55,7 @@ subplot(414),semilogx(w,phase1,'b'),grid
 ylabel('Phase [deg]');xlabel('Frequency [rad/s]')
 hold on,semilogx(w,phase2,'r'),hold off
 legend('1st-order model','2nd-order model')
+
+set(findall(gcf,'type','line'),'linewidth',2)
+set(findall(gcf,'type','text'),'FontSize',14)
+set(findall(gcf,'type','legend'),'FontSize',14)
