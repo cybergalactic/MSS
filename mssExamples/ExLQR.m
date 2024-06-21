59f587dec0fec79ba79ad2dd74a744b7a8c9c0f4
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2024-06-21 13:06:01 +0200

    Update ExLQR.m

--- mssExamples/ExLQR.m
+++ mssExamples/ExLQR.m
@@ -1,16 +1,18 @@
-% ExLQR      Computes the LQR gains for a mass-damper system
+% eLQR is compatible with MATLAB and GNU Octave (www.octave.org).
+% The script computes the LQR gains for a mass-damper system.
+% 
 % Author:    Thor I. Fossen
-% Date:      25 June 2001
+% Date:      2001-06-25
 % Revisions: 
 
 % Design matrices
-Q = diag([1]);      % user editable tracking error weights (dim m x m)
-R = diag([1]);      % user editable input weights (dim r x r)
-%
+Q = diag(1);      % User editable tracking error weights (dim m x m)
+R = diag(1);      % User editable input weights (dim r x r)
+
 % System matrices
-A = [0 1; -1 -2];   % user editable state matrix (dim n x n)
-B = [0; 1];         % user editable input matrix (dim n x r)
-C = [1 0];          % user editable output matrix (dim m x n)
+A = [0 1; -1 -2];   % User editable state matrix (dim n x n)
+B = [0; 1];         % User editable input matrix (dim n x r)
+C = [1 0];          % User editable output matrix (dim m x n)
 %
 % Compute the optimal feedback gain matrix G
 [K,P,E] = lqr(A,B,C'*Q*C,R);
