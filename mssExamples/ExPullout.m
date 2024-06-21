3a3f6d79690582942aba2acbca7dd459ed8beb2f
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-21 15:39:56 +0200

    Update ExPullout.m

--- mssExamples/ExPullout.m
+++ mssExamples/ExPullout.m
@@ -1,27 +1,30 @@
-% ExPullout    Performes a pullout maneuver for two different ships
+% exPullout is compatible with MATLAB and GNU Octave (www.octave.org).  
+% Performes a pullout maneuver for two different ships.
 %
 % Author:   Thor I. Fossen
 % Date:     25th July 2001
 % Revisions: 
 
-delta_c = 20*pi/180;     % rudder angle for manuver (rad)
-h = 0.1;                 % sampling time (sec)
+delta_c = deg2rad(20);   % Rudder angle for manuver (rad)
+h = 0.1;                 % Sampling time (sec)
 
-disp('Pullout maneuver for the Mariner class vessel (stable ship)')
+disp('Pullout maneuver for the Mariner-class cargo vessel (stable ship)')
 
-% Mariner class cargo ship, cruise speed U0 = 7.7 m/s (see mariner.m)
+%% Mariner-class cargo ship, cruise speed U0 = 7.7 m/s (see mariner.m)
 x  = zeros(7,1);        % x  = [ u v r x y psi delta ]' (initial values)
 ui = delta_c;           % ui = delta_c 
  
-[t,r1,r2] = pullout('mariner',x,ui,h);
+pullout('mariner',x,ui,h);
 
-% The Esso Osaka tanker (see tanker.m)
-disp(' '); disp('Press a key to simulate: The Esso Osaka tanker (unstable ship)'); pause
+%% The Esso Osaka tanker (see tanker.m)
+disp(' '); 
+disp('Press a key to simulate: The Esso Osaka tanker (unstable ship)'); 
+pause
 
 n = 80;
 U = 8.23;
-x = [ U 0 0 0 0 0 0 n ]';     % x = [ u v r x y psi delta n ]' (initial values)
-n_c     = 80;                 % n_c = propeller revolution in rpm
-depth   = 200;                % water depth
+x = [ U 0 0 0 0 0 0 n ]';  % x = [ u v r x y psi delta n ]' (initial values)
+n_c     = 80;              % n_c = propeller revolution in rpm
+depth   = 200;             % Water depth
 ui = [delta_c, n_c, depth];
 [t,r1,r2] = pullout('tanker',x,ui,h);
