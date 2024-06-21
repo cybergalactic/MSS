a5bb2ce0a27b02410ffaf8184cb81812871d1c78
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2024-06-21 13:09:01 +0200

    Update and rename ExEKF.m to exEKF.m

--- mssExamples/exEKF.m
+++ mssExamples/exEKF.m
@@ -1,4 +1,4 @@
-% exEKF is compatibel with MATLAB and GNU Octave (www.octave.org).
+% exEKF is compatible with MATLAB and GNU Octave (www.octave.org).
 % This script simulates a discrete-time Extended Kalman filter (EKF), which 
 % demonstrates how the "predictor-corrector representation" can be 
 % applied to the nonlinear model:
