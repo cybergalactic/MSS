45c0e60e31138946ff20e0b1d28224edfac5e285
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-21 16:02:11 +0200

    Update quest6dof.m

--- GNC/quest6dof.m
+++ GNC/quest6dof.m
@@ -1,8 +1,11 @@
-function [eta,q,R] = quest6DOF(y,mb,rcamera)
-% [eta,q,R] = quest6DOF(y,mb,rcamera)  computes the 6 DOF vector eta = [x,y,z,phi,theta,psi]
-% and unit quaternion vector q = [eta,eps1,eps2,eps3] from three 3x1 marker position measurement 
-% vectors stacked in y = [y1_e, y2_e, y3_e] using the QUEST algorithm (see quest.m). 
-% The 3x1 body-frame marker position vectors are stacked in mb = [m1_b, m2_b, m3_b].
+function [eta,q,R] = quest6dof(y,mb,rcamera)
+% quest6dof is compatible with MATLAB and GNU Octave (www.octave.org).
+% [eta,q,R] = quest6dof(y,mb,rcamera) computes the 6-DOF vector 
+% eta = [x,y,z,phi,theta,psi] and unit quaternion vector 
+% q = [eta,eps1,eps2,eps3] from three 3x1 marker position measurement 
+% vectors stacked in y = [y1_e, y2_e, y3_e] using the QUEST algorithm
+% (see quest.m). The 3x1 body-frame marker position vectors are stacked 
+% in mb = [m1_b, m2_b, m3_b].
 % 
 % Inputs: 
 %    y  = [x1,y1,z1,x2,y2,z2,x3,y3,z3] vector of 3 camera measurements (earth-frame)
@@ -11,18 +14,24 @@ function [eta,q,R] = quest6DOF(y,mb,rcamera)
 %
 % Outputs:
 %    eta - 6x1 Earth-fixed positions and attitude (Euler angles)
-%    q   - 4x1 vector of unit quaternions (optionally)
-%    R   - 3x3 rotation matrix from BODY to NED (optionally) 
+%    q   - 4x1 Vector of unit quaternions (optionally)
+%    R   - 3x3 Rotation matrix from BODY to NED (optionally) 
 %
 % Author:    Karl-Petter Lindegaard and Thor I. Fossen
 % Date:      5th August 2001
 % Revisions:
 
-% check input dimensions
+% Check input dimensions
 [mx,my] = size(mb);
 [nx,ny] = size(y);  
-if my>mx,help quest6DOF; error('mb must be a column vector of 3x1 position vectors'); end
-if ny>nx,help quest6DOF; error(' y must be a column vector of 3x1 position vectors'); end
+if my > mx
+    help quest6dof; 
+    error('mb must be a column vector of 3x1 position vectors'); 
+end
+if ny > nx
+    help quest6dof; 
+    error(' y must be a column vector of 3x1 position vectors'); 
+end
   
 % Map inputs to 3x1 position vectors
 m1_b = mb(1:3);  m2_b = mb(4:6);  m3_b = mb(7:9);
@@ -36,5 +45,5 @@ V=[m1_b-m3_b m2_b-m3_b m1_b-m2_b];
 [R,q] = quest(W,V);
 
 % compute 6 DOF position/attitude vector
-eta(1:3) = rcamera + (y1_e+y2_e+y3_e - R*(m1_b+m2_b+m3_b))/3;  % mean position
-[eta(4) eta(5) eta(6)] = q2euler(q);                           % Euler angles
+eta(1:3) = rcamera + (y1_e+y2_e+y3_e - R*(m1_b+m2_b+m3_b))/3;  % Mean position
+[eta(4), eta(5), eta(6)] = q2euler(q);                         % Euler angles
