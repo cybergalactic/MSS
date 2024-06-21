7662e927c05aaee4b7398a7b6200db0b9641401f
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-21 16:10:36 +0200

    Update ExResonance.m

--- mssExamples/ExResonance.m
+++ mssExamples/ExResonance.m
@@ -1,4 +1,5 @@
-% ExResonance computes the responses in heave, roll and pitch for a marine
+% exResonance is compatible with MATLAB and GNU Octave (www.octave.org).
+% The script computes the responses in heave, roll, and pitch for a marine
 % craft exposed to a regular wave. The closed-form solution of a linear
 % mass-damper-spring system with sinusoidal forcing F sin(w_e t) is used. 
 % Harmonic oscillator:
@@ -12,9 +13,9 @@
 % Date:      2019-05-01
 % Revisions: 
 
-load supply;		% load supply data (ShipX)
+load supply;		% Load supply data (ShipX)
 
-% compute relative damping rations and natural frequencies
+% Compute relative damping rations and natural frequencies
 [T,zeta] = DPperiods(vessel);
 z3 = zeta(3);   z4 = zeta(4);   z5 = zeta(5);
 w3 = 2*pi/T(3); w4 = 2*pi/T(4); w5 = 2*pi/T(5);
@@ -25,19 +26,19 @@ figure(gcf)
 
 w = w3 * wr;
 Zm3w = sqrt((w.^2)*(2*z3*w3)^2 + (w3^2-w.^2).^2);  % Zm3 * w_e
-plot(wr, w3^3./Zm3w,'-.k','linewidth',2),hold on
+plot(wr, w3^3./Zm3w,'-.','linewidth',2),hold on
 
 w = w4 * wr;
 Zm4w = sqrt((w.^2)*(2*z4*w4)^2 + (w4^2-w.^2).^2);  % Zm4 * w_e
-plot(wr, w4^2./Zm4w,'-k','linewidth',2)
+plot(wr, w4^2./Zm4w,'-','linewidth',2)
 
 z4_double = 2*z4;
 Zm4w = sqrt((w.^2)*(2*z4_double*w4)^2 + (w4^2-w.^2).^2);
-plot(wr, w4^2./Zm4w,'-k','linewidth',1)
+plot(wr, w4^2./Zm4w,'-','linewidth',1)
 
 w = w5 * wr;
 Zm5w = sqrt((w.^2)*(2*z5*w5)^2 + (w5^2-w.^2).^2);  % Zm5 * w_e
-plot(wr, w5^2./Zm5w,':k','linewidth',2)
+plot(wr, w5^2./Zm5w,':','linewidth',2)
 
 title('Amplitude = w_i^2 / (Z_m w_e)'),xlabel('w_e/w_n'), grid
 legend(sprintf('Heave: damping z_3 = %3.2f, nat. frequency w_3 = %3.2f',z3, w3),...
