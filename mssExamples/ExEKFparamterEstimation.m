6ebfaebd38e65c33ede383bf2afda628a9eee1e9
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2024-06-21 12:57:18 +0200

    Update ExEKFparamterEstimation.m

--- mssExamples/ExEKFparamterEstimation.m
+++ mssExamples/ExEKFparamterEstimation.m
@@ -1,4 +1,4 @@
-% ExEKFparameterEstimation is compatibel with MATLAB and GNU Octave
+% exEKFparameterEstimation is compatibel with MATLAB and GNU Octave
 % (www.octave.org). This script simulates a discrete-time extended Kalman
 % filter (EKF) implementation demonstrating how the "predictor-corrector
 % representation" can be be used to estimate unknown states and parameters
