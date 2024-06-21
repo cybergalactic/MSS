e6ae33b94c7d249773fe94d637813f8bd28b46b9
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2024-06-21 13:24:17 +0200

    Update ExPassiveObs.m

--- mssExamples/ExPassiveObs.m
+++ mssExamples/ExPassiveObs.m
@@ -1,16 +1,19 @@
-% ExPassiveObs Plots the loop transfer function of the passive observer
+% exPassiveObs is compatibel with MATLAB and GNU Octave (www.octave.org).
+% The script plots the loop transfer function of the passive observer in
+% Fossen (2021, Chapter 13.5) for dynamic positioning.
+%
 % Author:      Thor I. Fossen
 % Date:        15th October 2002
 % Revisions: 
 
-% filter paramaters
+% Filter paramaters
 T = eye(3)/1000
 lambda = 0.1;
 wo   = 0.8976;
 wc   = 1.1;
 T = diag([1000 1000 1000]);
 
-% filter gains
+% Filter gains
 K1 = [-2*(1-lambda)*(wc/wo)*eye(3)
     2*wo*(1-lambda)*eye(3)     ]
 K2 = diag([wc wc wc])
@@ -22,11 +25,15 @@ figure(gcf)
 w = logspace(-4,1.5,100);
 
 i=1;  % mode = surge
-h0 = tf([1 2*lambda*wo wo*wo],[1 (K1(i+3,i)+K2(i,i)+2*lambda*wo) (wo^2+2*lambda*wo*K2(i,i)-K1(i,i)*wo^2) wo^2*K2(i,i)]) 
+h0 = tf([1 2*lambda*wo wo*wo],[1 (K1(i+3,i)+K2(i,i)+2*lambda*wo) ...
+    (wo^2+2*lambda*wo*K2(i,i)-K1(i,i)*wo^2) wo^2*K2(i,i)]) 
 hB = tf(K4(i,i)*[1 (1/T(i,i)+K3(i,i)/K4(i,i))],[1 1/T(i,i)])
-wave = tf([wo^2 0],[1 2*lambda*wo wo^2])      % wave response spectrum
+wave = tf([wo^2 0],[1 2*lambda*wo wo^2])      % Wave response spectrum
 
 bode(series(h0,hB),wave,w)
+
+set(findall(gcf,'type','text'),'FontSize',14)
+set(findall(gcf,'type','line'),'linewidth',2)
 
 
 
