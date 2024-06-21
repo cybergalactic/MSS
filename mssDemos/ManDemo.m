d0d11f715b8f15bc49e740d45a25ed28a1d473ce
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-21 15:46:12 +0200

    Update ManDemo.m

--- mssDemos/ManDemo.m
+++ mssDemos/ManDemo.m
@@ -1,20 +1,22 @@
-%       MANEUVERING TRIALS demonstrated by simulating m-file vessel models:
-%       Type: >> mssHelp for more information
+% ManDemo is compatible with MATLAB and GNU Octave (www.octave.org).  
+% Maneuvering trials demonstrated by simulating m-file vessel models:
+% Type: >> mssHelp for more information
 %
-%       1)  Turning circle for the Mariner class vessel and a container ship
-%           (see mariner.m and container.m)
-%       2)  Zigzag manuvers for the Mariner class vessel and a container ship
-%           (see mariner.m and container.m)
-%       3)  Pullout maneuver for the Mariner class vessel and the Esso Osaka tanker
-%           (see mariner.m and tanker.m)
+%   1)  Turning circle for the Mariner-class cargo vessel and a container ship
+%       (see mariner.m and container.m)
+%   2)  Zigzag manuvers for the Mariner class vessel and a container ship
+%       (see mariner.m and container.m)
+%   3)  Pullout maneuver for the Mariner-class cargo vessel and the Esso 
+%       Osaka tanker (see mariner.m and tanker.m)
 %
-%       0)  Quit
+%   0)  Quit
 echo off
 
 while(1)
-    demos= ['ExTurnCircle'
-        'ExZigZag    '
-        'ExPullout   '];
+    demos= [
+        'exTurnCircle'
+        'exZigZag    '
+        'exPullout   ' ];
     clc
     help ManDemo
     n = input('Select a demo number: ');
