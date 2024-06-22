870dd30669a37eee37f0a267daf364bd7893dd5e
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-22 10:27:00 +0200

    Update q2euler.m

--- GNC/q2euler.m
+++ GNC/q2euler.m
@@ -4,14 +4,15 @@ function [phi,theta,psi] = q2euler(q)
 %
 % Author:    Thor I. Fossen
 % Date:      2001-06-14  
-% Revisions: 2022-04-15 Fixed test to avoid abs(R(3,1)) > 1 and norm(q) > 1
+% Revisions: 
+%   2022-04-15 - Fixed test to avoid abs(R(3,1)) > 1 and norm(q) > 1
 
-q = q / norm(q);            % normalize q, handle round-off errors 
+q = q / norm(q);            % Normalize q, handle round-off errors 
 R = Rquat(q);
 
 phi = atan2(R(3,2),R(3,3));
 
-if (abs( R(3,1 )) > 1)      % handle NaN due to round-off errors
+if (abs( R(3,1 )) > 1)      % Handle NaN due to round-off errors
     R(3,1) = sign(R(3,1));
 end
 
