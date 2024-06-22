a4fde8ff4222f53fcc052318d1672d14ea4f79a1
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-22 10:26:32 +0200

    Update zigzag.m

--- GNC/zigzag.m
+++ GNC/zigzag.m
@@ -1,10 +1,10 @@
 function [t,u,v,r,x,y,psi,U] = zigzag(ship,x,ui,t_final,t_rudderexecute, ...
     h,maneuver)
 % [t,u,v,r,x,y,psi,U] = zigzag(ship,x,ui,t_final,t_rudderexecute,h,maneuver)
-% performs the zigzag maneuver, see ExZigZag.m
+% performs the zigzag maneuver, see exZigzag.m
 %
-% Inputs :
-% 'ship':          ship model. Compatible with the models under .../VESSELS
+% Inputs:
+% 'ship':          ship model. Compatible with the models under MSS/VESSELS/
 % x:               initial state vector for ship model
 % ui:              [delta,:] where delta=0 and the other values are nonzero if any
 % t_final:         final simulation time
@@ -14,14 +14,15 @@ function [t,u,v,r,x,y,psi,U] = zigzag(ship,x,ui,t_final,t_rudderexecute, ...
 %                  maneuver = [20, 20] rudder is changed to maneuver(1) when 
 %                  heading angle is larger than maneuver(2)
 %
-% Outputs :
-% t               = time vector
-% u,v,r,x,y,psi,U = time series
+% Outputs:
+% t               : time vector
+% u,v,r,x,y,psi,U : time series
 %
 % Author:    Thor I. Fossen
 % Date:      22th July 2001
-% Revisions: 15th July 2002, switching logic has been modified to handle 
-%                            arbitrarily maneuvers
+% Revisions: 
+%   15th July 2002 - Switching logic has been modified to handle 
+%                    arbitrarily maneuvers
 
 if nargin>7 || nargin < 6, error('number of inputs must be 6 or 7'); end
 if t_final < t_rudderexecute
@@ -29,8 +30,8 @@ if t_final < t_rudderexecute
 end
 if nargin == 6, maneuver = [20,20]; end
 
-N = round(t_final/h);               % number of samples
-xout = zeros(N+1,9);                % memory allocation
+N = round(t_final/h);               % Number of samples
+xout = zeros(N+1,9);                % Memory allocation
 
 disp('Simulating...')
 
@@ -54,14 +55,14 @@ for i=1:N+1
         end   
     end
  
-    [xdot,U] = feval(ship,x,u_ship);          % ship model
+    [xdot,U] = feval(ship,x,u_ship);          % Ship model
     
     xout(i,:) = [time,x(1:6)',U,u_ship(1)];  
     
     x = euler2(xdot,x,h);                     % Euler's method
 end
 
-% time-series
+% Time-series
 t       = xout(:,1);
 u       = xout(:,2); 
 v       = xout(:,3);         
@@ -72,7 +73,7 @@ psi     = rad2deg( xout(:,7) );
 U       = xout(:,8);
 delta_c = rad2deg( xout(:,9) );
 
-% plots
+%% Plots
 figure(1)
 plot(y,x),grid,axis('equal'),xlabel('East'),ylabel('North')
 title('Zigzag test')
