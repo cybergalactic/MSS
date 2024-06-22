6546035a1e90940e15732dce6a8830a5861ac66e
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-22 10:26:29 +0200

    Update wageningen.m

--- GNC/wageningen.m
+++ GNC/wageningen.m
@@ -1,13 +1,16 @@
 function [KT, KQ] = wageningen(Ja,PD,AEAO,z)
-% [KT, KQ] = wageningen(Ja,PD,AEAO,z) computes the thrust and torque
-% coefficients of the Wageningen B-series propellers for:
+% [KT, KQ] = wageningen(Ja,PD,AEAO,z) computes the thrust KT and torque KQ
+% coefficients of the Wageningen B-series propellers.
+% 
+% Inputs:
+%   Ja = Va/(n*D)  : Advance number
+%   PD             : Pitch/diameter ratio (typically 0.5-2.5)
+%   AEAO           : Blade area ratio (ratio of expanded blade area to 
+%                  : Propeller disk area)
+%   z              : Number of propeller blades
 %
-% Ja = Va/(n*D)  advance number
-% PD             pitch/diameter ratio (typically 0.5-2.5)
-% AEAO           blade area ratio (ratio of expanded blade area to propeller disk area)
-% z              number of propeller blades
-%
-% see ExWageningen.m for use of [K_T, K_Q] = Wageningen(Ja,PD,AEAO,z)
+% Usage:
+%  See exWageningen.m 
 %
 % The B-series propellers were designed and tested at the Netherlands Ship
 % Model Basin in Wageningen. The open_water characteristics of 120
@@ -17,13 +20,15 @@ function [KT, KQ] = wageningen(Ja,PD,AEAO,z)
 % blades, the blade area ratio, the pitch_diameter ratio and the advance
 % coefficient.
 %
-% Reference: Barnitsas, M.M., Ray, D. and Kinley, P. (1981).
-% KT, KQ and Efficiency Curves for the Wageningen B-Series Propellers
-% http://deepblue.lib.umich.edu/handle/2027.42/3557
+% Reference: 
+%   Barnitsas, M.M., Ray, D. and Kinley, P. (1981).
+%   KT, KQ and Efficiency Curves for the Wageningen B-Series Propellers
+%   http://deepblue.lib.umich.edu/handle/2027.42/3557
 %
 % Author:    Thor I. Fossen
 % Date:      7 October 2018
-% Revisions: 18 June 2019 - minor updates
+% Revisions: 
+%   18 June 2019 - minor updates
 
 load('WageningData.mat');
     
