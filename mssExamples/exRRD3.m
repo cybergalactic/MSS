7ff054b323cfa99f5308b1cf5ad935cd205f6444
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-22 09:10:06 +0200

    Update exRRD3.m

--- mssExamples/exRRD3.m
+++ mssExamples/exRRD3.m
@@ -1,34 +1,41 @@
-% exRRD3  Inverse response in roll due to right-half-plane zero 
-% (non-minimum phase) for the Son and Nomoto container ship.
+% exRRD3: Inverse response in roll due to right-half-plane zero 
+% (non-minimum phase) for the Son and Nomoto (1982) container ship.
+%
+% References:  
+%   K. Son og K. Nomoto (1982). On the Coupled Motion of Steering and 
+%   Rolling of a High Speed Container Ship, Naval Architect of 
+%   Ocean Engineering 20:73-83. From J.S.N.A., Japan, Vol. 150, 1981.
+%
 % Author:    Thor I. Fossen
-% Date:      31 October 2001
+% Date:      2001-10-31
 % Revisions: 
 
 conversion % Load conversion factors
 
-u0     = 7.3;    % Initial speed
-n_ref =  70;     % RPM
+u0     = 7.3;  % Initial speed (m/s)
+n_ref =  70;   % Desried propeller speed (RPM)
 
-N = 10000;  % No. of samples
-h = 0.05;   % Sample time
+N = 10000;     % No. of samples
+h = 0.05;      % Sample time in seconds
 
-x = [u0 0 0 0 0 0 0 0 0 70]';     % x = [u v r x y psi p phi delta n ]'
+% x = [u v r x y psi p phi delta n ]'
+x = [u0 0 0 0 0 0 0 0 0 70]';     
 U = u0;
 
 xout = zeros(N+1,length(x)+1);
 
 for i=1:N+1
     
-    % 10 deg rudder command
-    if (i>1000 && i<1500)
-        u_ref = 10*D2R;
+    % Rudder commands
+    if (i > 1000 && i < 1500)
+        u_ref = deg2rad(10);
     else
         u_ref = 0;
     end
         
     xout(i,:) = [x',U];
     
-    % Nonlinear model of Son and Nomoto
+    % Nonlinear model of Son and Nomoto (1982)
     [xdot,U] = container(x,[u_ref; n_ref]); 
     
      x = x + h*xdot;
@@ -37,15 +44,20 @@ end
 t = h*(0:1:N)';
 figure(gcf)
 
-subplot(211)
-plot(t,R2D*xout(:,6),t,10*R2D*xout(:,8))
-title(['Roll angle 10\cdot\phi deg and yaw angle \psi deg for a ' ...
-    '10 deg rudder step in 50 s'])
+subplot(311)
+plot(t, rad2deg(xout(:,6)), t, 10*rad2deg(xout(:,8)))
+title('Roll angle and yaw angle for a 50 s step in rudder angle (deg)')
+grid
+legend('10\cdot\phi (roll angle)','\psi (yaw angle)')
+
+subplot(312)
+plot(t, rad2deg(xout(:,9)))
+title('Rudder angle (deg)')
 grid
 
-subplot(212)
-plot(t,xout(:,11))
-title('Forward speed (m/s)')
+subplot(313)
+plot(t, xout(:,11))
+title('Ship speed (m/s)')
 grid
 
 set(findall(gcf,'type','text'),'FontSize',14)
