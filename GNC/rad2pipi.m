d1682f32ebcf84fd61bcd970eaf4a336abc5e2cd
Thor I. Fossen <thor.fossen@ntnu.no>
2024-06-22 10:26:52 +0200

    Update rad2pipi.m

--- GNC/rad2pipi.m
+++ GNC/rad2pipi.m
@@ -1,7 +1,8 @@
 function y = rad2pipi(angle)
-% RAD2PIPI Converts an angle in rad to the interval (-pi pi]
-%
-% Function is replaced by a more optimal function ssa.m
-% 2018-10-04 Thor I. Fossen
+% RAD2PIPI Converts an angle in rad to the interval (-pi pi]. The 
+% function is replaced by ssa.m.
+% 
+% Author:    Thor I. Fossen
+% Date:      2018-10-04
 
 y = ssa(angle,'rad');
