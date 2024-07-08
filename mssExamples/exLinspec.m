e0cf8f6087c7a55e894ae40d35f4aa1f8133fddc
Thor I. Fossen <thor.fossen@ntnu.no>
2024-07-08 21:23:08 +0200

    Update exLinspec.m

--- mssExamples/exLinspec.m
+++ mssExamples/exLinspec.m
@@ -1,66 +1,73 @@
 % exLinspec requires the MATLAB Optimization Toolbox.
-% Linear approximation to PM, JONSWAP og Torsethaugen spectra using 
-% nonlinear least-squares(NLS).
+% Linear approximation to PM, JONSWAP, and Torsethaugen spectra using 
+% nonlinear least-squares (NLS) curve fitting.
 %
 % Author:    Thor I. Fossen
 % Date:      2001-08-15 
 % Revisions: 
 %   2008-12-10  Use updated function wavespec.m
 %   2021-07-03  Function Slin.m added at the end of the file 
+%   2024-07-07  Use new function waveSpectrum.m. Removed the global variable
 
-global sigma wo
+clearvars;
 
 wo = 0.8;  
 To = 2 * pi / wo;
 Hs = 10;
 wmax = 3;
-w = (0.0001:0.01:wmax)';  
+w = (0:0.01:wmax)';  
+
+% Initial guess for lambda
+lambda0 = 0.1;
+
+figure(gcf); clf;
 
 % Modified PM
-figure(gcf)
 subplot(311)
-S = wavespec(3,[Hs,To],w,1);  
+S = waveSpectrum(3, [Hs, To], w, 1);  
 sigma = sqrt(max(S));
-
-lambda = lsqcurvefit(@Slin, 0.1, w, S)
-
-hold on; plot(w,Slin(lambda,w),'linewidth',2); hold off; 
-legend('Modified PM spectrum','Linear approximation')
+lambda = lsqcurvefit(@(lambda, w) Slin(lambda, wo, sigma, w), lambda0, w, S);
+disp(['lambda_PM = ', num2str(lambda)])
+hold on; plot(w, Slin(lambda, wo, sigma, w), 'b'); hold off; grid;
+legend('Modified PM spectrum', 'Linear approximation')
 
 % JONSWAP
 subplot(312)
-S = wavespec(7,[Hs,wo,3.3],w,1);   sigma = sqrt(max(S));
-
-lambda = lsqcurvefit(@Slin, 0.1, w, S)
-
-hold on; plot(w,Slin(lambda,w),'linewidth',2); hold off; 
-legend('JONSWAP spectrum','Linear approximation')
+S = waveSpectrum(7, [Hs, wo, 3.3], w, 1);   
+sigma = sqrt(max(S));
+lambda = lsqcurvefit(@(lambda, w) Slin(lambda, wo, sigma, w), lambda0, w, S);
+disp(['lambda_JONSWAP = ', num2str(lambda)])
+hold on; plot(w, Slin(lambda, wo, sigma, w), 'b'); hold off; grid;
+legend('JONSWAP spectrum', 'Linear approximation')
 
 % Torsethaugen (only one peak is fitted)
 subplot(313)
-S = wavespec(8,[Hs,wo],w,1);   sigma = sqrt(max(S));
+S = waveSpectrum(8, [Hs, wo], w, 1);   
+sigma = sqrt(max(S));
+lambda = lsqcurvefit(@(lambda, w) Slin(lambda, wo, sigma, w), lambda0, w, S);
+disp(['lambda_Torsethaugen = ', num2str(lambda)])
+hold on; plot(w, Slin(lambda, wo, sigma, w), 'b'); hold off; grid; 
+legend('Torsethaugen spectrum', 'Linear approximation')
 
-lambda = lsqcurvefit(@Slin, 0.1, w, S)
-
-hold on; plot(w,Slin(lambda,w),'linewidth',2); hold off; 
-legend('Torsethaugen spectrum','Linear approximation')
-
-set(gca,'FontSize',11)
+set(findall(gcf, 'type', 'line'), 'linewidth', 2)
+set(findall(gcf, 'type', 'text'), 'FontSize', 12)
+set(findall(gcf, 'type', 'legend'), 'FontSize', 12);
 
 %% Function Slin
-function Pyy = Slin(lambda,w)
-% Pyy = Slin(lambda,w) 2nd-order linear power spectracl density (PSD) function
+function Pyy = Slin(lambda, wo, sigma, w)
+% Pyy = Slin(lambda, wo, sigma, w) 2nd-order linear power spectral density 
+% (PSD) function.
 %
-% w       : Wave spectrum frequency (rad/s)
 % lambda  : Relative damping factor
+% wo      : Peak frequency (rad/s)
+% sigma   : Spectrum gain
+% w       : Wave spectrum frequency (rad/s)
 %
 % Author:   Thor I. Fossen
 % Date:     2001-08-15 
 % Revisions:
 
-global sigma wo
-
-Pyy = 4*(lambda*wo*sigma)^2*w.^2 ./ ( (wo^2-w.^2).^2 + 4*(lambda*wo.*w).^2 );
+Pyy = 4 * (lambda * wo * sigma)^2 * w.^2 ./ ...
+    ( (wo^2 - w.^2).^2 + 4 * (lambda * wo .* w).^2 );
 
 end
-
