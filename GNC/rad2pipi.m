74ce281258ae637b462414c0d9b45e92526e9c6b
Thor I. Fossen <thor.fossen@ntnu.no>
2024-07-09 19:18:02 +0200

    Delete rad2pipi.m

--- GNC/rad2pipi.m
+++
@@ -1,8 +0,0 @@
-function y = rad2pipi(angle)
-% RAD2PIPI Converts an angle in rad to the interval (-pi pi]. The 
-% function is replaced by ssa.m.
-% 
-% Author:    Thor I. Fossen
-% Date:      2018-10-04
-
-y = ssa(angle,'rad');
