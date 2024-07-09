0eac5124964a904c7b6998fafa4ca92f6116e712
Thor I. Fossen <thor.fossen@ntnu.no>
2024-07-09 19:17:59 +0200

    Update ucalloc.m

--- GNC/obsolete/ucalloc.m
+++ GNC/obsolete/ucalloc.m
@@ -3,9 +3,7 @@ function u = ucalloc(K, T, W, tau)
 % u = allocPseudoinverse(K,T,W,tau) for unconstrained control allocation.
 %
 % Author:    Thor I. Fossen
-% Date:      3 Nov 2001
-% Revisions: 
-%   25 Mar 2024 - function name changed to allocPseudoinverse.m
+% Date:      2001-11-13
 
 disp('u = ucalloc(K, T, W, tau) is obsolete.')
 disp('Use u = allocPseudoinverse(K, T, W, tau)')
