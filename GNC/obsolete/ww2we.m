1025dcf93876e3293e0860874213476ac18ff6a1
Thor I. Fossen <thor.fossen@ntnu.no>
2024-07-09 20:35:48 +0200

    Create ww2we.m

---
+++ GNC/obsolete/ww2we.m
@@ -0,0 +1,7 @@
+function Omega_e = ww2we(chi, U, Omega)
+% Omega_e = ww2we(chi, U, Omega)is obsolute and it has been replaced by
+% Omega_e = encounter(chi, U, Omega).
+
+Omega_e = encounter(chi, U, Omega);
+
+end
