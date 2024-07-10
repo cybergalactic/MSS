2a2664b354f4af3842b113e38ef70be87b7eccae
Thor I. Fossen <thor.fossen@ntnu.no>
2024-07-10 18:57:51 +0200

    Update SIMtanker.m

--- VESSELS/SIMtanker.m
+++ VESSELS/SIMtanker.m
@@ -4,8 +4,8 @@ function SIMtanker()
 % under feedback control. 
 %
 % Dependencies:
-%   tanker     - Nonlinear tanker model
-%   euler2     - Euler's integrations method
+%   tanker        - Nonlinear tanker model
+%   lowPassdilter - Low-pass filter
 %
 % Reference: 
 %   W. B. Van Berlekom and T. A. and Goddard (1972). Maneuvering of Large
@@ -14,14 +14,14 @@ function SIMtanker()
 % Author:    Thor I. Fossen
 % Date:      2024-05-02
 % Revisions:
-%   None
 
 close all;
 clearvars;
 
 %% USER INPUTS
-t_f = 3000;             % Final simulation time (sec)
-h   = 0.1;              % Sample time (sec)
+% Define simulation parameters
+T_final = 2000;	                % Final simulation time (s)
+h = 0.1;                        % Sampling time (s)
 
 water_depth = 100;      % Water depth must be larger than draft 18.5 m
 psi_ref = deg2rad(5);   % Desired heading
@@ -37,19 +37,17 @@ psi_d = 0;
 displayControlMethod();
 
 %% MAIN LOOP
-N = round(t_f/h);                       % Number of samples
-simdata = zeros(N+1,length(x)+2);       % Memory allocation
+t = 0:h:T_final;                         % Time vector
+simdata = zeros(length(t), length(x)+2); % Pre-allocate matrix for efficiency
                 
-for i=1:N+1
-
-    time = (i-1) * h;                   % Simulation time in seconds
+for i=1:length(t)
 
     r   = x(3);
     psi = x(6);
     
     % Control system (constant thrust + PD heading controller)
-    if time > 1200
-        psi_d = exp(-h*wn) * psi_d + (1 - exp(-h*wn)) * psi_ref;
+    if t(i) > 600
+        psi_d = lowPassFilter(psi_d, psi_ref, wn, h);
     end
     delta_c = -Kp * ( ssa(psi-psi_d) + Td * r );  % PD controller
     n_c = 70;
@@ -58,7 +56,7 @@ for i=1:N+1
     [xdot,U] = tanker(x,[-delta_c n_c water_depth]);   % Tanker model
    
     % Store data for presentation
-    simdata(i,:) = [time, x', U]; 
+    simdata(i,:) = [x', U, psi_d]; 
     
     % Euler's integration method (k+1)
     x = euler2(xdot,x,h);                         
@@ -68,16 +66,16 @@ end
 %% PLOTS
 scrSz = get(0, 'ScreenSize'); % Returns [left bottom width height]
 
-t     = simdata(:,1);
-u     = simdata(:,2); 
-v     = simdata(:,3);          
-r     = rad2deg(simdata(:,4));   
-x     = simdata(:,5);
-y     = simdata(:,6);
-psi   = rad2deg(simdata(:,7));
-delta = rad2deg(simdata(:,8));
-n     = simdata(:,9);
-U     = simdata(:,10);
+u     = simdata(:,1); 
+v     = simdata(:,2);          
+r     = rad2deg(simdata(:,3));   
+x     = simdata(:,4);
+y     = simdata(:,5);
+psi   = rad2deg(simdata(:,6));
+delta = rad2deg(simdata(:,7));
+n     = simdata(:,8);
+U     = simdata(:,9);
+psi_d = rad2deg(simdata(:,10));
 
 % North-East positions
 figure(1); 
@@ -90,11 +88,13 @@ set(findall(gcf,'type','text'),'FontSize',14)
 % Ship speed, yaw rate, yaw angle, rudder angle, and propeller speed
 figure(2); 
 if ~isoctave; set(gcf,'Position',[scrSz(3)/2,1,scrSz(3)/2.5,scrSz(4)]); end
-subplot(221),plot(t,r),xlabel('Time (s)')
+subplot(221)
+plot(t,r),xlabel('Time (s)')
 title('Yaw rate r (deg/s)'),grid
 subplot(222),plot(t,U),xlabel('Time (s)')
 title('Speed U (m/s)'),grid
-subplot(223),plot(t,psi),xlabel('Time (s)')
+subplot(223)
+plot(t,psi,t,psi_d),xlabel('Time (s)')
 title('Yaw angle \psi (deg)'),grid
 subplot(224)
 plot(t,delta),xlabel('Time (s)')
