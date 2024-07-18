6b2862f80d3db59323980bbf135ffe424c549658
Thor I. Fossen <thor.fossen@ntnu.no>
2024-07-18 10:04:44 +0200

    Update exINS_MEKF.m

--- mssExamples/exINS_MEKF.m
+++ mssExamples/exINS_MEKF.m
@@ -29,51 +29,51 @@
 %            29 Nov 2020, new Kalman filter weights
 
 %% USER INPUTS
-f_s    = 100;     % sampling frequency [Hz]
-f_gnss = 1;       % GNSS measurement frequency [Hz]
+T_final = 100;	  % Final simulation time (s)
+f_s    = 100;     % Sampling frequency (Hz)
+f_gnss = 1;       % GNSS measurement frequency (Hz)
 
 % Flags
 mag = 0;          % 0 = compass, 1 = magnetometer 
 vel = 1;          % 0 = no velocity meaurement, 1 = velocity aiding
 
 % Parameters
-Z = f_s/f_gnss;   % ratio betwween sampling/IMU frequencies
-h  = 1/f_s; 	  % sampling time: h  = 1/f_s (s) 
+Z = f_s/f_gnss;   % Ratio betwween sampling/IMU frequencies
+h  = 1/f_s; 	  % Sampling time: h  = 1/f_s (s) 
 h_gnss = 1/f_gnss;  
 
-% simulation parameters
-N  = 20000;		  % no. of iterations
+% Biases
 b_acc = [0.1 0.3 -0.1]';
 b_ars = [0.05 0.1 -0.05]';
    
-% initial values for x for signal generator
+% Initial values for x for signal generator
 x = [zeros(1,6) b_acc' zeros(1,3) b_ars']';	        
 
-% initialization of Kalman filter
+% Initialization of Kalman filter
 P_prd = eye(15);
 
-% process noise weights: v, acc_bias, w, ars_bias
+% Process noise weights: v, acc_bias, w, ars_bias
 Qd = diag([0.01 0.01 0.01 0.01 0.01 0.01 0.1 0.1 0.1 0.001 0.001 0.001]);
    
-if (vel == 0 && mag == 1)  % position aiding + magnetometer
+if (vel == 0 && mag == 1)  % Position aiding + magnetometer
      
    Rd = diag([1 1 1  1 1 1 0.01 0.01 0.01]);  % p, acc, mag  
    
-elseif  (vel == 1 && mag == 1)  % position/velocity aiding + magnetometer 
+elseif  (vel == 1 && mag == 1)  % Position/velocity aiding + magnetometer 
      
    Rd = diag([1 1 1  0.1 0.1 0.1  1 1 1  0.01 0.01 0.01]);  % p, v, acc, mag
    
-elseif (vel == 0 && mag == 0)  % position aiding + compass 
+elseif (vel == 0 && mag == 0)  % Position aiding + compass 
     
    Rd = diag([1 1 1  1 1 1  0.01]);  % p, acc, psi
    
-else % position/velocity aiding + compass 
+else % Position/velocity aiding + compass 
       
    Rd = diag([1 1 1  1 1 1  1 1 1  0.01]);  % p, vel, acc, psi
 
 end
 
-% initialization of INS
+% Initialization of INS
 p_ins = [0 0 0]'; 
 v_ins = [0 0 0]';
 b_acc_ins = [0 0 0]';
@@ -81,8 +81,12 @@ q_ins = euler2q(0, 0, 0);
 b_ars_ins = [0 0 0]';
 x_ins = [p_ins; v_ins; b_acc_ins; q_ins; b_ars_ins];
 
+% Time vector initialization
+t = 0:h:T_final;                % Time vector from 0 to T_final          
+nTimeSteps = length(t);         % Number of time steps
+
 % WGS-84 gravity model
-mu = 63.4305 * pi / 180;    % lattitude  
+mu = 63.4305 * pi / 180;    % Lattitude  
 g = gravity(mu);  
 
 %% Display
@@ -105,53 +109,52 @@ disp('----------------------------------------------------------');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% MAIN LOOP
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-simdata = zeros(N+1,32);                  % table of simulation data
-ydata = [0 x(1:3)'];                      % table of position measurements
+simdata = zeros(nTimeSteps,31); % Pre-allocate table of simulation data
+ydata = [0 x(1:3)'];            % Pre-allocatetable of position measurements
 
-for i=1:N+1
+for i=1:nTimeSteps
     
-    % INS signal generator
-    t = (i-1) * h;                      % time (s)   
-    [x, f_imu, w_imu, m_imu, m_ref] = insSignal(x, mu, h, t);
+    % INS signal generator  
+    [x, f_imu, w_imu, m_imu, m_ref] = insSignal(x, mu, h, t(i));
     y_psi = x(12);
     
     % GNSS measurements are Z times slower than the sampling time
-    if mod( t, h_gnss ) == 0
+    if mod( t(i), h_gnss ) == 0
         
-        y_pos = x(1:3) + 0.05 * randn(3,1);   % position measurements
-        y_vel = x(4:6) + 0.01 * randn(3,1);   % optionally velocity meas.
-        ydata = [ydata; t, y_pos'];           % store position measurements                  
+        y_pos = x(1:3) + 0.05 * randn(3,1);   % Position measurements
+        y_vel = x(4:6) + 0.01 * randn(3,1);   % Optionally velocity meas.
+        ydata = [ydata; t(i), y_pos'];        % Store position measurements                  
               
-        if (vel == 0 && mag == 1)  % position aiding + magnetometer
+        if (vel == 0 && mag == 1)  % Position aiding + magnetometer
             
             [x_ins,P_prd] = ins_mekf(...
                 x_ins,P_prd,mu,h,Qd,Rd,f_imu,w_imu,m_imu,m_ref,y_pos);
             
-        elseif (vel == 1 && mag == 1)  % position/velocity aiding + magnetometer
+        elseif (vel == 1 && mag == 1)  % Position/velocity aiding + magnetometer
             
              [x_ins,P_prd] = ins_mekf(...
                  x_ins,P_prd,mu,h,Qd,Rd,f_imu,w_imu,m_imu,m_ref,y_pos,y_vel);
         
-        elseif (vel == 0 && mag == 0)  % position aiding + compass
+        elseif (vel == 0 && mag == 0)  % Position aiding + compass
             
               [x_ins,P_prd] = ins_mekf_psi(...
                  x_ins,P_prd,mu,h,Qd,Rd,f_imu,w_imu,y_psi,y_pos);
              
-        else  % position/velocity aiding + compass
+        else  % Position/velocity aiding + compass
             
               [x_ins,P_prd] = ins_mekf_psi(...
                  x_ins,P_prd,mu,h,Qd,Rd,f_imu,w_imu,y_psi,y_pos,y_vel);
              
         end
         
-    else  % no aiding
+    else  % No aiding
         
-        if (mag == 1)  % magnetometer
+        if (mag == 1)  % Magnetometer
         
            [x_ins,P_prd] = ins_mekf(...
                x_ins,P_prd,mu,h,Qd,Rd,f_imu,w_imu,m_imu,m_ref);
              
-        else           % compass
+        else           % Compass
             
            [x_ins,P_prd] = ins_mekf_psi(...
                x_ins,P_prd,mu,h,Qd,Rd,f_imu,w_imu,y_psi);          
@@ -160,17 +163,14 @@ for i=1:N+1
         
     end
        
-    % store simulation data in a table (for testing)
-    simdata(i,:) = [t x' x_ins']; 
+    % Store simulation data in a table (for testing)
+    simdata(i,:) = [x' x_ins']; 
     
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% PLOTS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-t     = simdata(:,1);           
-x     = simdata(:,2:16); 
-x_hat = simdata(:,17:32); 
+%% PLOTS          
+x     = simdata(:,1:15); 
+x_hat = simdata(:,16:31); 
 
 phi = zeros(length(t),1);theta = zeros(length(t),1);psi = zeros(length(t),1);
 for i = 1:length(t)
@@ -178,7 +178,7 @@ for i = 1:length(t)
 end
 Theta = [phi theta psi];
 
-t_m = ydata(:,1);              % slow GNSS measurements
+t_m = ydata(:,1);              % Slow GNSS measurements
 y_m = ydata(:,2:4);
 
 figure(1); figure(gcf)
