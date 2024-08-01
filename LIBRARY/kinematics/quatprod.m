6c3a2c102e9deac2b796887b6fdee491d5024800
Thor I. Fossen <thor.fossen@ntnu.no>
2024-08-01 08:58:36 +0200

    Update quatprod.m

--- LIBRARY/kinematics/quatprod.m
+++ LIBRARY/kinematics/quatprod.m
@@ -1,14 +1,22 @@
-function q = quatprod(q1,q2)
-% q = quatprod(q1,q2) computes the quaternion product
+function q = quatprod(q1, q2)
+% quatprod is compatible with MATLAB and GNU Octave (www.octave.org).
+% This function calculates the quaternion product q = q1 o q2 of two 
+% unit quaternions q1 and q2. The quaternions are assumed to be in the 
+% scalar-first format, where the first element is the real part, and the 
+% remaining three elements are the imaginary parts.
 %
-% Author:   Thor I. Fossen
-% Date:     10th September 2010
-% Revisions: 
+% Author: Thor I. Fossen
+% Date: 2010-09-10
+% Revisions:
 
-eta1  = q1(1); 
-eps1  = q1(2:4);
-eta2  = q2(1); 
-eps2  = q2(2:4);
+q1 = q1(:);  % Ensure q1 is a column vector
+q2 = q2(:);  % Ensure q2 is a column vector
 
-q = [eta1*eta2-eps1'*eps2
-     eta2*eps1+eta1*eps2+cross(eps1,eps2)];
+eta1 = q1(1); 
+eps1 = q1(2:4);
+eta2 = q2(1); 
+eps2 = q2(2:4);
+
+q = [ eta1 * eta2 - eps1' * eps2;
+      eta2 * eps1 + eta1 * eps2 + cross(eps1, eps2) ];
+end
