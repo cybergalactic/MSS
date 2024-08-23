a8fc327a2e00aeb8ca52291e6ae5fb17284e8b9b
Thor I. Fossen <thor.fossen@ntnu.no>
2024-08-23 12:13:28 +0200

    Update insSignal.m

--- INS/functions/insSignal.m
+++ INS/functions/insSignal.m
@@ -14,56 +14,64 @@ function [x, f_imu, w_imu, m_imu, m_ref] = insSignal(x, mu, h, t_k)
 %         Theta - Euler angles (phi, theta, psi), 3 elements.
 %         b_ars^n - Attitude Rate Bias (ARS) in the BODY frame, 3 elements.
 %
-%   mu  : Latitude in radians.
-%   h   : Sampling time in seconds.
-%   t_k : Current time in seconds.
+%   mu   : Latitude in radians.
+%   h    : Sampling time in seconds.
+%   t_k  : Current time t[k] in seconds.
 %
 % Outputs:
-%   x[k1] : Signal vector, 15 elements.
-%   f_imu : IMU specific force vector expressed in the BODY frame, units in meters/second^2.
-%   w_imu : IMU attitude rate vector expressed in the BODY frame, units in radians/second.
-%   m_imu : IMU magnetic field vector expressed in the BODY frame, units in nanoTesla (nT).
-%   m_ref : Reference magnetic field vector expressed in the NED frame, specific to Trondheim, Norway, units in nT.
+%   x[k+1]     : Propagated signal vector, 15 elements
+%   f_imu[k+1] : IMU specific force vector expressed in the BODY frame, 
+%                units in meters/second^2.
+%   w_imu[k+1] : IMU attitude rate vector expressed in the BODY frame, 
+%                units in radians/second.
+%   m_imu[k+1] : IMU magnetic field vector expressed in the BODY frame, 
+%                units in nanoTesla (nT).
+%   m_ref      : Reference magnetic field vector expressed in the NED frame, 
+%                specific to Trondheim, Norway, units in nT.
 %
 % Author:
 %   Thor I. Fossen
 % Date:
 %   2020-04-28
 % Revisions:
-%   None
+%   2024-08-23 : Use RK4 for state propagation, update signal generators
 
-g_n = [0 0 gravity(mu)]';                 % NED gravity vector
-g_b = Rzyx(x(10),x(11),x(12))' * g_n;     % Transforming the gravity vector 
-                                          % from NED to BODY
+m_ref = [13559 921 50209]'; % Reference magnetic field, Trondheim, Norway
+g_n = [0 0 gravity(mu)]'; % NED gravity vector for specified lattitude mu
 
-f_true = [0.1 * sin(0.1*t_k)              % True specific force: f = a - g
-          0.1 * cos(0.1*t_k)
-          0.05 * sin(0.05*t_k)] - g_b;
+% True specific force: f[k] = a[k] - g
+f_true = @(t, g_n, x) [
+    0.1 * sin(0.1 * t);  
+    0.1 * cos(0.1 * t);
+    0.05 * sin(0.05 * t)] - Rzyx(x(10),x(11),x(12))' * g_n;
 
-w_true = [0.02 * cos(0.2*t_k)             % True angular rate
-          -0.02 * sin(0.1*t_k)
-           0.01 * sin(0.1*t_k)];
+% True angular rate: w[k]
+w_true = @(t) [
+    0.02 * cos(0.2 * t);
+   -0.02 * sin(0.1 * t);
+    0.01 * sin(0.1 * t) ] ;
 
-x_dot = [x(4:6)                           % Derivative of true states
-         Rzyx(x(10),x(11),x(12)) * f_true + g_n
-         zeros(3,1)
-         Tzyx(x(10),x(11)) * w_true
-         zeros(3,1)];
+% Define vehicle dynamics as an inline function
+dynamics = @(x, t) [
+    x(4:6); % Linear velocity 
+    Rzyx(x(10),x(11),x(12)) * f_true(t,g_n,x) + g_n; % Linear acceleration 
+    zeros(3,1); % Acceleration bias (static in this model)
+    Tzyx(x(10),x(11)) * w_true(t); % Angular rates 
+    zeros(3,1)]; % Angular rate bias (static in this model)
 
-% Simulation of IMU measurements with noise addition
-w1 = 0.01 * randn(3,1);         % Gaussian white noise for specific force
-w2 = 0.01 * randn(3,1);         % Gaussian white noise for angular rate
-b_acc = x(7:9);
-b_ars = x(13:15);
-f_imu = f_true + b_acc + w1;   % IMU specific force output
-w_imu = w_true + b_ars + w2;   % IMU angular rate output
+% Propagate states: x[k+1]
+x = rk4(dynamics, h, x, t_k);
+
+% IMU inertial measurements at time t_k+1 with noise addition
+w1 = 0.01 * randn(3,1); % Gaussian white noise for specific force
+w2 = 0.01 * randn(3,1); % Gaussian white noise for angular rate
+b_acc = x(7:9); % ACC bias: b_acc[k+1]
+b_ars = x(13:15); % ARS bias: b_ars[k+1]
+f_imu = f_true(t_k+h, g_n, x) + b_acc + w1; % IMU specific force: f_imu[k+1]
+w_imu = w_true(t_k+h) + b_ars + w2; % IMU angular rate: w_imu[k+1]
 
 % Calculation of magnetic field readings
-w3 = 0.01 * randn(3,1);        % Gaussian white noise for magnetic field
-m_ref = [13559 921 50209]';    % Reference magnetic field, Trondheim, Norway
-m_imu = Rzyx(x(10),x(11),x(12))' * m_ref + w3; % IMU magnetic field output
-
-% Propagation of the signal vector
-x = x + h * x_dot;
+w3 = 0.01 * randn(3,1); % Gaussian white noise for magnetic field
+m_imu = Rzyx(x(10),x(11),x(12))' * m_ref + w3; % IMU magnetic field: m_imu[k+1]
 
 end
