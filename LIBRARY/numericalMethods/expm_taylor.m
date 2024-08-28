a925e104f43e7fe7100ae9ba8fae940c00a75572
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2024-08-28 13:35:06 +0200

    Update expm_taylor.m

--- LIBRARY/numericalMethods/expm_taylor.m
+++ LIBRARY/numericalMethods/expm_taylor.m
@@ -1,8 +1,10 @@
 function E = expm_taylor(A)
 % expm_taylor is compatible with MATLAB and GNU Octave (www.octave.org).
-% The function computes the matrix exponential using a Taylor series 
-% expansion. It serves as a replacement for the built-in expm function, 
-% particularly in environments where a custom implementation is required.
+% The function computes the matrix exponential using a truncated Taylor series 
+% expansion. The function computes the matrix exponential by summing the terms of
+% the Taylor series until the norm of the term being added is smaller than a 
+% tolerance (1e-6 in this case)It serves as a replacement for the built-in expm
+% function, particularly in environments where a custom implementation is required.
 %
 % Inputs:
 %   A - A square matrix of size n x n, for which the matrix exponential is 
@@ -17,18 +19,11 @@ function E = expm_taylor(A)
 %
 % Example: INS unit quaternion propagation:
 %   q_ins = expm_taylor( Tquat(w_ins) * h ) * q_ins; % Exact discretization
-%   q_ins = q_ins / sqrt(q_ins' * q_ins);           % Normalization
+%   q_ins = q_ins / sqrt(q_ins' * q_ins);            % Normalization
 %
-% Notes:
-%   - The function computes the matrix exponential by summing the terms of
-%     the Taylor series until the norm of the term being added is smaller
-%     than a tolerance (1e-6 in this case).
-%   - For very large matrices or matrices with large elements, this method 
-%     may converge slowly compared to more advanced techniques used in the 
-%     built-in expm.
-%
-% See also:
-%   expm, factorial
+% Author: Thor I. Fossen
+% Date: 2024-08-28
+% Revisions:
 
 n = size(A, 1);  % Assume A is square
 E = eye(n);  % Start with the identity matrix
