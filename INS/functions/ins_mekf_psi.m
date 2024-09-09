458bd1cdce98b4bfd4f1a13507ec58a73e1ba42f
Thor I. Fossen <thor.fossen@ntnu.no>
2024-09-09 20:21:46 +0200

    Update ins_mekf_psi.m

--- INS/functions/ins_mekf_psi.m
+++ INS/functions/ins_mekf_psi.m
@@ -5,12 +5,12 @@ function [x_ins, P_prd] = ins_mekf_psi(...
 % (ESKF) specifically for Inertial Navigation Systems (INS) that are 
 % aided by compass and positional data. Attitude is parametrized using the 
 % 4-parameter unit quaternion representation and the Gibbs vector in the 
-% Multiplicative Error State Kalman Filter (MEKF) formulation, thus 
-% avoiding gimbal lock. 
+% Multiplicative Error State Kalman Filter (MEKF) formulation, thus avoiding
+% gimbal lock. 
 %
-% Usage scenarios are detailed in examples SIMaidedINSquat and ExINS_MEKF, 
-% demonstrating the implementation of the Kalman filter loop using the 
-% corrector-predictor representation:
+% Usage scenarios are detailed in SIMaidedINSquat.m demonstrating the 
+% implementation of the Kalman filter loop using the corrector-predictor 
+% representation:
 %
 %   - With new slow position measurements:
 %       [x_ins,P_prd] = ins_mekf_psi(...
@@ -36,9 +36,9 @@ function [x_ins, P_prd] = ins_mekf_psi(...
 %   h        : Sampling time in seconds.
 %   Qd, Rd   : Process and measurement noise covariance matrices for the 
 %              Kalman filter.
-%   f_imu[k] : Specific force measurements from the IMU.
-%   w_imu[k] : Angular rate measurements from the IMU.
-%   y_psi[k] : Fast compass measurement (yaw angle).
+%   f_imu[k] : High-rate IMU specific force measurements.
+%   w_imu[k] : High-rate IMU angular rate measurements. 
+%   y_psi[k] : Compass measurement (yaw angle).
 %   y_pos[k] : Slow position measurements aids the filter.
 %   y_vel[k] : (Optionally) Slow velocity measurements aids the filter.
 %
@@ -58,10 +58,11 @@ function [x_ins, P_prd] = ins_mekf_psi(...
 %   2022-08-30: Use atan2 instead of atan to avoid jumps in the formula:
 %               eps_psi = ssa( y_psi - atan2(u_y, u_x) ); 
 %   2024-08-31: Sign correction for v10 and using invQR.m instead of inv.m
+%   2024-09-09 : Redesign for slower compass measurements
 
 % Bias time constants (user specified)
-T_acc = 1000; 
-T_ars = 1000; 
+T_acc = 500; 
+T_ars = 500; 
 
 %% ESKF states and matrices
 p_ins = x_ins(1:3);          % INS states
@@ -97,27 +98,30 @@ A = [ O3 I3  O3            O3              O3
    
 % Ad = eye(15) + h * A + 0.5 * (h * A)^2 + ...
 Ad = expm_taylor(A * h); 
-
-% Linearization of the compass measurement equation
-a = (2/q_ins(1)) * [q_ins(2) q_ins(3) q_ins(4)]';      % 2 x Gibbs vector
-u_y = 2 * ( a(1)*a(2) + 2*a(3) );
-u_x = ( 4 + a(1)^2 - a(2)^2 - a(3)^2 );
-u = u_y / u_x;
-du = 1 / (1 + u^2);
-c_psi = du * (1 / ( 4 + a(1)^2 - a(2)^2 - a(3)^2 )^2 ) * ...
-        [ -2*((a(1)^2 + a(3)^2 - 4)*a(2) + a(2)^3 + 4*a(1)*a(3)) 
-           2*((a(2)^2 - a(3)^2 + 4)*a(1) + a(1)^3 + 4*a(2)*a(3)) 
-           4*((a(3)^2 + a(1)*a(2)*a(3) + a(1)^2 - a(2)^2 + 4)) ];  
-     
+       
 if (nargin == 10)
     Cd = [ I3 O3 O3 O3 O3                 % NED positions (x, y, z)
-           O3 O3 O3 Smtrx(R'*v01) O3      % Gravity           
-           zeros(1,9) c_psi' zeros(1,3)]; % Compass
+           O3 O3 O3 Smtrx(R'*v01) O3 ];   % Gravity           
 else
     Cd = [ I3 O3 O3 O3 O3                 % NED positions (x, y, z)
            O3 I3 O3 O3 O3                 % NED velocities 
-           O3 O3 O3 Smtrx(R'*v01) O3      % Gravity          
-           zeros(1,9) c_psi' zeros(1,3)]; % Compass
+           O3 O3 O3 Smtrx(R'*v01) O3 ];   % Gravity          
+end
+
+% Linearization of the compass measurement equation if y_psi is received
+if ~isempty(y_psi)
+    a = (2/q_ins(1)) * [q_ins(2) q_ins(3) q_ins(4)]'; % 2 x Gibbs vector
+    u_y = 2 * ( a(1)*a(2) + 2*a(3) );
+    u_x = ( 4 + a(1)^2 - a(2)^2 - a(3)^2 );
+    u = u_y / u_x;
+    du = 1 / (1 + u^2);
+    c_psi = du * (1 / ( 4 + a(1)^2 - a(2)^2 - a(3)^2 )^2 ) * ...
+        [ -2*((a(1)^2 + a(3)^2 - 4)*a(2) + a(2)^3 + 4*a(1)*a(3))
+        2*((a(2)^2 - a(3)^2 + 4)*a(1) + a(1)^3 + 4*a(2)*a(3))
+        4*((a(3)^2 + a(1)*a(2)*a(3) + a(1)^2 - a(2)^2 + 4)) ];
+
+    Cd = [ Cd
+        zeros(1,9) c_psi' zeros(1,3)]; % Augment the compass measurement to Cd
 end
 
 Ed = h *[  O3 O3    O3 O3
@@ -128,11 +132,8 @@ Ed = h *[  O3 O3    O3 O3
 
 %% Kalman filter algorithm       
 if (nargin == 9)               % No aiding
-    
     P_hat = P_prd;
-    
-else                           % INS aiding 
-    
+else                           % Aiding    
     % KF gain: K[k]
     K = P_prd * Cd' * invQR(Cd * P_prd * Cd' + Rd); 
     IKC = eye(15) - K * Cd;
@@ -140,7 +141,7 @@ else                           % INS aiding
     % BODY-fixed normalized IMU measurements (forward-starboard-down)
     v1 = f_ins / norm(f_ins);  % Specific force measurement
     
-    % Estimation error: eps[k]
+    % Estimation errors (injection terms)
     eps_pos = y_pos - p_ins;
     eps_g   = v1 - R' * v01; 
     
@@ -168,8 +169,7 @@ else                           % INS aiding
 	b_ars_ins = b_ars_ins + delta_x_hat(13:15);  % Reset ARS bias
      
     q_ins = quatprod(q_ins, delta_q_hat);        % Schur product    
-    q_ins = q_ins / norm(q_ins);                 % Normalization       
-    
+    q_ins = q_ins / norm(q_ins);                 % Normalization          
 end
 
 % Predictor: P_prd[k+1]
@@ -185,7 +185,7 @@ v_ins = v_ins + h * a_ins;                   % Exact discretization
 % of the quaternion differential equation: 
 %    q_ins_dot = Tquat(w_ins) * q_ins
 % You can replace the build-in Matlab function expm.m with the custom-made 
-% MSS functions expm_taylor.m or expm_squaresPade.m for this computation.
+% MSS function expm_squaresPade.m for this computation.
 q_ins = expm( Tquat(w_ins) * h ) * q_ins;    % Exact discretization
 q_ins = q_ins / norm(q_ins);                 % Normalization
 
