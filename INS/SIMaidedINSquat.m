dfd36e1bc2ccb69c772cf9bbb573fb6a1faf7dca
Thor I. Fossen <thor.fossen@ntnu.no>
2024-11-02 11:52:16 +0100

    Revert "Update SIMaidedINSquat.m"
    This reverts commit f65bfb42f3cfbfc0e88b5fd3119c94eb61bdbc85.

--- INS/SIMaidedINSquat.m
+++ INS/SIMaidedINSquat.m
@@ -32,29 +32,32 @@ function SIMaidedINSquat()
 % Revisions:
 %   2024-08-20 : Using the updated insSignal.m generator.
 %   2024-09-09 : Redesign for slower magnetometer measurements.
-%   2024-11-02 : Improved logic for slow position and magnetometer data
 clearvars;
 
 %% USER INPUTS
 T_final = 100; % Final simulation time (s)
-f_s = 1000; % Sampling frequency, equal to the IMU measurement frequency (Hz)
-f_mag = 100; % Magnetometer measurement frequency (Hz)
-f_pos = 5; % Position measurement frequency (Hz)
+f_s    = 1000; % Sampling frequency, equal to the IMU measurement frequency (Hz)
+f_mag  = 100;  % Magnetometer measurement frequency (Hz)
+f_pos = 5;     % Position measurement frequency (Hz)
 
 % Sampling times in seconds
 h  = 1/f_s; 	
 h_pos = 1/f_pos; 
 h_mag = 1/f_mag;
 
-% Initial values for signal generator
-[m_ref, ~, mu, cityName] = magneticField(1); % Magntic field and latitude for city #1
-b_acc = [0.1 0.3 -0.1]'; % IMU biases
-b_ars = [0.05 0.1 -0.05]'; % Initial values for signal generator
-x = [zeros(1,6) b_acc' zeros(1,3) b_ars']';	
+% Magntic field and latitude for city #1, see magneticField.m
+[m_ref, ~, mu, cityName] = magneticField(1);
 
 % Display simulation options
 [attitudeFlag, velFlag] = displayMethod(cityName);
  
+% IMU biases
+b_acc = [0.1 0.3 -0.1]';
+b_ars = [0.05 0.1 -0.05]';
+   
+% Initial values for signal generator
+x = [zeros(1,6) b_acc' zeros(1,3) b_ars']';	        
+
 % Initialization of ESKF covariance matrix
 P_prd = eye(15);
 
@@ -84,15 +87,12 @@ b_ars_ins = [0 0 0]';
 x_ins = [p_ins; v_ins; b_acc_ins; q_ins; b_ars_ins];
 
 % Time vector initialization
-t_slow_pos = 0; % Initialize the time for the next slow position measurement
-t_slow_mag = 0; % Initialize the time for the next slow magnetometer measurement
-t = 0:h:T_final; % Time vector from 0 to T_final          
-nTimeSteps = length(t); % Number of time steps
+t = 0:h:T_final;                % Time vector from 0 to T_final          
+nTimeSteps = length(t);         % Number of time steps
 
 %% MAIN LOOP
 simdata = zeros(nTimeSteps,31); % Pre-allocate table for simulation data
-posdata = zeros(floor(T_final * f_pos), 4); % Pre-allocate table for position data
-pos_index = 0; % Initialize index for posdata
+ydata = zeros(nTimeSteps,4);    % Pre-allocate table for position measurements
 
 for i=1:nTimeSteps
     
@@ -109,12 +109,11 @@ for i=1:nTimeSteps
     end
     
     % Position measurements are slower than the sampling time
-    if t(i) > t_slow_pos
+    if mod( t(i), h_pos ) == 0
         
-        pos_index = pos_index + 1; 
-        y_pos = x(1:3) + 0.05 * randn(3,1); % Position measurements
-        y_vel = x(4:6) + 0.01 * randn(3,1); % Optionally velocity meas.
-        posdata(pos_index,:) = [t(i), y_pos']; % Store position measurements                  
+        y_pos = x(1:3) + 0.05 * randn(3,1);   % Position measurements
+        y_vel = x(4:6) + 0.01 * randn(3,1);   % Optionally velocity meas.
+        ydata(i,:) = [t(i), y_pos'];          % Store position measurements                  
               
         if (velFlag == 1 && attitudeFlag == 2)
             % Position aiding + magnetometer
@@ -133,9 +132,6 @@ for i=1:nTimeSteps
             [x_ins,P_prd] = ins_mekf_psi(...
                 x_ins,P_prd,mu,h,Qd,Rd,f_imu,w_imu,y_psi,y_pos,y_vel);
         end
-
-        % Update the time for the next slow position measurement
-        t_slow_pos = t_slow_pos + h_pos;
         
     else  % No aiding
         
@@ -160,7 +156,7 @@ end
 scrSz = get(0, 'ScreenSize'); % Get screen dimensions
 legendSize = 12;
          
-x     = simdata(:,1:15); % High-rate IMU data
+x     = simdata(:,1:15); 
 x_hat = simdata(:,16:31); 
 
 phi = zeros(length(t),1);theta = zeros(length(t),1);psi = zeros(length(t),1);
@@ -169,8 +165,8 @@ for i = 1:length(t)
 end
 Theta = [phi theta psi];
 
-t_m = posdata(:,1); % Slow-rate position measurements
-y_m = posdata(:,2:4);
+t_m = ydata(:,1); % Slow position measurements
+y_m = ydata(:,2:4);
 
 figure(1); 
 if ~isoctave; set(gcf,'Position',[1,1,0.4*scrSz(3),scrSz(4)]); end
