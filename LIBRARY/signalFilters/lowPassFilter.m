28863c80bb31607dce16cabeaaa494bf05e534a3
Thor I. Fossen <thor.fossen@ntnu.no>
2024-11-30 13:51:37 +0100

    Update lowPassFilter.m

--- LIBRARY/signalFilters/lowPassFilter.m
+++ LIBRARY/signalFilters/lowPassFilter.m
@@ -1,9 +1,9 @@
-function xf = lowPassFilter(xf, u, w_n, h)
+function [xf_next, y] = lowPassFilter(xf, u, w_n, h)
 % lowPassFilter is compatible with MATLAB and GNU Octave (www.octave.org).
 % This function implements a first-order low-pass filter using exact
-% discretization. The filter smooths the input signal `u` and updates
-% the filtered output `xf` based on the natural frequencies `w_n` and the
-% sampling time `h`. This version supports both scalar and vector inputs.
+% discretization. The filter smooths the input signal u[k] and updates
+% the filtered output y[k] based on the natural frequencies w_n and the
+% sampling time h. This version supports both scalar and vector inputs.
 % The filter transfer function is:
 %
 %    h_lowPass(s) = w_n / (s + w_n)
@@ -16,22 +16,25 @@ function xf = lowPassFilter(xf, u, w_n, h)
 %   h    - The sampling time in seconds (scalar).
 %
 % Output:
-%   xf   - Filtered output at time k+1 (scalar or vector).
+%   xf_next - Propagated filter state at time k+1 (scalar or vector)
+%   y       - Filtered output at time k (scalar or vector)
 %
 % Author: Thor I. Fossen
 % Date: 2024-04-26
 % Revisions:
-%   None
 
 % Ensure w_n is a vector if a diagonal matrix is provided
 if ~isvector(w_n)
     w_n = diag(w_n);
 end
 
+%  Filtered output at time k 
+y = xf;
+
 % Compute the filter coefficient
 phi = exp(-h * w_n);
 
 % Update the filtered value at time k+1
-xf = phi .* xf + (1 - phi) .* u;
+xf_next = phi .* xf + (1 - phi) .* u;
 
 end
