fc020a346edabf8d291e3156816eab4365a76552
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2025-06-17 23:15:48 +0200

    Update quatObserver.m

--- INS/functions/quatObserver.m
+++ INS/functions/quatObserver.m
@@ -103,7 +103,7 @@ w_imu = imu_meas(4:6)';
 
 % Nonlinear injection terms (low rate)
 if length(imu_meas) == 9
-    % If new IMU measuremenst + magnetometer: [f_imu' w_imu' m_imu'] 
+    % IMU measuremenst with 3-axis magnetometer measurement: [f_imu' w_imu' m_imu'] 
     m_imu = imu_meas(7:9)'; % Magnetic field IMU measurements: m_imu[k] in BODY
     v01 = [0 0 -1]'; % Normalized gravity reference vector in NED (measuring -g at rest)
     v1 = f_imu / norm(f_imu); % Normalized specific force measurement
@@ -114,7 +114,7 @@ if length(imu_meas) == 9
     sigma = sigma1 + sigma2;
 
 elseif length(imu_meas) == 7 
-    % If new IME measurements + compass: [f_imu' w_imu' psi]  
+    % IMU measurements with scalar compass meausrement: [f_imu' w_imu' psi]  
     psi = imu_meas(7);  % Compass measurement: psi[k]
     v01 = [0 0 -1]'; % Normalized gravity reference vector in NED (measuring -g at rest)
     v1 = f_imu / norm(f_imu); % Normalized specific force measurement
