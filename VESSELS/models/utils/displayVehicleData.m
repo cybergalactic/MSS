90bef3e6b7bfc9d949574dc044ecdbe8bac141fe
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2025-10-08 10:18:03 +0200

    Update displayVehicleData.m

--- VESSELS/models/utils/displayVehicleData.m
+++ VESSELS/models/utils/displayVehicleData.m
@@ -5,52 +5,54 @@ function displayVehicleData(vehicleName, vehicleData, imageFile, figNo)
 %
 % Inputs:
 %   vehicleName - A string representing the name of the vehicle 
-%                 (e.g., 'Remus 100 AUV')
 %   vehicleData - A cell array of key-value pairs representing the vehicle's 
-%                 characteristics (e.g., {'Length', '1.6 m', 
-%                 'Diameter', '19 cm', 'Mass', '31.9 kg', ...})
-%   imageFile   - A string representing the file name of the vehicle image 
-%                 located in .../MSS/SIMULINK/figs/ (e.g., 'remus100.jpg')
-%   figNo       - A number representing the figure number to use for the display
-%
-% Outputs:
-%   None
+%                 characteristics
+%   imageFile   - File name of the vehicle image (in path or MSS/SIMULINK/figs/)
+%   figNo       - Figure number
 %
 % Author:     Thor I. Fossen
 % Date:       2024-06-07
 
-% Create the heading text
-Heading = sprintf('%-30s\n%-30s\n', 'MSS Toolbox', vehicleName);
-
-% Create the data text with proper alignment
-numEntries = length(vehicleData) / 2;
-formatSpec = repmat('%-30s : %s\n', 1, numEntries);
+% Heading and table text
+Heading = sprintf('%s\n%s\n', 'MSS Toolbox', vehicleName);
+numEntries = numel(vehicleData)/2;
+formatSpec = repmat('%-25s : %s\n', 1, numEntries);
 MSS_text = sprintf(formatSpec, vehicleData{:});
 
-% Create a figure window
-figure(figNo); figure(gcf);
+% Figure window
+figure(figNo); clf;
+set(gcf, 'Name', vehicleName, 'NumberTitle', 'off', 'Color', 'w');
+set(gcf, 'Units', 'pixels');
+set(gcf, 'Position', [100 100 480 420]);   
+set(gcf, 'Resize', 'off');
 
-% Create an axes for the heading
-axes('Position', [0.1 0.8 0.8 0.1]);
-text(0, 1, Heading, ...
-    'FontSize', 20, ...
-    'FontWeight', 'bold', ...
+% Heading (top) 
+axes('Position', [0.06 0.84 0.88 0.12]);
+text(0, 0.95, Heading, ...
+    'FontSize', 13, 'FontWeight', 'bold', ...
     'FontName', 'Courier', ...
-    'HorizontalAlignment', 'left');
+    'HorizontalAlignment', 'left', 'VerticalAlignment', 'top');
 axis off;
 
-% Create an axes for the text
-axes('Position', [0.1 0.53 0.8 0.3]);
+% Vehicle info text (middle)
+axes('Position', [0.06 0.56 0.88 0.25]);
 text(0, 1, MSS_text, ...
-    'FontSize', 16, ...
-    'FontName', 'Courier', ...
-    'HorizontalAlignment', 'left', ...
-    'VerticalAlignment', 'top');
+    'FontSize', 11, 'FontName', 'Courier', ...
+    'HorizontalAlignment', 'left', 'VerticalAlignment', 'top', ...
+    'Interpreter', 'none');
 axis off;
 
-% Read and display the image
+% Image 
 filePath = which(imageFile);
-axes('Position', [0.1 0.05 0.8 0.4]);
-imshow(imread(filePath));
+axes('Position', [0.15 0.20 0.7 0.33]);  
+if isempty(filePath)
+    text(0, 0.5, sprintf('Image not found:\n%s', imageFile), ...
+        'FontSize', 11, 'Color', 'r', 'FontName', 'Courier', ...
+        'Interpreter', 'none');
+    axis off;
+else
+    img = imread(filePath);
+    imshow(img, 'InitialMagnification', 'fit');
+end
 
 end
