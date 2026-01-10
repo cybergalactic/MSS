e34a00cfc17efb064117d6c73b033d1cc8d0f96b
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2026-01-10 15:06:22 +0100

    Update rbody.m

--- LIBRARY/modeling/rbody.m
+++ LIBRARY/modeling/rbody.m
@@ -1,52 +1,45 @@
-function [MRB,CRB] = rbody(m,R44,R55,R66,nu2,r_bp)
-% [MRB,CRB] = rbody(m,R44,R55,R66,nu2,r_bp) computes the 6x6 rigid-body 
-% mass and Coriolis-centripetal matrices for an arbitrariliy point P with
-% respect to the CO (coordinate origin of the GNC system). If r_bp = [0 0 0]' 
-% the CG coincides with the CO and the matrices MRB and CRB are computed 
-% in the CO = CG. Else if the distance vector from the CO to the CG is 
-% r_bg = [xg, yg, zg]' use r_bp = r_bg.
-%
-% [MRB,CRB] = rbody(m,R44,R55,R66,nu2,[0, 0, 0]') computes MRB and CRB in CG
-% [MRB,CRB] = rbody(m,R44,R55,R66,nu2,r_bg) computes MRB and CRB in CO
+function [MRB,CRB] = rbody(m,R44,R55,R66,nu2,r_bG)
+% [MRB,CRB] = rbody(m,R44,R55,R66,nu2,r_bG) computes the 6x6 rigid-body
+% mass and Coriolis–centripetal matrices expressed at the body-fixed origin CO.
+% If r_bG = [0 0 0]', the center of gravity (CG) coincides with CO and the
+% matrices are computed about CO = CG.
 %
 % In general nu = [u, v, w, p, q, r]' but for a rigid body there exists a
-% CRB matrix which only depends on the angular velocity nu2 = [p, q, r,]'. 
-% In other words, CRB is independent of linear velocity nu1 = [u, v, w]'. 
-% This is particular useful for the relative equations of motion where 
-% nu_r = nu - nu_c and nu_c = [u_c, v_c, w_c, 0, 0, 0]' is the irrotational
-% current velocity. This implies that the marine craft equations of motion
-% expressed in the CO satisfies (Fossen 2021, Chapter 3.3.1):
-% 
-%  MRB * nudot + CRB(nu) * nu = MRB * nudot_r + CRB(nu_r) * nu_r = tau 
+% parametrization CRB = CRB(nu2) which is independent of the linear velocity
+% nu1 = [u, v, w]'. This is particularly useful for relative equations of
+% motion with irrotational current velocity nu_c = [u_c, v_c, w_c, 0, 0, 0]':
+%
+%   MRB*nudot + CRB(nu2)*nu = MRB*nudot_r + CRB(nu2_r)*nu_r = tau
+%
+% where nu_r = nu - nu_c and nu2_r is the angular part of nu_r.
 %
 % Outputs:
-%  MRB                 - Rigid-body mass matrix
-%  CRB = CRB(nu2)      - Coriolis-centripetal matrix, independet of nu1
+%  MRB            - Rigid-body mass matrix about CO
+%  CRB = CRB(nu2) - Coriolis–centripetal matrix about CO (independent of nu1)
 %
 % Inputs:
-%  nu2 = [p, q, r]'    - Angular velocity vector 
-%  R44, R55, R66       - Radii of gyration with respect to CG
-%  r_bp                - Vector from CO to an arbitrarily point P, e.g.
-%                        r_bp = [0, 0, 0]'    CO = CG
-%                        r_bp = r_bg = [xg, yg, zg]' vector from CO to CG
-% 
+%  m              - Mass
+%  R44,R55,R66    - Radii of gyration about the CG
+%  nu2 = [p,q,r]' - Angular velocity vector
+%  r_bG           - Lever arm from CO to CG, expressed in {b}
+%
 % Author:    Thor I. Fossen
 % Date:      2019-04-19
-% Revisions: 
+% Revisions:
 %  2021-04-21 - Improved the documentation
 
 I3 = eye(3);
 O3 = zeros(3,3);
 
-% MRB and CRB expressed in the CG
-I_g = m * diag([R44^2, R55^2, R66^2]);
-MRB_CG = [ m * I3  O3
-          O3      I_g ];
-CRB_CG = [ m * Smtrx(nu2)    O3
-           O3               -Smtrx(I_g*nu2)  ];
-      
-% Transform the matrices MRB and CRB from the CG to the CO           
-H = Hmtrx(r_bp);      
+% MRB and CRB expressed about the CG
+I_G   = m * diag([R44^2, R55^2, R66^2]);
+MRB_CG = [ m*I3   O3
+           O3     I_G ];
+
+CRB_CG = [ m*Smtrx(nu2)     O3
+           O3              -Smtrx(I_G*nu2) ];
+
+% Transform MRB and CRB from CG to CO using the screw transformation
+H   = Hmtrx(r_bG);
 MRB = H' * MRB_CG * H;
 CRB = H' * CRB_CG * H;
-
