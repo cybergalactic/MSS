6e546b79a614f4984e321327c314272d5966c266
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2026-05-14 16:18:07 +0200

    Update plotWD.m

--- HYDRO/plotWD.m
+++ HYDRO/plotWD.m
@@ -1,24 +1,20 @@
 function plotWD(vessel,x_axis,velno)
-% plotWD plots the wave drift amplitudes 
+% plotWD plots 2nd-order mean wave-drift amplitudes in surge, sway and yaw.
 %
 %    plotWD(vessel,x_axis,velno)      
 %
-%    plotWD(vessel,'s',1)      - period in seconds
+%    plotWD(vessel,'s',1)      - Period in seconds
 %    plotWD(vessel,'hz',1)     - 1/s
-%   plotWD(vessel,'rads',1)    - rad/s
+%    plotWD(vessel,'rads',1)   - rad/s
 %
 % Inputs: 
 %    vessel: MSS vessel structure
 %    x_axis {'rads','s','hz'}
-%    velno  (optionally):  speed number
+%    velno  (optionally): speed number
 %
 % Author:    Thor I. Fossen
 % Date:      2007-06-27
 % Revisions: 
-
-velocities = vessel.velocities;
-
-nvel  = length(velocities);
 
 if nargin == 2
     velno = 1;
@@ -29,7 +25,7 @@ end
 
 w     = vessel.driftfrc.w;
 amp   = vessel.driftfrc.amp;
-txt   = 'WD amplitude:';
+txt   = 'Mean wave-drift amplitude:';
 figno = 100;
 ytxt11 = 'kN/A^2';
 ytxt12 = 'kNm/A^2';
@@ -37,25 +33,24 @@ S1 = 1/1000;     % amp (N)  1-3 scaling
 S2 = 1/1000;     % amp (Nm) 4-6 scaling
 
 dim = size(amp{1});
-if dim(2) ~= 36,
+if dim(2) ~= 36
     disp('Error: can only plot RAO data for 0-360 deg with 10 deg interval')
     return
 end
 
-
-% surge
+% Surge
 figure(figno)
 arg = S1*amp{1}(:,:,velno);
 
 if strcmp(x_axis,'rads')
    xarg = w;
-   xtxt = 'wave frequency (rad/s)';
+   xtxt = 'Wave frequency (rad/s)';
 elseif strcmp(x_axis,'s')
     xarg = 2*pi./w;
-    xtxt = 'period (s)';
+    xtxt = 'Period (s)';
 else
     xarg = w/(2*pi);
-    xtxt = 'wave frequency (Hz)';    
+    xtxt = 'Wave frequency (Hz)';    
 end
 
 subplot(311)
@@ -66,7 +61,7 @@ xlabel(xtxt)
 ylabel(ytxt11)
 grid
 
-% sway
+% Sway
 arg = S1*amp{2}(:,:,velno);
 subplot(312)
 plot(xarg,arg(:,1),xarg,arg(:,4),xarg,arg(:,7),xarg,arg(:,10),xarg,arg(:,13),xarg,arg(:,16),xarg,arg(:,19))
@@ -76,7 +71,7 @@ xlabel(xtxt)
 ylabel(ytxt11)
 grid
 
-% yaw
+% Yaw
 subplot(313)
 arg = S2*amp{3}(:,:,velno);
 plot(xarg,arg(:,1),xarg,arg(:,4),xarg,arg(:,7),xarg,arg(:,10),xarg,arg(:,13),xarg,arg(:,16),xarg,arg(:,19))
@@ -85,4 +80,8 @@ legend('0 deg','30 deg','60 deg','90 deg','120 deg','150 deg','180 deg')
 xlabel(xtxt)
 ylabel(ytxt12)
 grid
+
+set(findall(gcf,'type','text'),'FontSize',12)
+set(findall(gcf,'type','legend'),'FontSize',10)
+set(findall(gcf,'type','line'),'linewidth',1.5)
 
