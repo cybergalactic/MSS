2009ed09df1aba396cf88e1801ae80de3e078f72
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2026-08-12 10:44:12 +0200

    Update crosstrack.m

--- GNC/crosstrack.m
+++ GNC/crosstrack.m
@@ -1,7 +1,7 @@
-function [x_p, y_p, y_e] = crosstrack(x_t, y_t, x_ref, y_ref, x, y, flag)
+function [x_proj, y_proj, y_e] = crosstrack(x_t, y_t, x_ref, y_ref, x, y, flag)
 % crosstrack is compatible with MATLAB and GNU Octave (www.octave.org). 
-% [x_p, y_p, y_e] = crosstrack(x_t, y_t, x_ref, y_ref, x, y, flag) computes 
-% the coordinate origin (x_p, y_p) of the path-tangential reference frame,
+% [x_proj, y_proj, y_e] = crosstrack(x_t, y_t, x_ref, y_ref, x, y, flag) computes 
+% the coordinate origin (x_proj, y_proj) of the path-tangential reference frame,
 % obtained as the orthogonal projection of the craft position (x, y) onto
 % the straight-line path from (x_ref, y_ref) to (x_t, y_t). The cross-track 
 % error y_e for a craft located at (x, y) is the signed distance from the path.
@@ -13,12 +13,12 @@ function [x_p, y_p, y_e] = crosstrack(x_t, y_t, x_ref, y_ref, x, y, flag)
 %   flag (optionally)   : 1 to plot the geometry, else no plot
 %
 % Outputs:  
-%   (x_p, y_p)          : Origin of path-tangential frame expressed in NED
+%   (x_proj, y_proj)    : Origin of path-tangential frame expressed in NED
 %   y_e                 : Cross-track error
 %
 % Usage:
-%   [xp, yp, ye] = crosstrack(150, 300, 0, 0, 80, 30, 1)   % Plot geometry
-%   [xp, yp, ye] = crosstrack(150, 300, 0, 0, 80, 30)      % No plot
+%   [x_proj, y_proj, ye] = crosstrack(150, 300, 0, 0, 80, 30, 1)  % Plot geometry
+%   [x_proj, y_proj, ye] = crosstrack(150, 300, 0, 0, 80, 30)     % No plot
 %
 % For GNC applications it is not necessary to compute (xp, yp) implying
 % that visualization and matrix inversion can be avoided. Hence, it is
@@ -40,18 +40,18 @@ p  = [x; y];              % Vehicle position (N, E)
 d  = p2 - p1;             % Direction vector (N, E)
 
 % Orthogonal projection of p onto line (p1, p2)
-t = dot(p - p1, d) / dot(d, d);
-p_p = p1 + t * d;
+PI = d * d' / norm(d)^2;
+p_proj = p1 + PI * (p - p1);
 
 % Output reference point
-x_p = p_p(1);  % North
-y_p = p_p(2);  % East
+x_proj = p_proj(1);  % North
+y_proj = p_proj(2);  % East
 
 % Path heading (angle from North axis)
 pi_h = atan2(d(2), d(1));  % atan2(East, North)
 
 % Cross-track error in NE frame
-y_e = -(x - x_p) * sin(pi_h) + (y - y_p) * cos(pi_h);
+y_e = -(x - x_proj) * sin(pi_h) + (y - y_proj) * cos(pi_h);
 
 if (nargin == 7 && flag == 1)
     axis normal;
@@ -60,9 +60,9 @@ if (nargin == 7 && flag == 1)
     hold on
     plot(y_ref,x_ref,'k>','MarkerFaceColor','c','MarkerSize',12,'LineWidth',2)
     plot(y_t,x_t,'rx','MarkerSize',12,'LineWidth',2)
-    plot(y_p,x_p,'ks','MarkerFaceColor','c','MarkerSize',12,'LineWidth',2)
+    plot(y_proj,x_proj,'ks','MarkerFaceColor','c','MarkerSize',12,'LineWidth',2)
     plot([y_ref y_t],[x_ref x_t],'k-','LineWidth',2)
-    plot([y_p y],[x_p x],'r-.','LineWidth',2)
+    plot([y_proj y],[x_proj x],'r-.','LineWidth',2)
     hold off
     grid;
     xlabel('East [m]')
