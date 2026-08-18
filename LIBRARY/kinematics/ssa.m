e48f5087f04efdcc4961cfa453a0f18017441d6e
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2026-08-18 14:42:30 +0200

    Update ssa.m

--- LIBRARY/kinematics/ssa.m
+++ LIBRARY/kinematics/ssa.m
@@ -8,7 +8,7 @@ function angle = ssa(angle,unit)
 %
 % For feedback control systems and state estimators used to control the 
 % attitude of vehicles, the difference of two angles should always be
-% mapped to [-pi pi) or [-180 180) to avoid step inputs/discontinuties.           
+% mapped to [-pi pi) or [-180 180) to avoid step inputs/discontinuities.           
 %
 % Note that in many languages (C, C++, C#, JavaScript), the modulus  
 % operator mod(x,y) returns a value with the same sign as x. 
@@ -18,9 +18,9 @@ function angle = ssa(angle,unit)
 % Author:     Thor I. Fossen
 % Date:       2018-09-21
 % Revisions:  
-%   2020-03-04  Deafult rad, optional argument for degrees.
+%   2020-03-04  Default rad, optional argument for degrees.
 %   2024-12-04  Allow rad as optional argument. Throw error for invalid
-%   unit. Author: Tor Børve Rasmussen.
+%               unit. Author: Tor Børve Rasmussen.
 
 if nargin == 1 || strcmp(unit, 'rad')
     angle = mod( angle + pi, 2 * pi ) - pi; 
