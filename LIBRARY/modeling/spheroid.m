99bf0b30e9ae0dca3515d02e0bbd06c54cc0c2f7
Thor I. Fossen <43878665+cybergalactic@users.noreply.github.com>
2026-08-27 14:36:10 +0200

    Update spheroid.m

--- LIBRARY/modeling/spheroid.m
+++ LIBRARY/modeling/spheroid.m
@@ -1,19 +1,19 @@
-function [MRB,CRB] = spheroid(a,b,nu2,r_bg)
-% [MRB,CRB] = spheroid(a,b,nu2,r_bg) computes the 6x6 rigid-body mass
+function [MRB,CRB] = spheroid(a,b,nu2,r_bG)
+% [MRB,CRB] = spheroid(a,b,nu2,r_bG) computes the 6x6 rigid-body mass
 % and Coriolis-centripetal matrices of a prolate spheroid of length
 % L = 2 * a and diameter D = 2 * b. The spheroid can be used to approximate 
 % a cylinder-shaped autonomous underwater vehicle (AUV). In general 
-% nu = [u,v,w,p,q,r]', while linear and angular velocities are denoted by
+% nu = [u, v, w, p, q, r]', while linear and angular velocities are denoted by
 % nu1 = [u, v, w]' and nu2 = [p, q, r]'. The CRB matrix is computed using 
-% the linear velocity-independent representation (Fossen 2021, Chapter 3.3.1)
+% the linear velocity-independent representation (Fossen 2027, Chapter 3.3.2)
 % according to:
 %
-%  [MRB,CRB] = spheriod(a, b,[p, q, r]',[xg, yg, zg]') 
+%  [MRB,CRB] = spheroid(a,b,[p, q, r]',[xG, yG, zG]') 
 %
 % This is particular useful for the relative equations of motion where 
 % nu_r = nu - nu_c and nu_c = [u_c, v_c, w_c, 0, 0, 0]' is the irrotational
-% current velocity. This implies that the AUV equations of motion
-% expressed in the CO satisfies:
+% current velocity. This implies that the AUV equations of motion about the CO 
+% satisfies:
 % 
 %  MRB * nudot + CRB(nu) * nu = MRB * nudot_r + CRB(nu_r) * nu_r = tau
 %
@@ -24,14 +24,14 @@ function [MRB,CRB] = spheroid(a,b,nu2,r_bg)
 % Inputs:
 %  a, b:                Semiaxes a > b
 %  nu2 = [p, q, r]':    Angular velocity vector 
-%  r_bg:                r_bg = [xg, yg, zg]' vector from the CO to the CG
+%  r_bG:               r_bG = [xG, yG, zG]' vector from CO to CG
 % 
 % Author:    Thor I. Fossen
 % Date:      24 April 2021 
 
 O3 = zeros(3,3);
 
-% Mass of spheriod 
+% Mass of spheroid 
 rho = 1025;
 m = 4/3 * pi * rho * a * b^2;   
 
@@ -41,13 +41,13 @@ Iy = (1/5) * m * (a^2 + b^2);
 Iz = Iy;
 Ig = diag([Ix Iy Iz]);
 
-% Rigid-body matrices expressed in the CG
-MRB_CG = diag([ m m m Ix Iy Iz ]);
+% Rigid-body matrices expressed about the CG
+MRB_CG = diag([m m m Ix Iy Iz]);
 CRB_CG = [ m * Smtrx(nu2)    O3
-           O3               -Smtrx(Ig*nu2) ];
+           O3               -Smtrx(Ig * nu2) ];
        
-% Transform MRB and CRB from the CG to the CO        
-H = Hmtrx(r_bg);
+% Transform MRB and CRB from CG to CO using the screw transformation        
+H = Hmtrx(r_bG);
 MRB = H' * MRB_CG * H;    
 CRB = H' * CRB_CG * H;
 
